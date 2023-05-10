817ab348a2212567f30b37076625ba6d45cc94a6
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-10 00:28:44 -0700

    neighborhood

--- template-parts/components/neighborhood.php
+++ template-parts/components/neighborhood.php
@@ -20,7 +20,7 @@ $peroperties = new WP_Query($arg);
         <div class="row">
             <div class="col-lg-12">
                 <div class="titr-list">
-                    <h3 class="font-weight-bold">Condo Assignment</h3>
+                    <h3 class="font-weight-bold">Neighborhood</h3>
                     <a href="<?= home_url('group/condo-assignment') ?>" title="" class="view-more">View more</a>
                 </div>
                 <div class="d-flex justify-content-center">
