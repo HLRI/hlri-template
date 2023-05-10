9da7ad226d4f5027cdc5ef66bdc7cb3ea63c4aec
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-09 23:01:57 -0700

    mobile search

--- template-parts/components/search-mobile.php
+++ template-parts/components/search-mobile.php
@@ -1,5 +1,5 @@
 <?php if (!is_home()) : ?>
-<!-- <div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog m-0" role="document">
         <div class="modal-content rounded-0">
             <div class="modal-body px-2 py-3">
@@ -12,5 +12,5 @@
             </div>
         </div>
     </div>
-</div> -->
+</div>
 <?php endif; ?>
