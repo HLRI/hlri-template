21a9437a3f60ca7556c2b2f4477637eb274e572a
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-09 22:57:41 -0700

    mobile search

--- template-parts/components/search-main-menu.php
+++ template-parts/components/search-main-menu.php
@@ -1,5 +1,5 @@
 <?php if (!is_home()) : ?>
-    <!-- <div class="wrap-search position-relative">
+    <div class="wrap-search position-relative">
         <div class="input-group">
             <input autocomplete="off" name="address" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by location..." aria-label="Recipient's username" aria-describedby="basic-addon2">
             <div class="input-group-append">
@@ -7,5 +7,5 @@
             </div>
         </div>
         <div class="search-result"></div>
-    </div> -->
+    </div>
 <?php endif; ?>
--- template-parts/components/search-mobile.php
+++ template-parts/components/search-mobile.php
@@ -5,7 +5,7 @@
             <div class="modal-body px-2 py-3">
                 <div class="">
                     <div class="input-group">
-                        <input autocomplete="off" name="address" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by location...">
+                        <input autocomplete="off" name="address" type="search" onkeyup="hlr_search_mobile()" class="form-control input-search keyword" placeholder="Search by location...">
                     </div>
                 </div>
                 <div class="search-result"></div>
