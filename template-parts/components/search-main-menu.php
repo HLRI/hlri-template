61f277627350a826ed51c9c68a65c70e1bc3de7e
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-09 22:43:27 -0700

    mobile search

--- template-parts/components/search-main-menu.php
+++ template-parts/components/search-main-menu.php
@@ -1,9 +1,11 @@
-<div class="wrap-search position-relative">
-    <div class="input-group">
-        <input  autocomplete="off" name="address" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by location..." aria-label="Recipient's username" aria-describedby="basic-addon2">
-        <div class="input-group-append">
-            <button class="btn btn-search"><i class="fa fa-search"></i></button>
+<?php if (!is_home()) : ?>
+    <div class="wrap-search position-relative">
+        <div class="input-group">
+            <input autocomplete="off" name="address" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by location..." aria-label="Recipient's username" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-search"><i class="fa fa-search"></i></button>
+            </div>
         </div>
+        <div class="search-result"></div>
     </div>
-    <div class="search-result"></div>
-</div>
+<?php endif; ?>
