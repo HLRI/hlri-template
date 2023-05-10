e9624b916d1d9b19472e483307d3a9cdb93dcc4c
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-09 22:41:53 -0700

    mobile search

--- template-parts/components/search-slider.php
+++ template-parts/components/search-slider.php
@@ -1,8 +1,8 @@
-<!-- <div class="wrap-search-slider">
+<div class="wrap-search-slider">
     <div class="search-slider">
         <input autocomplete="off" name="address" type="search" onkeyup="hlr_search()" class="form-control input-search-slider keyword" placeholder="Search by location...">
         <i class="fa fa-search" style="color: #dfdfdf; font-size: 19px;"></i>
     </div>
 
     <div class="search-result"></div>
-</div> -->
+</div>
