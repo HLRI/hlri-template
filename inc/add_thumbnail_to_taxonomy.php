e4932a9542a59b3b58fb6391b687e59ee2b60829
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-10 01:20:06 -0700

    neighborhood

--- inc/add_thumbnail_to_taxonomy.php
+++ inc/add_thumbnail_to_taxonomy.php
@@ -23,3 +23,23 @@ function taxonomy_edit_custom_field($term) {
     <?php
 }
 add_action( 'neighborhood_edit_form_fields', 'taxonomy_edit_custom_field', 10, 2 );
+
+
+function aw_include_script() {
+  
+    if ( ! did_action( 'wp_enqueue_media' ) ) {
+        wp_enqueue_media();
+    }
+  
+    wp_enqueue_script( 'awscript', HLR_THEME_ASSETS. 'js/taxonomy-thumbnail.js', array('jquery'), null, false );
+}
+add_action( 'admin_enqueue_scripts', 'aw_include_script' );
+
+
+function save_taxonomy_custom_meta_field( $term_id ) {
+    if ( isset( $_POST['neighborhood_image'] ) ) {
+        update_term_meta($term_id, 'neighborhood_image', $_POST['neighborhood_image']);
+    }
+}  
+add_action( 'edited_neighborhood', 'save_taxonomy_custom_meta_field', 10, 2 );  
+add_action( 'create_neighborhood', 'save_taxonomy_custom_meta_field', 10, 2 );
