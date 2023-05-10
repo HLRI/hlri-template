febf13ff004e42cbb1c465dcf383b5ca814cb931
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-09 22:02:50 -0700

    update

---
+++ template-parts/components/search-mobile.php
@@ -0,0 +1,11 @@
+<div class="wrap-search">
+    <div class="input-group">
+        <input autocomplete="off" name="address" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by location..." aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+            <button class="btn btn-search" type="button"><i class="fa fa-search"></i></button>
+        </div>
+    </div>
+    <div class="search-result"></div>
+</div>
+
+
