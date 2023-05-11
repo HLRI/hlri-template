9295996b7eb22c206b977bfe86736a9683d2aa66
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-10 17:45:26 -0700

    neighborhood

--- template-parts/components/neighborhood.php
+++ template-parts/components/neighborhood.php
@@ -1,47 +1,37 @@
-<?php
-$arg = [
-    'post_type' => 'properties',
-    'post_status' => 'publish',
-    'posts_per_page' => 8,
-    'tax_query' => [
-        [
-            'taxonomy' => 'group',
-            'field' => 'term_id',
-            'terms' => 23,
-        ]
-    ]
-];
 
-$peroperties = new WP_Query($arg);
+<?php
+
+$terms = get_terms([
+    'taxonomy' => 'neighborhood',
+    'hide_empty' => false,
+]);
+
 
 ?>
-<?php if ($peroperties->have_posts()) : ?>
     <div class="container-fluid my-5">
         <div class="row">
             <div class="col-lg-12">
                 <div class="titr-list">
                     <h3 class="font-weight-bold">Neighborhood</h3>
-                    <a href="<?= home_url('group/condo-assignment') ?>" title="" class="view-more">View more</a>
+                    <a href="#" title="" class="view-more">View more</a>
                 </div>
                 <div class="d-flex justify-content-center">
                     <div class="owl-carousel owl-theme neighborhood wrap-list">
-                        <?php while ($peroperties->have_posts()) : $peroperties->the_post();
-                            $mdata = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
-                        ?>
+                        <?php foreach ($terms as $term) : ?>
 
                             <div class="wrap-neighborhood">
-                                <?php the_post_thumbnail('large', ['class' => 'neighborhood-image']) ?>
+                                <?php get_term_meta($term->id, 'category_image', true); ?>
+                                <img src="<?= $image ?>" class="neighborhood-image" alt="">
                                 <div class="neighborhood-title">
-                                    <?php the_title() ?>
+                                    <?= $term->name ?>
                                 </div>
                                 <a href="#" class="neighborhood-link">5 Listing</a>
                             </div>
 
-                        <?php endwhile; ?>
+                        <?php endforeach; ?>
                         <?php wp_reset_postdata(); ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-<?php endif; ?>
