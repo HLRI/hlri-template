21f5fed43dffc6a00afbacf2c6c29c099bd47db2
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-16 09:37:12 -0700

    add navigation

--- template-parts/components/navigation-single-property.php
+++ template-parts/components/navigation-single-property.php
@@ -1,17 +1,20 @@
-<div class="hlr-navigation">
-    <a href="#" class="hlr-navigation-item">
-        Gallery
-    </a>
-    <a href="#" class="hlr-navigation-item">
-        Overview
-    </a>
-    <a href="#" class="hlr-navigation-item">
-        Price List
-    </a>
-    <a href="#" class="hlr-navigation-item">
-        Floor Plans
-    </a>
-    <a href="#" class="hlr-navigation-item">
-        Register Now
-    </a>
+<div class="wrap-hlr-navigation">
+    <i class="fa fa-arrow-left"></i>
+    <div class="hlr-navigation">
+        <a href="#" class="hlr-navigation-item">
+            Gallery
+        </a>
+        <a href="#" class="hlr-navigation-item">
+            Overview
+        </a>
+        <a href="#" class="hlr-navigation-item">
+            Price List
+        </a>
+        <a href="#" class="hlr-navigation-item">
+            Floor Plans
+        </a>
+        <a href="#" class="hlr-navigation-item">
+            Register Now
+        </a>
+    </div>
 </div>
