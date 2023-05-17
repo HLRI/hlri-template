7cef1920e10f273ed4ab634d1acbedf85157563f
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-17 07:43:20 -0700

    related

--- single-properties.php
+++ single-properties.php
@@ -25,18 +25,18 @@ $price_images = @get_post_meta(get_the_ID(), 'hlr_framework_properties_price_lis
             <div class="card-profile-details p-0 position-relative">
                 <span class="update-label">Last Update : <?= get_the_modified_date('j F Y') ?></span>
                 <div class="d-flex align-items-center justify-content-between">
-                <h1 class="mb-2"><?php the_title() ?></h1>
-                <div class="card-share-single my-0">
-                    <div class="card-share-single-options">
-                        <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-facebook-square"></i></a>
-                        <a target="_blank" href="https://reddit.com/submit?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&title=<?php the_title() ?>"><i class="fa fa-reddit"></i></a>
-                        <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=linkedin&title=<?php the_title() ?>&summary=<?php the_excerpt() ?>"><i class="fa fa-linkedin-square"></i></a>
-                        <a target="_blank" href="https://wa.me/?text=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-whatsapp"></i></a>
-                        <a target="_blank" href="https://telegram.me/share/url?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=telegram"><i class="fa fa-telegram"></i></a>
-                        <a target="_blank" href="https://www.pinterest.com/pin/create/button?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&media=<?= get_the_post_thumbnail_url() ?>&description=<?php the_title() ?>"><i class="fa fa-pinterest"></i></a>
-                        <a target="_blank" href="https://twitter.com/intent/tweet?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-twitter-square"></i></a>
+                    <h1 class="mb-2"><?php the_title() ?></h1>
+                    <div class="card-share-single my-0">
+                        <div class="card-share-single-options">
+                            <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-facebook-square"></i></a>
+                            <a target="_blank" href="https://reddit.com/submit?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&title=<?php the_title() ?>"><i class="fa fa-reddit"></i></a>
+                            <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=linkedin&title=<?php the_title() ?>&summary=<?php the_excerpt() ?>"><i class="fa fa-linkedin-square"></i></a>
+                            <a target="_blank" href="https://wa.me/?text=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-whatsapp"></i></a>
+                            <a target="_blank" href="https://telegram.me/share/url?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=telegram"><i class="fa fa-telegram"></i></a>
+                            <a target="_blank" href="https://www.pinterest.com/pin/create/button?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&media=<?= get_the_post_thumbnail_url() ?>&description=<?php the_title() ?>"><i class="fa fa-pinterest"></i></a>
+                            <a target="_blank" href="https://twitter.com/intent/tweet?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-twitter-square"></i></a>
+                        </div>
                     </div>
-                </div>
                 </div>
                 <div class="content-profile"><?php the_excerpt() ?></div>
             </div>
@@ -177,7 +177,10 @@ $price_images = @get_post_meta(get_the_ID(), 'hlr_framework_properties_price_lis
         <?php endif; ?>
     <?php endif; ?>
 
+</div>
 
+
+<div class="container-fluid">
     <div class="row my-4">
         <?php
         $terms = get_the_terms(get_the_ID(), array('stage', 'type', 'city', 'neighborhood', 'group'));
@@ -300,10 +303,7 @@ $price_images = @get_post_meta(get_the_ID(), 'hlr_framework_properties_price_lis
 
         <?php endif; ?>
     </div>
-
-
-
-
 </div>
+
 
 <?php get_footer(); ?>
