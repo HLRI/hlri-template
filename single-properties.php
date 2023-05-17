3d698371a94d0ae21ebb4fcffb6558eb0698c17f
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-17 07:28:08 -0700

    related

--- single-properties.php
+++ single-properties.php
@@ -233,6 +233,9 @@ $price_images = @get_post_meta(get_the_ID(), 'hlr_framework_properties_price_lis
             $query = new WP_Query($args);
         }
 
+
+        dd($query->have_posts());
+        
         if ($query->have_posts()) :
         ?>
             <div class="related-posts">
