479738ae54db1a585fc23a7246f7ca9dbdcf5866
Shahab Arvin <shahab1992@hotmail.com>
2023-05-18 19:10:28 -0400

    neighbourhood link

--- template-parts/components/neighborhood.php
+++ template-parts/components/neighborhood.php
@@ -21,8 +21,9 @@ $terms = get_terms([
                     ?>
 
                         <div class="wrap-neighborhood">
-                            <a target="_blank" href="<?= $meta['opt-neighborhood-link']['url'] ?>" style="z-index: 3;position: absolute;width: 100%;height: 100%;"></a><img src="<?= $meta['opt-neighborhood-image']['url'] ?>" class="neighborhood-image" alt="<?= $meta['opt-neighborhood-image']['alt'] ?>">
-                            <div class="neighborhood-title">
+                            <a target="_blank" href="<?= $meta['opt-neighborhood-link']['url'] ?>" style="z-index: 3;position: absolute;width: 100%;height: 100%;">
+                            <img src="<?= $meta['opt-neighborhood-image']['url'] ?>" class="neighborhood-image" alt="<?= $meta['opt-neighborhood-image']['alt'] ?>">
+                            </a><div class="neighborhood-title">
                                 <?= $term->name ?>
                             </div>
                             <a target="_blank" href="<?= $meta['opt-neighborhood-link']['url'] ?>" class="neighborhood-link"><?= get_post_count_taxonomy($term->term_id,'neighborhood','properties') ?> Listing</a>
