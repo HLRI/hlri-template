c1134021a707dde6114a448d0d7191ff5a0b1aeb
Shahab Arvin <shahab1992@hotmail.com>
2023-05-18 19:14:46 -0400

    neighbourhood link

--- template-parts/components/neighborhood.php
+++ template-parts/components/neighborhood.php
@@ -21,7 +21,7 @@ $terms = get_terms([
                     ?>
 
                         <div class="wrap-neighborhood">
-                            <a target="_blank" href="<?= $meta['opt-neighborhood-link']['url'] ?>" style="z-index: 1;width: 100%;height: 100%;">
+                            <a target="_blank" href="<?= $meta['opt-neighborhood-link']['url'] ?>" title="<?= $meta['opt-neighborhood-link']['alt'] ?>" style="z-index: 1;width: 100%;height: 100%;">
                             <img src="<?= $meta['opt-neighborhood-image']['url'] ?>" class="neighborhood-image" alt="<?= $meta['opt-neighborhood-image']['alt'] ?>">
                             </a><div class="neighborhood-title">
                                 <?= $term->name ?>
