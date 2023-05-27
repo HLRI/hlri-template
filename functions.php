76ec71bcae35fa2aeb201886e1f8e247f2e22a8f
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-27 09:13:26 -0700

    move info from panel

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ $request = wp_remote_get('http://panel.hlric.com/wp-json/mapdata/v2/getResult');
 $data = wp_remote_retrieve_body($request);
 $body = json_decode($data, true);
 foreach ($body as $item) {
-    $post_id = newPost($item['thumbnail'], $item['title'], 'test', 'test');
+    // $post_id = newPost($item['thumbnail'], $item['title'], 'test', 'test');
 
     // $mapMeta['opt-available-floorplans'] = $item['available_floorplans'];
     // $mapMeta['opt-address'] = $item['address'];
