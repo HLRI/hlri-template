f299b6280daf70c4479e84daddb7a9dee2acad31
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-27 07:58:58 -0700

    move info from panel

--- functions.php
+++ functions.php
@@ -21,10 +21,10 @@ include HLR_THEME_PATH . 'inc/taxonomy.php';
 include HLR_THEME_PATH . 'inc/visit_history.php';
 
 $request = wp_remote_get('http://panel.hlric.com/wp-json/mapdata/v2/getResult');
-        $data = wp_remote_retrieve_body($request);
-        $body = json_decode($data);
-        echo $body;
-    wp_die('g');
+$data = wp_remote_retrieve_body($request);
+$body = json_decode($data);
+var_dump($body);
+wp_die();
 
 // function my_awesome_func_two()
 // {
