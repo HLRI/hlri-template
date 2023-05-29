c74b2c8606265ae2cd7804b7a6861597afc68d89
Shahab Arvin <shahab1992@hotmail.com>
2023-05-29 14:25:47 -0400

    fixed-navigation-on-scroll

--- constant.php
+++ constant.php
@@ -2,5 +2,5 @@
 
 define('HLR_THEME_URL', get_template_directory_uri().'/');
 define('HLR_THEME_PATH', get_template_directory().'/');
-define('HLR_THEME_COMPONENT', 'template-parts/components/'.get_template_directory().'/');
+define('HLR_THEME_COMPONENT', HLR_THEME_PATH . 'template-parts/components/');
 define('HLR_THEME_ASSETS', HLR_THEME_URL.'assets/');
