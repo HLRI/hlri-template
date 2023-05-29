1c9b900ecf09f0a547b26546e54a291a3729a06d
Shahab Arvin <shahab1992@hotmail.com>
2023-05-29 14:00:03 -0400

    fixed-navigation-on-scroll

--- single-properties.php
+++ single-properties.php
@@ -13,6 +13,7 @@ $price_images = @get_post_meta(get_the_ID(), 'hlr_framework_properties_price_lis
 
 
 <?php include HLR_THEME_PATH . 'template-parts/components/navigation-single-property.php' ?>
+<?php include HLR_THEME_PATH . 'template-parts/components/navigation-single-property-on-scroll.php' ?>
 
 <div class="container-fluid px-lg-5">
     <div class="row mt-10">
---
+++ template-parts/components/navigation-single-fixed-on-scroll.php
@@ -0,0 +1,28 @@
+<div class="wrap-hlr-navigation-fixed">
+<!--    <div class="close-hlr-navigation">-->
+<!--        <i class="fa fa-arrow-right"></i>-->
+<!--    </div>-->
+    <div class="hlr-navigation-fixed">
+        <a href="#ga" class="hlr-navigation-item-fixed">
+            Gallery
+        </a>
+        <a href="#ov" class="hlr-navigation-item-fixed">
+            Overview
+        </a>
+        <a href="#pl" class="hlr-navigation-item-fixed">
+            Price List
+        </a>
+        <a href="#fp" class="hlr-navigation-item-fixed">
+            Floor Plans
+        </a>
+        <a href="#rn" class="hlr-navigation-item-fixed">
+            Register Now
+        </a>
+        <a href="#rp" class="hlr-navigation-item-fixed">
+            Related Properties
+        </a>
+        <a href="#hp" class="hlr-navigation-item-fixed">
+            New Projects
+        </a>
+    </div>
+</div>
