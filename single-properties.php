4f5136e5e8e8f7769806df7f0128962cb9b844fa
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-02 12:27:21 -0700

    rating

--- single-properties.php
+++ single-properties.php
@@ -280,8 +280,7 @@ $mdata_single = get_post_meta($post_id, 'hlr_framework_mapdata', true);
 
 <?php
 $terms = get_the_terms($post_id, array('stage', 'type', 'city', 'neighborhood', 'group'));
-wp_die('ggg',empty($terms));
-if (!empty($terms)) {
+if (!empty($terms)) :
     $term_ids = array();
 
     foreach ($terms as $item) {
@@ -323,100 +322,101 @@ if (!empty($terms)) {
         ),
     );
     $peroperties_single = new WP_Query($args);
-}
-if ($peroperties_single->have_posts()) :
+
+    if ($peroperties_single->have_posts()) :
 ?>
-    <div class="container-fluid my-4" id="rp">
-        <div class="row">
-            <div class="col-12 px-lg-5">
-                <div class="titr-list ml-0">
-                    <h3 class="font-weight-bold">Related Properties</h3>
+        <div class="container-fluid my-4" id="rp">
+            <div class="row">
+                <div class="col-12 px-lg-5">
+                    <div class="titr-list ml-0">
+                        <h3 class="font-weight-bold">Related Properties</h3>
+                    </div>
                 </div>
-            </div>
-            <div class="col-12">
-                <div class="d-flex justify-content-center">
-                    <div class="owl-carousel owl-theme listing-wrap wrap-list">
-                        <?php while ($peroperties_single->have_posts()) : $peroperties_single->the_post();
-                            $mdata = get_post_meta($post_id, 'hlr_framework_mapdata', true);
-                        ?>
-                            <div class="card-listing card-listing-v2">
+                <div class="col-12">
+                    <div class="d-flex justify-content-center">
+                        <div class="owl-carousel owl-theme listing-wrap wrap-list">
+                            <?php while ($peroperties_single->have_posts()) : $peroperties_single->the_post();
+                                $mdata = get_post_meta($post_id, 'hlr_framework_mapdata', true);
+                            ?>
+                                <div class="card-listing card-listing-v2">
 
-                                <div class="card-listing-image card-listing-image-v2">
-                                    <a href="<?= get_the_permalink() ?>" title="<?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?>">
-                                        <?php the_post_thumbnail() ?>
-                                    </a>
-                                </div>
-
-                                <div class="card-body-listing card-body-listing-v2">
-
-                                    <div class="card-listing-content card-listing-content-v2">
+                                    <div class="card-listing-image card-listing-image-v2">
                                         <a href="<?= get_the_permalink() ?>" title="<?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?>">
-                                            <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
+                                            <?php the_post_thumbnail() ?>
                                         </a>
-                                        <div class="card-listing-description card-listing-description-v2">
-                                            <a href="<?= get_the_permalink() ?>" title="<?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?>">
-                                                <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_content()) ?>
-                                            </a>
-                                        </div>
                                     </div>
 
+                                    <div class="card-body-listing card-body-listing-v2">
 
-                                    <!-- <div class="card-listing-content card-listing-content-v2">
+                                        <div class="card-listing-content card-listing-content-v2">
+                                            <a href="<?= get_the_permalink() ?>" title="<?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?>">
+                                                <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
+                                            </a>
+                                            <div class="card-listing-description card-listing-description-v2">
+                                                <a href="<?= get_the_permalink() ?>" title="<?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?>">
+                                                    <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_content()) ?>
+                                                </a>
+                                            </div>
+                                        </div>
+
+
+                                        <!-- <div class="card-listing-content card-listing-content-v2">
                                         <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
                                         <div class="card-listing-description card-listing-description-v2">
                                             <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_content()) ?>
                                         </div>
                                     </div> -->
 
-                                    <div class="lable-listing lable-listing-v2">
-                                        <?php if (!empty($mdata['opt-min-price-sqft'])) : ?>
-                                            <div><?= "$" . $mdata['opt-min-price-sqft'] . " to " . "$" . $mdata['opt-max-price-sqft'] ?></div>
-                                        <?php endif; ?>
-                                        <?php if (!empty($mdata['opt-size-min'])) : ?>
-                                            <div><?= $mdata['opt-size-min'] . " - " . $mdata['opt-size-max'] . " Sq Ft | " . $mdata['opt-occupancy'] ?></div>
-                                        <?php endif; ?>
-                                        <div>2781 Yonge St</div>
-                                    </div>
-                                </div>
-
-
-
-                                <div class="more more-v2">
-                                    <div class="card-listing-options">
-                                        <div>
-                                            <i onclick="setLikeProperties(this, <?= $post_id ?>)" role="button" class="fa fa-heart" <?= isset($_COOKIE[$post_id]) ? ' style="color:red" ' : '' ?>></i>
-                                            <span class="text-muted" id="like-total">
-                                                <?php if (!empty(get_post_meta($post_id, 'total_like', true))) : ?>
-                                                    <?= get_post_meta($post_id, 'total_like', true)   ?>
-                                                <?php endif; ?>
-                                            </span>
+                                        <div class="lable-listing lable-listing-v2">
+                                            <?php if (!empty($mdata['opt-min-price-sqft'])) : ?>
+                                                <div><?= "$" . $mdata['opt-min-price-sqft'] . " to " . "$" . $mdata['opt-max-price-sqft'] ?></div>
+                                            <?php endif; ?>
+                                            <?php if (!empty($mdata['opt-size-min'])) : ?>
+                                                <div><?= $mdata['opt-size-min'] . " - " . $mdata['opt-size-max'] . " Sq Ft | " . $mdata['opt-occupancy'] ?></div>
+                                            <?php endif; ?>
+                                            <div>2781 Yonge St</div>
                                         </div>
-
-                                        <i role="button" class="fa fa-share-alt"></i>
-                                        <i <?= is_user_logged_in() ? in_array($post_id, get_user_meta(get_current_user_id(), 'properties_favorites', true)) ? ' style="color:#9de450" ' : '' : '' ?> role="button" onclick="bookmark(this,<?= $post_id ?>)" class="fa fa-bookmark"></i>
                                     </div>
-                                    <a href="<?= get_the_permalink() ?>" title="<?php the_title() ?>" class="">more</a>
 
-                                </div>
 
-                                <div class="card-share">
-                                    <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?= wp_get_shortlink($post_id, 'post', true) ?>"><i class="fa fa-facebook-square"></i></a>
-                                    <a target="_blank" href="https://reddit.com/submit?url=<?= wp_get_shortlink($post_id, 'post', true) ?>&title=<?php the_title() ?>"><i class="fa fa-reddit"></i></a>
-                                    <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= wp_get_shortlink($post_id, 'post', true) ?>?ref=linkedin&title=<?php the_title() ?>&summary=<?php the_content() ?>"><i class="fa fa-linkedin-square"></i></a>
-                                    <a target="_blank" href="https://wa.me/?text=<?= wp_get_shortlink($post_id, 'post', true) ?>"><i class="fa fa-whatsapp"></i></a>
-                                    <a target="_blank" href="https://telegram.me/share/url?url=<?= wp_get_shortlink($post_id, 'post', true) ?>?ref=telegram"><i class="fa fa-telegram"></i></a>
-                                    <a target="_blank" href="https://www.pinterest.com/pin/create/button?url=<?= wp_get_shortlink($post_id, 'post', true) ?>&media=<?= get_the_post_thumbnail_url() ?>&description=<?php the_title() ?>"><i class="fa fa-pinterest"></i></a>
-                                    <a target="_blank" href="https://twitter.com/intent/tweet?url=<?= wp_get_shortlink($post_id, 'post', true) ?>"><i class="fa fa-twitter-square"></i></a>
-                                    <span class="share-close"><i role="button" class="fa fa-arrow-up"></i></span>
+
+                                    <div class="more more-v2">
+                                        <div class="card-listing-options">
+                                            <div>
+                                                <i onclick="setLikeProperties(this, <?= $post_id ?>)" role="button" class="fa fa-heart" <?= isset($_COOKIE[$post_id]) ? ' style="color:red" ' : '' ?>></i>
+                                                <span class="text-muted" id="like-total">
+                                                    <?php if (!empty(get_post_meta($post_id, 'total_like', true))) : ?>
+                                                        <?= get_post_meta($post_id, 'total_like', true)   ?>
+                                                    <?php endif; ?>
+                                                </span>
+                                            </div>
+
+                                            <i role="button" class="fa fa-share-alt"></i>
+                                            <i <?= is_user_logged_in() ? in_array($post_id, get_user_meta(get_current_user_id(), 'properties_favorites', true)) ? ' style="color:#9de450" ' : '' : '' ?> role="button" onclick="bookmark(this,<?= $post_id ?>)" class="fa fa-bookmark"></i>
+                                        </div>
+                                        <a href="<?= get_the_permalink() ?>" title="<?php the_title() ?>" class="">more</a>
+
+                                    </div>
+
+                                    <div class="card-share">
+                                        <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?= wp_get_shortlink($post_id, 'post', true) ?>"><i class="fa fa-facebook-square"></i></a>
+                                        <a target="_blank" href="https://reddit.com/submit?url=<?= wp_get_shortlink($post_id, 'post', true) ?>&title=<?php the_title() ?>"><i class="fa fa-reddit"></i></a>
+                                        <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= wp_get_shortlink($post_id, 'post', true) ?>?ref=linkedin&title=<?php the_title() ?>&summary=<?php the_content() ?>"><i class="fa fa-linkedin-square"></i></a>
+                                        <a target="_blank" href="https://wa.me/?text=<?= wp_get_shortlink($post_id, 'post', true) ?>"><i class="fa fa-whatsapp"></i></a>
+                                        <a target="_blank" href="https://telegram.me/share/url?url=<?= wp_get_shortlink($post_id, 'post', true) ?>?ref=telegram"><i class="fa fa-telegram"></i></a>
+                                        <a target="_blank" href="https://www.pinterest.com/pin/create/button?url=<?= wp_get_shortlink($post_id, 'post', true) ?>&media=<?= get_the_post_thumbnail_url() ?>&description=<?php the_title() ?>"><i class="fa fa-pinterest"></i></a>
+                                        <a target="_blank" href="https://twitter.com/intent/tweet?url=<?= wp_get_shortlink($post_id, 'post', true) ?>"><i class="fa fa-twitter-square"></i></a>
+                                        <span class="share-close"><i role="button" class="fa fa-arrow-up"></i></span>
+                                    </div>
                                 </div>
-                            </div>
-                        <?php endwhile; ?>
-                        <?php wp_reset_postdata(); ?>
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata(); ?>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
 <?php endif; ?>
 
 
