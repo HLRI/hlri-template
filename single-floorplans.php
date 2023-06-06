320c2b7d78491ce7800e652d3ce241e337f1b5ad
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-06 22:25:41 +0330

    floorplan post type

--- single-floorplans.php
+++ single-floorplans.php
@@ -5,6 +5,10 @@ $theme_options = get_option('hlr_framework');
 
 <div class="container-fluid px-5 my-4">
     <div class="row">
+        <div class="col-lg-10">Presidential Floor Plan</div>
+        <div class="col-lg-2">$746/sq.ft</div>
+    </div>
+    <div class="row">
         <div class="col-lg-8">
             <div class="image-floorplan">
                 <?php the_post_thumbnail() ?>
@@ -14,8 +18,8 @@ $theme_options = get_option('hlr_framework');
             <?php if ($theme_options['opt-properties-status']) : ?>
                 <?php if (!empty($theme_options['opt-properties-shortcode'])) : ?>
                     <div class="properties-shortcode">
-                        <div class="titr-list ml-0 mb-2">
-                            <h3 class="font-weight-bold">Register Now to get full package , book your unit</h3>
+                        <div class="titr-list ml-0 mb-2 pb-1 mr-0">
+                            <h3 class="font-weight-bold h4 mb-0">Register Now to get full package , book your unit</h3>
                         </div>
                         <style>
                             <?php echo $theme_options['opt-properties-style'] ?>
