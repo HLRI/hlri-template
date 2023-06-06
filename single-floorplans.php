7c04b23642867867dd3c0a557fec2ca0a0af086b
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-06 22:01:50 +0330

    floorplan post type

--- single-floorplans.php
+++ single-floorplans.php
@@ -13,24 +13,15 @@ $theme_options = get_option('hlr_framework');
         <div class="col-lg-4">
             <?php if ($theme_options['opt-properties-status']) : ?>
                 <?php if (!empty($theme_options['opt-properties-shortcode'])) : ?>
-                    <div class="row mt-5 properties-shortcode" id="RegisterNow">
-                        <div class="col-12">
-                            <div class="titr-list ml-0 mb-2">
-                                <h3 class="font-weight-bold">Register Now to get full package , book your unit</h3>
-                            </div>
+                    <div class="properties-shortcode">
+                        <div class="titr-list ml-0 mb-2">
+                            <h3 class="font-weight-bold">Register Now to get full package , book your unit</h3>
                         </div>
-                        <div class="col-12 col-lg-4">
-                            <style>
-                                <?php echo $theme_options['opt-properties-style'] ?>
-                            </style>
-                            <div class="card-form">
-                                <?= do_shortcode($theme_options['opt-properties-shortcode']) ?>
-                            </div>
-                        </div>
-                        <div class="col-12 col-lg-8 d-none d-lg-block">
-                            <div class="form-image">
-                                <img class="fit-form-image" src="<?= $theme_options['opt-properties-banner']['url'] ?>" alt="<?= $theme_options['opt-properties-banner']['alt'] ?>">
-                            </div>
+                        <style>
+                            <?php echo $theme_options['opt-properties-style'] ?>
+                        </style>
+                        <div class="card-form">
+                            <?= do_shortcode($theme_options['opt-properties-shortcode']) ?>
                         </div>
                     </div>
                 <?php endif; ?>
