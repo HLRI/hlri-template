5ffb4b42b6c48156123f3b452a980f2c06c684ed
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-07 18:34:32 +0330

    floorplan post type

--- single-floorplans.php
+++ single-floorplans.php
@@ -163,7 +163,7 @@ $floorplans = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
             </div>
 
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 mt-5 mt-lg-0">
             <?php if ($theme_options['opt-properties-status']) : ?>
                 <?php if (!empty($theme_options['opt-properties-shortcode'])) : ?>
                     <div class="properties-shortcode">
