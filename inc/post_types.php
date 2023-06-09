ef816175c075347dd85d2f833da2dea40c77bb5f
Shahab Arvin <shahab1992@hotmail.com>
2023-06-08 23:37:13 -0400

    gh

--- inc/post_types.php
+++ inc/post_types.php
@@ -345,10 +345,10 @@ function custom_render_associated_floorplans() {
         ) );
 
         if ( $floorplans ) {
-            echo '<div class="inside">';
+            echo '<style>.flIl{border: 1px solid rebeccapurple;padding: 10px;border-radius: 5px;background-color: #f2e5ff;font-size: 16px;}</style><div class="inside">';
             echo '<ul>';
             foreach ( $floorplans as $floorplan ) {
-                echo '<li><span>' . esc_html( $floorplan->post_title ) . '</span> <a href="' . get_edit_post_link( $floorplan->ID ) . '">ویرایش</a>  <span>  </span>  <a href="' . get_post_permalink( $floorplan->ID ) . '">نمایش</a></li>';
+                echo '<li class="flIl"><span>' . esc_html( $floorplan->post_title ) . '</span> <a href="' . get_edit_post_link( $floorplan->ID ) . '">edit</a>  <span>  </span>  <a href="' . get_post_permalink( $floorplan->ID ) . '">view</a></li>';
             }
             echo '</ul>';
             echo '</div>';
