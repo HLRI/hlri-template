65bd952b6570eee31f9ddedbd92e15ea6463c494
Shahab Arvin <shahab1992@hotmail.com>
2023-06-09 17:58:10 -0400

    delete floorplan on property delete

--- inc/post_types.php
+++ inc/post_types.php
@@ -338,40 +338,7 @@ function admin_enqueue($hook)
     if ($hook == 'post-new.php') {
         if ($post_type == 'floorplans') {
             echo 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk';
-//            ?>
-<!--            <script>-->
-<!--                jQuery(document).ready(function($) {-->
-<!--                    // Handle the form submission-->
-<!--                    $('form#post').on('submit', function(event) {-->
-<!--                        // Check if an associated property is selected-->
-<!--                        var associatedProperty = $('select#associated_property').val();-->
-<!--                        if (!associatedProperty) {-->
-<!--                            event.preventDefault(); // Prevent the default form submission-->
-<!--                            alert('Please select an associated property.');-->
-<!--                            return;-->
-<!--                        }-->
-<!---->
-<!--                        // Check if a floor plan name is entered-->
-<!--                        var floorPlanName = $('input#title').val();-->
-<!--                        if (!floorPlanName) {-->
-<!--                            event.preventDefault(); // Prevent the default form submission-->
-<!--                            alert('Please enter a floor plan name.');-->
-<!--                            return;-->
-<!--                        }-->
-<!---->
-<!--                        // Update the preview link with the correct URL structure-->
-<!--                        var previewLink = $('a#sample-permalink');-->
-<!--                        var previewURL = previewLink.attr('href');-->
-<!--                        var permalink = previewURL.replace(/\/properties\/\d+\//, '/properties/' + associatedProperty + '/');-->
-<!--                        permalink = permalink.replace(/\/floorplans\/[^\/]+\//, '/floorplans/' + floorPlanName.toLowerCase().replace(/\s+/g, '-') + '/');-->
-<!--                        previewLink.attr('href', permalink);-->
-<!---->
-<!--                        // If validation passes, proceed with the form submission-->
-<!--                        $(this).unbind('submit').submit();-->
-<!--                    });-->
-<!--                });-->
-<!--            </script>-->
-<!--            --><?php
+
         }
     } elseif ($hook == 'post.php') {
         if ($post_type == 'floorplans') {
