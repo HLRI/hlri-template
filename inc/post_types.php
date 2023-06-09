7b761175a542a468327c0b9e16087ae81e35e1a1
Shahab Arvin <shahab1992@hotmail.com>
2023-06-09 18:06:05 -0400

    delete floorplan on property delete

--- inc/post_types.php
+++ inc/post_types.php
@@ -326,7 +326,7 @@ add_action('admin_footer', 'add_custom_validation_script');
 
 function checkIfItsFloorplanAdd($hook)
 {
-    dd($hook);
+    die();
     $post_type = get_post_type($_GET['post']);
     if ($hook == 'post-new.php') {
         if ($post_type == 'floorplans') {
