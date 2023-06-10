a2d04b742457b7408332f87b4c12505567d9436b
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-10 19:32:06 +0330

    floorplan table

--- single-properties.php
+++ single-properties.php
@@ -355,7 +355,7 @@ $mdata_single = get_post_meta($post_id, 'hlr_framework_mapdata', true);
                         <tr>
                             <td>
                                 <div class="d-none">
-                                <?= $floor['opt-floorplans-status'] == 'available' ? 'available' : 'Sold Out' ?>
+                                <?= $floor['opt-floorplans-status'] == 'available' ? 'Available' : 'Sold Out' ?>
                                 </div>
                                 <span class="status-floorplan <?= $floor['opt-floorplans-status'] == 'available' ? 'status-color-success' : 'status-color-danger' ?>"></span>
                             </td>
