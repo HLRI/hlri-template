9dce401e0d4754902b93bc81ac7909fef14a8e50
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-10 20:03:42 +0330

    floorplan table

--- single-floorplans.php
+++ single-floorplans.php
@@ -4,24 +4,24 @@ $theme_options = get_option('hlr_framework');
 $floorplans = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
 ?>
 <div class="container-fluid px-0 mt-lg-5 mt-2">
-        <div class="content">
-            <div class="row mb-lg-4 mb-2">
-                <div class="col-12 mb-4">
-                    <h4 class="font-weight-bold h3">Browse more Imperia Condos by Truman Floor Plans</h4>
-                </div>
-                <div class="col-12">
-                    <div class="btn-group submitter-group float-left">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">Status</div>
-                        </div>
-                        <select class="form-control status-dropdown">
-                            <option value="">All</option>
-                            <option value="Sold Out">Sold Out</option>
-                            <option value="Available">Available</option>
-                        </select>
+    <div class="content">
+        <div class="row mb-lg-4 mb-2">
+            <div class="col-12 mb-4">
+                <h4 class="font-weight-bold h3">Browse more Imperia Condos by Truman Floor Plans</h4>
+            </div>
+            <div class="col-12">
+                <div class="btn-group submitter-group float-left">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">Status</div>
                     </div>
+                    <select class="form-control status-dropdown">
+                        <option value="">All</option>
+                        <option value="Sold Out">Sold Out</option>
+                        <option value="Available">Available</option>
+                    </select>
                 </div>
-                <!-- <div class="col-8">
+            </div>
+            <!-- <div class="col-8">
                     <div class="filter-wrapper">
                         <input type="checkbox" class="filter-checkbox" value="Software Engineer" /> Software Engineer
                         <input type="checkbox" class="filter-checkbox" value="Accountant" /> Accountant
@@ -41,10 +41,10 @@ $floorplans = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
                         </select>
                     </div>
                 </div> -->
-            </div>
         </div>
-      <div class="card-form py-4">
-      <table id="example" class="table pt-4">
+    </div>
+    <div class="card-form py-4">
+        <table id="example" class="table pt-4">
             <thead>
                 <tr>
                     <th></th>
@@ -58,6 +58,9 @@ $floorplans = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
             </thead>
             <tbody>
                 <?php
+
+                $associated_property = get_post_meta(get_the_ID(), 'associated_property', true);
+
                 $args = array(
                     'post_type' => 'floorplans',
                     'numberposts' => -1,
@@ -66,13 +69,14 @@ $floorplans = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
                     'meta_query' => array(
                         array(
                             'key' => 'associated_property',
-                            'value' => get_the_ID(),
-                            'compare' => '='
-                        )
+                            'value' => $associated_property,
+                            'compare' => '=',
+                        ),
                     )
                 );
 
                 $associated_floorplans = new WP_Query(${args});
+                
                 if ($associated_floorplans->have_posts()) :
                     while ($associated_floorplans->have_posts()) :
                         $associated_floorplans->the_post();
@@ -102,6 +106,6 @@ $floorplans = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
                 ?>
             </tbody>
         </table>
-      </div>
     </div>
+</div>
 <?php get_footer(); ?>
