0cd6a483705afcdac3fb7ba6d7064d88078b198d
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-10 17:57:08 +0330

    floorplan table

--- single-properties.php
+++ single-properties.php
@@ -278,6 +278,96 @@ $mdata_single = get_post_meta($post_id, 'hlr_framework_mapdata', true);
         <?php endif; ?>
     <?php endif; ?>
 
+    <div class="container-fluid px-0 mt-lg-5 mt-2">
+        <div class="content">
+            <div class="row mb-lg-4 mb-2">
+                <div class="col-12 mb-4">
+                    <h4 class="font-weight-bold h3">Browse more Imperia Condos by Truman Floor Plans</h4>
+                </div>
+                <div class="col-12">
+                    <div class="btn-group submitter-group float-left">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Status</div>
+                        </div>
+                        <select class="form-control status-dropdown">
+                            <option value="">All</option>
+                            <option value="Sold Out">Sold Out</option>
+                            <option value="Available">Available</option>
+                        </select>
+                    </div>
+                </div>
+                <!-- <div class="col-8">
+                    <div class="filter-wrapper">
+                        <input type="checkbox" class="filter-checkbox" value="Software Engineer" /> Software Engineer
+                        <input type="checkbox" class="filter-checkbox" value="Accountant" /> Accountant
+                        <input type="checkbox" class="filter-checkbox" value="Sales Assistant" /> Sales Assistant
+                        <input type="checkbox" class="filter-checkbox" value="Developer" /> Developer
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="btn-group submitter-group float-right">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Status</div>
+                        </div>
+                        <select class="form-control status-dropdown">
+                            <option value="">All</option>
+                            <option value="Sold Out">Sold Out</option>
+                            <option value="Available">Available</option>
+                        </select>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+        <table id="example" class="table pt-4">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Suite Name</th>
+                    <th>Suite Type</th>
+                    <th>Size</th>
+                    <th>View</th>
+                    <th>Price</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $associated_floorplans = get_posts(array(
+                    'post_type' => 'floorplans',
+                    'numberposts' => -1,
+                    'meta_query' => array(
+                        array(
+                            'key' => 'associated_property',
+                            'value' => get_the_ID(),
+                            'compare' => '='
+                        )
+                    )
+                ));
+
+                if ($associated_floorplans) :
+                    foreach ($associated_floorplans as $floorplan) :
+                ?>
+                        <tr>
+                            <td>Tiger Nixon</td>
+                            <td>System Architect</td>
+                            <td>Edinburgh</td>
+                            <td>61</td>
+                            <td>2011/04/25</td>
+                            <td>$320,800</td>
+                            <td>
+                                <div class="d-none">
+                                    Available
+                                </div>
+                                <span class="status-floorplan status-color-success"></span>
+                            </td>
+                        </tr>
+                <?php
+                    endforeach;
+                endif;
+                ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <?php
