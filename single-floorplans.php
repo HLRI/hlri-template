d1e01ca8bf4da65e2290fcb2b20e5c9ea15bc61e
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-13 16:12:28 +0330

    taxonomy

--- single-floorplans.php
+++ single-floorplans.php
@@ -31,6 +31,7 @@ $property = new WP_Query(${args});
                     <?php if ($property->have_posts()) : ?>
                         <?php
                         while ($property->have_posts()) : $property->the_post();
+                        wp_die(var_dump(get_the_terms(get_the_ID(), 'neighborhood')));
                             $mdata_single = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
                         ?>
                             <div>
