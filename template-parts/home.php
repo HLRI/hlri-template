90fd5f5384c5dd3137125ddc554aeff7653014bb
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-05 18:10:17 +0330

    change performance

--- template-parts/home.php
+++ template-parts/home.php
@@ -7,31 +7,55 @@
 <div class="container-fluid mb-5 mt-5">
     <div class="row px-2 px-lg-4">
         <div class="col-lg-3">
-            <div class="card-loading">
-                <div class="profile_thumb-loading"></div>
-                <h1 class="post_title-loading"></h1>
-                <p class="paragraph-loading"></p>
+            <div class="skeleton">
+                <div class="skeleton-left">
+                    <div class="line h17 w40 m10"></div>
+                    <div class="line"></div>
+                    <div class="line h8 w50"></div>
+                    <div class="line  w75"></div>
+                </div>
+                <div class="skeleton-right">
+                    <div class="square"></div>
+                </div>
             </div>
         </div>
         <div class="col-lg-3">
-            <div class="card-loading">
-                <div class="profile_thumb-loading"></div>
-                <h1 class="post_title-loading"></h1>
-                <p class="paragraph-loading"></p>
+            <div class="skeleton">
+                <div class="skeleton-left">
+                    <div class="line h17 w40 m10"></div>
+                    <div class="line"></div>
+                    <div class="line h8 w50"></div>
+                    <div class="line  w75"></div>
+                </div>
+                <div class="skeleton-right">
+                    <div class="square"></div>
+                </div>
             </div>
         </div>
         <div class="col-lg-3">
-            <div class="card-loading">
-                <div class="profile_thumb-loading"></div>
-                <h1 class="post_title-loading"></h1>
-                <p class="paragraph-loading"></p>
+            <div class="skeleton">
+                <div class="skeleton-left">
+                    <div class="line h17 w40 m10"></div>
+                    <div class="line"></div>
+                    <div class="line h8 w50"></div>
+                    <div class="line  w75"></div>
+                </div>
+                <div class="skeleton-right">
+                    <div class="square"></div>
+                </div>
             </div>
         </div>
         <div class="col-lg-3">
-            <div class="card-loading">
-                <div class="profile_thumb-loading"></div>
-                <h1 class="post_title-loading"></h1>
-                <p class="paragraph-loading"></p>
+            <div class="skeleton">
+                <div class="skeleton-left">
+                    <div class="line h17 w40 m10"></div>
+                    <div class="line"></div>
+                    <div class="line h8 w50"></div>
+                    <div class="line  w75"></div>
+                </div>
+                <div class="skeleton-right">
+                    <div class="square"></div>
+                </div>
             </div>
         </div>
     </div>
