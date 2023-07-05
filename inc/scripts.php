0294934fe95004241b61e281df7844fa1eeb7965
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-05 18:53:03 +0330

    change performance

--- inc/scripts.php
+++ inc/scripts.php
@@ -31,7 +31,7 @@ function theme_scripts()
     wp_register_script('sweetalert2', HLR_THEME_ASSETS . 'js/sweetalert2@11.js', [], "1.0.0", true);
     wp_register_script('HLR-googleapis', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDRDql7G99eM5ij1iv2XjBX3GBw1TollJc&libraries=places&callback=initAutocomplete', [], "1.0.0", true);
     wp_register_script('datatables', 'https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js', array("jquery"), "1.0.0", true);
-    wp_register_script('alpinejs',  'https://unpkg.com/alpinejs@3.12.3/dist/cdn.min.js', [], "", true);
+    wp_register_script('alpinejs',  'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js', [], "", false);
     wp_register_script('HLR-script', HLR_THEME_ASSETS . 'js/script.js', [], "1.0.0", true);
     wp_register_script('HLR-ajax', HLR_THEME_ASSETS . 'js/ajax.js', [], "1.0.0", true);
 
