6c4df72e6e970ff8cf4553ab5e411853f5d8e96b
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-05 22:22:52 +0330

    change performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -365,11 +365,11 @@ jQuery(document).ready(function ($) {
                     var minSize = '';
 
                     if (item.metadata['opt-min-price-sqft'] != '') {
-                        maxPriceSqft = '<div>"$"' + item.metadata['opt-min-price-sqft'] + " to " + "$" + item.metadata['opt-max-price-sqft'] + '</div>';
+                        maxPriceSqft = '<div>$' + item.metadata['opt-min-price-sqft'] + ' to  $' + item.metadata['opt-max-price-sqft'] + '</div>';
                     }
 
                     if (item.metadata['opt-size-min'] != '') {
-                        minSize = '<div>' + item.metadata['opt-size-min'] + " - " + item.metadata['opt-size-max'] + " Sq Ft | " + item.metadata['opt-occupancy'] + '</div>';
+                        minSize = '<div>' + item.metadata['opt-size-min'] + ' - ' + item.metadata['opt-size-max'] + ' Sq Ft | ' + item.metadata['opt-occupancy'] + '</div>';
                     }
 
 
