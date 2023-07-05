1b49d3f1e91d5eb716b2c60323d236ad0aedec46
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-05 18:22:30 +0330

    change performance

--- template-parts/home.php
+++ template-parts/home.php
@@ -24,31 +24,34 @@
         </div>
         <div class="col-lg-4">
             <div class="skeleton">
-                <div class="skeleton-left">
-                    <div class="line h17 w40 m10"></div>
+                <div class="skeleton-left flex1">
+                    <div class="square"></div>
+                </div>
+                <div class="skeleton-right flex2">
+                    <div class="line h25 w75 m10"></div>
+                    <div class="line"></div>
+                    <div class="line h8 w50"></div>
                     <div class="line"></div>
                     <div class="line h8 w50"></div>
                     <div class="line  w75"></div>
-                </div>
-                <div class="skeleton-right">
-                    <div class="square"></div>
                 </div>
             </div>
         </div>
         <div class="col-lg-4">
             <div class="skeleton">
-                <div class="skeleton-left">
-                    <div class="line h17 w40 m10"></div>
+                <div class="skeleton-left flex1">
+                    <div class="square"></div>
+                </div>
+                <div class="skeleton-right flex2">
+                    <div class="line h25 w75 m10"></div>
+                    <div class="line"></div>
+                    <div class="line h8 w50"></div>
                     <div class="line"></div>
                     <div class="line h8 w50"></div>
                     <div class="line  w75"></div>
                 </div>
-                <div class="skeleton-right">
-                    <div class="square"></div>
-                </div>
             </div>
         </div>
-       
     </div>
 </div>
 
