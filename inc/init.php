956ba4ee1fde6a4308d07b4f43e89ebfd0afb183
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-06 17:58:12 +0330

    change performance

--- inc/init.php
+++ inc/init.php
@@ -1,24 +1,58 @@
 <?php
 
-function theme_setup(){
+function theme_setup()
+{
     add_theme_support('custom-logo');
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
 }
-add_action( 'init', 'theme_setup' );
+add_action('init', 'theme_setup');
 
-function custom_menus() {
+function custom_menus()
+{
     $locations = array(
         'main-menu'   => 'Main menu',
         'mobile-menu'   => 'Mobile menu',
     );
-    register_nav_menus( $locations );
- }
- add_action( 'init', 'custom_menus' );
+    register_nav_menus($locations);
+}
+add_action('init', 'custom_menus');
 
 
- add_action('manage_users_columns','wp_add_user_column');
-function wp_add_user_column($columns) {
+add_action('manage_users_columns', 'wp_add_user_column');
+function wp_add_user_column($columns)
+{
     $columns['user_auth_key'] = "Api token";
     return $columns;
+}
+
+add_filter('manage_users_custom_column', 'user_posts_count_column_content', 10, 3);
+function user_posts_count_column_content($output, $column_name, $user_id)
+{
+    if ($column_name == "user_auth_key") {
+        if (isset($_GET['set_user_token']) and $_GET['set_user_token'] == $user_id) {
+            update_user_meta($user_id, 'api_token', wp_generate_password(50, false));
+        }
+        $user_token = get_user_meta($user_id, "api_token", true);
+        if ($user_token != "") {
+            $output = $user_token;
+        } else {
+            $output = '<a href="' . admin_url('users.php?set_user_token=' . $user_id) . '" class="button button-primary">Get Token</a>';
+        }
+    }
+    return $output;
+}
+
+
+add_action( 'rest_api_init', 'add_custom_users_api');
+function add_custom_users_api(){
+	register_rest_route( 'method', '/get_user/', array(
+		'methods' => 'GET',
+		'callback' => 'get_custom_users_data',
+	));
+}
+
+add_filter( 'rest_url_prefix', 'change_wp_json_prefix_url');
+function change_wp_json_prefix_url( $slug ) {
+	return 'api';
 }
