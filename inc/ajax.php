bd52d39d0945416a04043f0f11d2f33f37b236ea
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-06 20:38:56 +0330

    change performance

--- inc/ajax.php
+++ inc/ajax.php
@@ -458,7 +458,8 @@ function getProperties(WP_REST_Request $request)
 
     wp_send_json([
         'data' => in_array(3654, get_user_meta($auth_user->data['id'], 'properties_favorites', true)),
-        'user_id' => $auth_user->data['id']
+        'user_id' => $auth_user->data['id'],
+        'user_meta' => get_user_meta($auth_user->data['id'], 'properties_favorites', true)
     ]);
 
     $arg = [
