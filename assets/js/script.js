769c681c4ed145a1f4500d889982e82a09512f0d
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-07 19:48:09 +0330

    change performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -348,9 +348,9 @@ jQuery(document).ready(function ($) {
 
     var token = getCookie('uthlri');
     getPropertiesRestApi('commingsoon', 6, 19, token);
-    getPropertiesRestApi('just-launched', 6, 20, token);
-    getPropertiesRestApi('buy-with-five-percent-down', 6, 21, token);
-    getPropertiesRestApi('buy-with-10-percent-down', 6, 22, token);
+    // getPropertiesRestApi('just-launched', 6, 20, token);
+    // getPropertiesRestApi('buy-with-five-percent-down', 6, 21, token);
+    // getPropertiesRestApi('buy-with-10-percent-down', 6, 22, token);
 
     function getPropertiesRestApi(className, totalProperty, termID, token) {
 
