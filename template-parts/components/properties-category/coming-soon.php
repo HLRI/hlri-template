5c684dc66f34a44bafc3deecf007cec9e9ef3771
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-07 19:49:36 +0330

    change performance

--- template-parts/components/properties-category/coming-soon.php
+++ template-parts/components/properties-category/coming-soon.php
@@ -1,3 +1,26 @@
+
+<div class="container-fluid my-5">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="titr-list">
+                <h3 class="font-weight-bold">Coming Soon</h3>
+                <a href="<?= home_url('group/coming-soon') ?>" title="" class="view-more">View more</a>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div class="owl-carousel owl-theme listing-wrap wrap-list commingsoon">
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <div class="container-fluid my-5">
     <div class="row">
         <div class="col-lg-12">
