d2c8223c5c476b711eaf653e8e4e4d230b2b376e
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-07 21:53:05 +0330

    change performance

--- template-parts/components/properties-category/coming-soon.php
+++ template-parts/components/properties-category/coming-soon.php
@@ -6,7 +6,10 @@
                 <a href="<?= home_url('group/coming-soon') ?>" title="" class="view-more">View more</a>
             </div>
             <div class="d-flex justify-content-center">
-                <div class="owl-carousel owl-theme listing-wrap wrap-list commingsoon">
+                <div class="owl-carousel owl-theme listing-wrap wrap-list ">
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
                 </div>
             </div>
         </div>
