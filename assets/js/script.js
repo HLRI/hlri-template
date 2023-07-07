c8b9efb59f667df4a664463c3496b0c4fa441a82
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-07 19:37:57 +0330

    change performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -367,6 +367,46 @@ jQuery(document).ready(function ($) {
             '                            <div class="line  w75"></div>' +
             '                        </div>' +
             '                    </div>').owlCarousel('update');
+              $("." + className).owlCarousel('add', '<div class="skeleton">' +
+            '                        <div class="skeleton-left flex1">' +
+            '                            <div class="square"></div>' +
+            '                        </div>' +
+            '                        <div class="skeleton-right flex2">' +
+            '                            <div class="line h25 w75 m10"></div>' +
+            '                            <div class="line"></div>' +
+            '                            <div class="line h8 w50"></div>' +
+            '                            <div class="line"></div>' +
+            '                            <div class="line h8 w50"></div>' +
+            '                            <div class="line  w75"></div>' +
+            '                        </div>' +
+            '                    </div>').owlCarousel('update');
+              $("." + className).owlCarousel('add', '<div class="skeleton">' +
+            '                        <div class="skeleton-left flex1">' +
+            '                            <div class="square"></div>' +
+            '                        </div>' +
+            '                        <div class="skeleton-right flex2">' +
+            '                            <div class="line h25 w75 m10"></div>' +
+            '                            <div class="line"></div>' +
+            '                            <div class="line h8 w50"></div>' +
+            '                            <div class="line"></div>' +
+            '                            <div class="line h8 w50"></div>' +
+            '                            <div class="line  w75"></div>' +
+            '                        </div>' +
+            '                    </div>').owlCarousel('update');
+              $("." + className).owlCarousel('add', '<div class="skeleton">' +
+            '                        <div class="skeleton-left flex1">' +
+            '                            <div class="square"></div>' +
+            '                        </div>' +
+            '                        <div class="skeleton-right flex2">' +
+            '                            <div class="line h25 w75 m10"></div>' +
+            '                            <div class="line"></div>' +
+            '                            <div class="line h8 w50"></div>' +
+            '                            <div class="line"></div>' +
+            '                            <div class="line h8 w50"></div>' +
+            '                            <div class="line  w75"></div>' +
+            '                        </div>' +
+            '                    </div>').owlCarousel('update');
+            
 
         $.ajax({
             type: "GET",
