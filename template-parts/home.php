7d330d6fb1900699690c9e732213086bc67d2071
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-10 23:46:21 +0330

    test

--- template-parts/home.php
+++ template-parts/home.php
@@ -1,15 +1,5 @@
 <?php $theme_options = get_option('hlr_framework'); ?>
-<?php include(HLR_THEME_COMPONENT . 'slider.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'top-section.php'); ?>
 
-<?php include(HLR_THEME_COMPONENT . 'neighborhood.php'); ?>
-
-
-
-
-
-
-<?php include(HLR_THEME_COMPONENT . 'properties-category/just-launched.php'); ?>
 
 
 <!-- <div class="container-fluid my-5">
