24f1c16e1f442bbb827c9fdf173f6c8076fdce54
Shahab Avin <shahab1992@hotmail.com>
2023-07-11 15:47:02 -0400

    new

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -101,7 +101,8 @@ $posts = new WP_Query($arg);
 
 <?php include HLR_THEME_PATH . 'template-parts/components/search-mobile.php' ?>
 <?php include HLR_THEME_PATH . 'template-parts/components/login-popup.php' ?>
-<script>jQuery('.owl-carousel').trigger('refresh.owl.carousel');</script>
+<script>jQuery('.owl-carousel').trigger('refresh.owl.carousel');
+console.log('ejra shod');</script>
 
 <?php wp_footer(); ?>
 
