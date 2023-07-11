b7c9678337d20fd556b4fe5df1b3a189e3e48a4f
Shahab Avin <shahab1992@hotmail.com>
2023-07-11 15:42:09 -0400

    new

--- footer.php
+++ footer.php
@@ -4,9 +4,5 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
 	get_template_part( 'template-parts/footer' );
 }
 
-echo "<script>jQuery('.owl-carousel').trigger('refresh.owl.carousel');</script>";
-
-wp_footer();
-
 ?>
 
