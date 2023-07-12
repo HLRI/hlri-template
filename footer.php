774d84bbfb1fc13093f268a0bfa07351bae9ffa4
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-12 18:15:38 +0330

    wp_head

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
 
 if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
 	get_template_part( 'template-parts/footer' );
+}else{
+	wp_footer();
 }
 
 ?>
--- header.php
+++ header.php
@@ -5,4 +5,6 @@
 
 if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
 	get_template_part( 'template-parts/header' );
+}else{
+	wp_head();
 }
