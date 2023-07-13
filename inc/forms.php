28e5c1cccc31364bebe57073907ae35f791517c1
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-13 20:06:04 +0330

    add formbuilder

--- inc/forms.php
+++ inc/forms.php
@@ -3,7 +3,7 @@
 add_shortcode('hlr-contact-form', 'hlr_contact_form');
 function hlr_contact_form()
 {
-    ?>
+?>
     <div class="container">
         <div class="row py-5">
             <div class="col-12">
@@ -11,8 +11,11 @@ function hlr_contact_form()
                     <label for="name">name</label>
                     <input class="form-control" type="text" name="name" placeholder="name">
                 </div>
+                <div class="my-4">
+                    <button class="btn btn-primary">send</button>
+                </div>
             </div>
         </div>
     </div>
-    <?php
+<?php
 }
