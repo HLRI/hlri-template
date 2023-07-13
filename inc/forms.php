3b07a08d9f66824320027f28a015d7f8cc843b61
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-13 22:39:27 +0330

    add formbuilder

--- inc/forms.php
+++ inc/forms.php
@@ -6,23 +6,23 @@ function hlr_contact_form()
 ?>
     <div class="container">
         <div class="row py-5">
-            <div class="col-6 my-4">
+            <div class="col-6 my-3">
                 <label for="fname">First Name</label>
                 <input class="form-control" type="text" name="fname" id="fname">
             </div>
-            <div class="col-6 my-4">
+            <div class="col-6 my-3">
                 <label for="lname">Last Name</label>
                 <input class="form-control" type="text" name="lname" id="lname">
             </div>
-            <div class="col-6 my-4">
+            <div class="col-6 my-3">
                 <label for="email">Email</label>
                 <input class="form-control" type="text" name="email" id="email">
             </div>
-            <div class="col-6 my-4">
+            <div class="col-6 my-3">
                 <label for="phone">Phone Number</label>
                 <input class="form-control" type="text" name="emaiphonel" id="phone">
             </div>
-            <div class="col-12 my-4">
+            <div class="col-12 my-3">
                 <p><label for="realtor">Are you a realtor?</label></p>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="realtor" id="realtor1" value="1">
@@ -33,15 +33,15 @@ function hlr_contact_form()
                     <label class="form-check-label" for="realtor2">No</label>
                 </div>
             </div>
-            <div class="col-12 my-4">
+            <div class="col-12 my-3">
                 <label for="message">Message</label>
                 <textarea class="form-control" name="message" id="message" rows="5"></textarea>
             </div>
-            <div class="my-4">
+            <div class="my-3">
                 <button id="send" class="btn btn-primary">Send</button>
             </div>
 
-            <div class="my-4 d-none success-message">
+            <div class="my-3 d-none success-message">
                 <small class="text-success"></small>
             </div>
         </div>
