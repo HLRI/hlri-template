19faadb77deabcd4888766f117fcb5cce72ec0fb
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-13 19:52:26 +0330

    add formbuilder

--- inc/forms.php
+++ inc/forms.php
@@ -3,6 +3,6 @@
 add_shortcode('hlr-contact-form', 'hlr_contact_form');
 function hlr_contact_form()
 {
-    $form = new Formr\Formr('bulma');
+    $form = new Formr\Formr('bootstrap');
     $form->create_form('Name, Email, Comments|textarea');
 }
