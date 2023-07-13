2c8c65bdf929f1a0abe14f96fbfe68864145a579
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-14 00:28:29 +0330

    add formbuilder

--- inc/forms.php
+++ inc/forms.php
@@ -5,24 +5,24 @@ function hlr_contact_form()
 {
 ?>
     <div class="container">
-        <div class="row py-5">
-            <div class="col-6 my-3">
+        <div class="row py-2">
+            <div class="col-6 my-2">
                 <label for="fname">First Name</label>
                 <input class="form-control" type="text" name="fname" id="fname">
             </div>
-            <div class="col-6 my-3">
+            <div class="col-6 my-2">
                 <label for="lname">Last Name</label>
                 <input class="form-control" type="text" name="lname" id="lname">
             </div>
-            <div class="col-6 my-3">
+            <div class="col-6 my-2">
                 <label for="email">Email</label>
                 <input class="form-control" type="text" name="email" id="email">
             </div>
-            <div class="col-6 my-3">
+            <div class="col-6 my-2">
                 <label for="phone">Phone Number</label>
                 <input class="form-control" type="text" name="phone" id="phone">
             </div>
-            <div class="col-12 my-3">
+            <div class="col-12 my-2">
                 <p><label for="realtor">Are you a realtor?</label></p>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="realtor" id="realtor2" value="0" checked>
@@ -36,16 +36,16 @@ function hlr_contact_form()
             <div class="col-6 my-2 d-none brokerage">
                 <input class="form-control" type="text" placeholder="Your Brokerage Name" name="brokerage" id="brokerage">
             </div>
-            <div class="col-12 my-3">
+            <div class="col-12 my-2">
                 <label for="message">Message</label>
                 <textarea class="form-control" name="message" id="message" rows="5"></textarea>
             </div>
 
-            <div class="col-12 my-3">
+            <div class="col-12 my-2">
                 <button id="send" class="btn btn-block btn-primary">Send</button>
             </div>
 
-            <div class="col-12 my-3 d-none success-message">
+            <div class="col-12 my-2 d-none success-message">
                 <small class="text-success"></small>
             </div>
         </div>
