aefdb1b648063d72bfd700baeded7954d9be4371
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-14 20:34:51 +0330

    add fast update properties

--- template-parts/admin-pages/fast-updates-properties.php
+++ template-parts/admin-pages/fast-updates-properties.php
@@ -9,47 +9,62 @@ $peroperties = new WP_Query($args);
 ?>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <style>
-    #table_length , #table_filter{
+    #table_length,
+    #table_filter {
         margin-bottom: 20px;
     }
-    .wrap-button{
+
+    .wrap-button {
         margin-bottom: 20px;
     }
 </style>
-<div class="wrap">
-    <h1><?= $title ?></h1><br>
-    <div class="wrap-button">
-        <button type="submit" class="button button-primary">Update</button>
-    </div>
-    <?php if ($peroperties->have_posts()) : ?>
-        <table style="margin-bottom: 20px;" class="wp-list-table widefat fixed striped table-view-list" id="table">
-            <thead>
-                <tr>
-                    <th>Property Name</th>
-                    <th>Close</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while ($peroperties->have_posts()) : $peroperties->the_post(); ?>
-                    <?php $meta = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true); ?>
+<form action="" method="post">
+    <div class="wrap">
+        <h1><?= $title ?></h1><br>
+        <div class="wrap-button">
+            <button name="btn-set" type="submit" class="button button-primary">Update</button>
+        </div>
+        <?php if ($peroperties->have_posts()) : ?>
+            <table style="margin-bottom: 20px;" class="wp-list-table widefat fixed striped table-view-list" id="table">
+                <thead>
                     <tr>
-                        <td><?php the_title() ?></td>
-                        <td><input <?= $meta['opt-project-status'] ? 'checked' : '' ?> type="checkbox" name="" id=""></td>
-                        <td><input type="text" value="<?= $meta['opt-price'] ?>"></td>
+                        <th>Property Name</th>
+                        <th>Close</th>
+                        <th>Price</th>
                     </tr>
-                <?php
-                endwhile;
-                wp_reset_postdata();
-                wp_reset_query();
-                ?>
-            </tbody>
-        </table>
-    <?php endif; ?>
-    <div class="wrap-button">
-        <button type="submit" class="button button-primary">Update</button>
+                </thead>
+                <tbody>
+                    <?php while ($peroperties->have_posts()) : $peroperties->the_post(); ?>
+                        <?php $meta = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true); ?>
+                        <tr>
+                            <input type="hidden" name="ids[]" value="<?= get_the_ID() ?>">
+                            <td><?php the_title() ?></td>
+                            <td><input <?= $meta['opt-project-status'] ? 'checked' : '' ?> type="checkbox" name="opt-project-status[]"></td>
+                            <td><input type="text" value="<?= $meta['opt-price'] ?>" name="opt-price[]"></td>
+                        </tr>
+                    <?php
+                    endwhile;
+                    wp_reset_postdata();
+                    wp_reset_query();
+                    ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
+        <div class="wrap-button">
+            <button name="btn-set" type="submit" class="button button-primary">Update</button>
+        </div>
     </div>
-</div>
+</form>
+
+<?php
+
+if(isset($_POST['btn-set'])){
+    
+}
+
+?>
+
+
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script>
     dataTable = jQuery("#table").DataTable({
