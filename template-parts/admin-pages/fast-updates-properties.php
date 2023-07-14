c3ce110164d9c0c556a503621ea64a51ad285bd8
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-14 22:10:05 +0330

    add fast update properties

--- template-parts/admin-pages/fast-updates-properties.php
+++ template-parts/admin-pages/fast-updates-properties.php
@@ -43,10 +43,10 @@ $peroperties = new WP_Query($args);
                     <?php while ($peroperties->have_posts()) : $peroperties->the_post(); ?>
                         <?php $meta = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true); ?>
                         <tr>
-                            <input type="hidden" name="test[<?= get_the_ID() ?>][ids]" value="<?= get_the_ID() ?>">
+                            <input type="hidden" name="data[<?= get_the_ID() ?>][ids]" value="<?= get_the_ID() ?>">
                             <td><?php the_title() ?></td>
-                            <td><input <?= $meta['opt-project-status'] ? 'checked' : '' ?> type="checkbox" name="test[<?= get_the_ID() ?>][opt-project-status]"></td>
-                            <td><input type="text" value="<?= $meta['opt-price'] ?>" name="test[<?= get_the_ID() ?>][opt-price]"></td>
+                            <td><input <?= $meta['opt-project-status'] ? 'checked' : '' ?> type="checkbox" name="data[<?= get_the_ID() ?>][opt-project-status]"></td>
+                            <td><input type="text" value="<?= $meta['opt-price'] ?>" name="data[<?= get_the_ID() ?>][opt-price]"></td>
                         </tr>
                     <?php
                     endwhile;
@@ -66,7 +66,7 @@ $peroperties = new WP_Query($args);
 
 if (isset($_POST['btn-set'])) {
 
-    wp_die(var_dump($_POST));
+    wp_die(var_dump($_POST['data']));
     // foreach($_POST as $item){
 
     // }
