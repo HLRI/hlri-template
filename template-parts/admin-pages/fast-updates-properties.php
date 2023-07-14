eebcf73a238da476875354fdba5e5014cc19c68c
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-14 19:36:04 +0330

    add fast update properties

--- template-parts/admin-pages/fast-updates-properties.php
+++ template-parts/admin-pages/fast-updates-properties.php
@@ -1,3 +1,31 @@
+<?php
+$args = array(
+    'post_type' => 'properties',
+    'post_status' => 'publish',
+    'posts_per_page' => -1,
+
+);
+$peroperties = new WP_Query($args);
+?>
+
 <div class="wrap">
     <h1><?= $title ?></h1>
+    <?php if ($peroperties_single->have_posts()) : ?>
+        <table class="wp-list-table widefat fixed striped table-view-list">
+            <thead>
+                <tr>
+                    <th>test</th>
+                    <th>test</th>
+                    <th>test</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            </tbody>
+        </table>
+    <?php endif; ?>
 </div>
