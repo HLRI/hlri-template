9b84ef5dd32f369d19e3da74c680276587a8d957
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-14 23:02:44 +0330

    add fast update properties

--- template-parts/admin-pages/fast-updates-properties.php
+++ template-parts/admin-pages/fast-updates-properties.php
@@ -15,12 +15,7 @@ if (isset($_POST['btn-set'])) {
 
 
         $data = json_encode($meta);
-        header('Content-Type: application/json');
-        header('Content-Disposition: attachment; filename=data.json');
-        header('Expires: 0'); //No caching allowed
-        header('Cache-Control: must-revalidate');
-        header('Content-Length: ' . strlen($data));
-        file_put_contents('php://output', $data);
+        
 
     }
     $success = true;
