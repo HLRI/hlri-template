db51ae6616ea1bf01add1599e24ea121d68a72c4
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-07-15 18:06:16 +0330

    add form to single property

--- inc/forms.php
+++ inc/forms.php
@@ -42,7 +42,9 @@ function hlr_contact_form()
             </div>
 
             <div class="col-12 my-2">
-                <a id="send" class="btn-block top-section-button">Send</a>
+                <a id="send" class="btn-block top-section-button">
+                    Send
+                </a>
             </div>
 
             <div class="col-12 my-2 d-none success-message">
@@ -75,6 +77,8 @@ function hlr_contact_form()
 
             jQuery('#send').click(function() {
 
+                jQuery(this).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> <span class="sr-only">Processing...</span>');
+
                 jQuery('.input-error').remove();
                 jQuery('.input-error').remove();
                 jQuery('.success-message').addClass('d-none');
@@ -100,6 +104,7 @@ function hlr_contact_form()
                                 jQuery('#' + index).after('<small class="text-danger input-error">' + error + '</small>');
                             });
                         } else if (response.status == 'success') {
+                            jQuery(this).html('Send');
                             jQuery('.success-message').removeClass('d-none');
                             jQuery('.success-message small').text(response.data);
                         }
