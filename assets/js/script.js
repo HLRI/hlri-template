cf2e982e9e427b796795bd971e300693ddb4a80e
Shahab Avin <shahab1992@hotmail.com>
2023-07-25 15:01:48 -0400

    Auto-commit on Tue 07-25-2023 15_01_48.52

--- assets/js/script.js
+++ assets/js/script.js
@@ -475,7 +475,7 @@ function getPropertiesRestApi(className, totalProperty, termID, token) {
                     '                                </div>'
                 ).owlCarousel('update')
             });
-            jQuery('.F').trigger('refresh.owl.carousel');
+            jQuery('.owl-carousel').trigger('refresh.owl.carousel');
         },
         error: function (xhr, status, errorThrown) {
             xhr.status;
