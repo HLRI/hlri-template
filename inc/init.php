60f6f75a468f2bca13c00fd283d66483caaee64f
Shahab Avin <shahab1992@hotmail.com>
2023-07-25 17:54:47 -0400

    Auto-commit on Tue 07-25-2023 17_54_47.71

--- inc/init.php
+++ inc/init.php
@@ -66,9 +66,9 @@ function setTokenAfterLogin($username, $user)
 add_action('wp_login', 'setTokenAfterLogin', 10, 2);
 
 
-add_action('publish_properties', 'setpropertiesquery');
-add_action('trash_properties', 'setpropertiesquery');
-add_action('draft_properties', 'setpropertiesquery');
+//add_action('publish_properties', 'setpropertiesquery');
+//add_action('trash_properties', 'setpropertiesquery');
+//add_action('draft_properties', 'setpropertiesquery');
 function setpropertiesquery()
 {
     // if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE){
