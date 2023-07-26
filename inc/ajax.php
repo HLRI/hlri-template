d0532318baebc5707ea61f31a4cdbe5d224e8dfa
ehsanerfani98 <you@example.com>
2023-07-26 07:51:27 -0700

    edit cache

--- inc/ajax.php
+++ inc/ajax.php
@@ -471,10 +471,8 @@ function getProperties(WP_REST_Request $request)
 
     $i = 0;
     $result = get_transient('properties_data');
-    return new WP_REST_Response([
-        'list' =>$result == false
-    ], 200);
-    if ($result === false) {
+ 
+    if ($result == false) {
         $args = [
             'post_type' => 'properties',
             'post_status' => 'publish',
