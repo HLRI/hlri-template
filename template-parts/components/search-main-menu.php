39b7487708091ea16b52437969a19c12f0c0c53c
ehsanerfani98 <you@example.com>
2023-07-26 09:58:15 -0700

    author

--- template-parts/components/search-main-menu.php
+++ template-parts/components/search-main-menu.php
@@ -1,7 +1,7 @@
 <?php if (!is_home()) : ?>
     <div class="wrap-search position-relative">
         <div class="input-group">
-            <input name="type" type="text" class="form-control">
+            <input name="type" type="text">
             <div class="input-group-append">
                 <button class="btn btn-search"><i class="fa fa-search"></i></button>
             </div>
