779598d5ec503461ef7cf43dcb314fb54161e559
Shahab Avin <shahab1992@hotmail.com>
2023-07-27 11:42:50 -0400

    Auto-commit on Thu 07-27-2023 11_42_49.95

--- single-properties.php
+++ single-properties.php
@@ -101,9 +101,8 @@ $psd = properties_single_cached();
 
                 </div>
 
-                <?php if (!empty($psd['content'])) : ?>
-                    <div class="content-profile"><?= $psd['content'] ?></div>
-                // added content
+                <?php if (!empty($psd['excerpt'])) : ?>
+                    <div class="content-profile"><?= $psd['excerpt'] ?></div>
                 <?php endif; ?>
             </div>
         </div>
