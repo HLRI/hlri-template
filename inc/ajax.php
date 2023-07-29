0329a5d4190c0ec04a33e03e1ea4d05e39177a9f
ehsanerfani98 <you@example.com>
2023-07-29 09:25:20 -0700

    add api

--- inc/ajax.php
+++ inc/ajax.php
@@ -603,3 +603,101 @@ function getForm(WP_REST_Request $request)
         ]);
     }
 }
+
+
+
+
+
+add_action('rest_api_init', function () {
+    register_rest_route('mapdata/v2', '/getResult', array(
+        'methods' => 'GET',
+        'callback' => 'my_awesome_func_two',
+    ));
+});
+
+function my_awesome_func_two()
+{
+    $args = array(
+        'post_type' => 'mapdata',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        // 'p' => 2567
+    );
+
+    $my_query = null;
+    $my_query = new WP_query($args);
+    if ($my_query->have_posts()) :
+        while ($my_query->have_posts()) : $my_query->the_post();
+
+            $mapMeta = get_post_meta(get_the_ID(), 'hlr_framework_map', true);
+            // if($mapMeta['opt-status'] !== "sold out"){
+            if (true == true) {
+                if (!empty($mapMeta)) {
+                    $slug = get_post_field('post_name', get_post());
+
+                    // $mapMetaType = array_map(function($item) {
+                    //     return ($item == "Townhouse") ? "TownHouse" : $item;
+                    // }, $mapMeta['opt-type']);
+
+
+
+                    if (!is_array($mapMeta['opt-type']) && $mapMeta['opt-type'] != null) {
+
+                        $mapMetaType = explode(',', $mapMeta['opt-type']);
+                    } else {
+                        $mapMetaType = $mapMeta['opt-type'];
+                    }
+
+                    $mapMetaType = array_map(function ($item) {
+                        return ($item == "Home") ? "Detached" : $item;
+                    }, $mapMetaType);
+
+                    $mapdata[] = [
+                        'post_id' => strval(get_the_ID()),
+                        'title' => get_the_title(),
+                        'available_floorplans' => $mapMeta[$mapMeta['opt-available-floorplans']],
+                        //                  'permalink' => get_the_permalink(),
+                        'permalink' => 'https://locatecondo.com/i/' . $slug,
+                        'updated' => get_the_date(),
+                        'address' => $mapMeta['opt-address'],
+                        'thumbnail' => get_the_post_thumbnail_url(),
+                        'pricepersqft' => $mapMeta['opt-pricepersqft'],
+                        'strings' => [],
+                        'terms' => $mapMeta['opt-incentives'],
+                        'price' => $mapMeta['opt-price'],
+                        'min_price' => $mapMeta['opt-price-min'],
+                        'max_price' => $mapMeta['opt-price-max'],
+                        'min_size' => $mapMeta['opt-size-min'],
+                        'max_size' => $mapMeta['opt-size-max'],
+                        'sales_type' => $mapMeta['opt-sales-type'],
+                        'min_bed' => $mapMeta['opt-min-bed'],
+                        'max_bed' => $mapMeta['opt-max-bed'],
+                        'min_bath' => $mapMeta['opt-min-bath'],
+                        'max_bath' => $mapMeta['opt-max-bath'],
+                        'type' => $mapMetaType,
+                        'min_price_sqft' => $mapMeta['opt-min-price-sqft'],
+                        'max_price_sqft' => $mapMeta['opt-max-price-sqft'],
+                        'sqft_avg' => $mapMeta['opt-sqft-avg'],
+                        'occupancy' => $mapMeta['opt-occupancy'],
+                        'coming_soon' => $mapMeta['opt-coming-soon'],
+                        'comission_by_percent' => $mapMeta['opt-comission-by-percent'],
+                        'comission_by_flatfee' => $mapMeta['opt-comission-by-flatfee'],
+                        'floorplans' => [],
+                        'city' => $mapMeta['opt-city'],
+                        'studio' => $mapMeta['opt-studio'],
+                        'status' => $mapMeta['opt-status'],
+                        'coords' => [$mapMeta['opt-coords']['longitude'], $mapMeta['opt-coords']['latitude']],
+                        'coords2' => $mapMeta['opt-coords'],
+                        'externalid' => $mapMeta['opt-externalid'],
+                    ];
+                }
+            }
+
+        endwhile;
+        wp_reset_postdata();
+    else :
+        _e('Sorry, no posts matched your criteria.');
+    endif;
+
+    return $mapdata;
+}
