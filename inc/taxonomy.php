1ac2a9f7781b5e0e18bd6e827887797bd234184c
Shahab Avin <shahab1992@hotmail.com>
2023-08-02 19:21:39 -0400

    Auto-commit on Wed 08-02-2023 19_21_39.73

--- inc/taxonomy.php
+++ inc/taxonomy.php
@@ -190,28 +190,44 @@ function developer ()
 add_action('init', 'developer', 0);
 
 
-function salesteam ()
+// Register Sales Team taxonomy
+function salesteam()
 {
-  register_taxonomy('sales-team', 'properties', array(
-    'hierarchical' => true,
-    'labels' => array(
-      'name' => _x('Sales Team', 'taxonomy general name'),
-      'singular_name' => _x('Sales Team', 'taxonomy singular name'),
-      'search_items' =>  __('Search Sales Team'),
-      'all_items' => __('All Sales Team'),
-      'parent_item' => __('Parent Sales Team'),
-      'parent_item_colon' => __('Parent Sales Team:'),
-      'edit_item' => __('Edit Sales Team'),
-      'update_item' => __('Update Sales Team'),
-      'add_new_item' => __('Add New Sales Team'),
-      'new_item_name' => __('New Sales Team Name'),
-      'menu_name' => __('Sales Team'),
-    ),
-    'rewrite' => array(
-      'slug' => 'sales-team',
-      'with_front' => false,
-      'hierarchical' => false
-    ),
-  ));
+    register_taxonomy('sales-team', 'properties', array(
+        'hierarchical' => true,
+        'labels' => array(
+            'name' => _x('Sales Team', 'taxonomy general name'),
+            'singular_name' => _x('Sales Team', 'taxonomy singular name'),
+            'search_items' => __('Search Sales Team'),
+            'all_items' => __('All Sales Team'),
+            'parent_item' => __('Parent Sales Team'),
+            'parent_item_colon' => __('Parent Sales Team:'),
+            'edit_item' => __('Edit Sales Team'),
+            'update_item' => __('Update Sales Team'),
+            'add_new_item' => __('Add New Sales Team'),
+            'new_item_name' => __('New Sales Team Name'),
+            'menu_name' => __('Sales Team'),
+        ),
+        'rewrite' => array(
+            'slug' => 'sales-team',
+            'with_front' => false,
+            'hierarchical' => false
+        ),
+    ));
 }
 add_action('init', 'salesteam', 0);
+
+// Enable Sales Team taxonomy for searching in Properties edit page
+function enable_sales_team_search($query)
+{
+    if (is_admin() && $query->is_main_query() && $query->get('post_type') === 'properties') {
+        $query->set('tax_query', array(
+            array(
+                'taxonomy' => 'sales-team',
+                'field' => 'slug',
+                'terms' => get_query_var('sales-team'),
+            ),
+        ));
+    }
+}
+add_action('pre_get_posts', 'enable_sales_team_search');
