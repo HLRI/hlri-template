92d51271ef5f68eb50a3d1e94d5dc24d6c2530c2
ehsanerfani98 <you@example.com>
2023-08-03 07:49:27 -0700

    taxonomy developer

--- taxonomy-developer.php
+++ taxonomy-developer.php
@@ -3,97 +3,99 @@
         <div class="col-12">
             <div class="modal-body body-login">
 
+                <div class="card">
                 <div class="modal-body-login login-form">
 
-                    <div class="form-loading d-none">
-                        <div class="spinner-border text-primary" role="status"></div>
-                    </div>
+<div class="form-loading d-none">
+    <div class="spinner-border text-primary" role="status"></div>
+</div>
 
-                    <h3 class="title">Login Form</h3>
-                    <p class="description"></p>
-                    <div class="form-group">
-                        <span class="input-icon"><i class="fa fa-user"></i></span>
-                        <input type="text" class="form-control" id="username" placeholder="Enter Email or Username">
-                    </div>
-                    <div class="form-group">
-                        <span class="input-icon"><i class="fas fa-key"></i></span>
-                        <input type="password" class="form-control" id="password" placeholder="Enter Password">
-                    </div>
-                    <div class="notif-info d-none"></div>
+<h3 class="title">Login Form</h3>
+<p class="description"></p>
+<div class="form-group">
+    <span class="input-icon"><i class="fa fa-user"></i></span>
+    <input type="text" class="form-control" id="username" placeholder="Enter Email or Username">
+</div>
+<div class="form-group">
+    <span class="input-icon"><i class="fas fa-key"></i></span>
+    <input type="password" class="form-control" id="password" placeholder="Enter Password">
+</div>
+<div class="notif-info d-none"></div>
 
 
-                    <!-- <div class="form-group checkbox">
-                    <input id="remember" type="checkbox">
-                    <label for="remember">Remamber me</label>
-                </div> -->
+<!-- <div class="form-group checkbox">
+<input id="remember" type="checkbox">
+<label for="remember">Remamber me</label>
+</div> -->
 
-                    <?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
+<?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
 
-                    <div class="sign-in-btn">
-                        <button class="btn" id="submit-login">Login</button>
-                        <button class="btn btn-register btn-orange-form">Register</button>
-                    </div>
+<div class="sign-in-btn">
+    <button class="btn" id="submit-login">Login</button>
+    <button class="btn btn-register btn-orange-form">Register</button>
+</div>
 
-                    <div class="login-by-social">
-                        <?php echo do_shortcode('[nextend_social_login]'); ?>
-                        <?php do_action('login_hlri_form'); ?>
-                    </div>
+<div class="login-by-social">
+    <?php echo do_shortcode('[nextend_social_login]'); ?>
+    <?php do_action('login_hlri_form'); ?>
+</div>
 
-                    <div class="wrap-bottom-login">
-                        <br>
-                        <a href="#" class="forgot-pass btn-forgot-password">Forgot Password?</a>
-                    </div>
-                </div>
+<div class="wrap-bottom-login">
+    <br>
+    <a href="#" class="forgot-pass btn-forgot-password">Forgot Password?</a>
+</div>
+</div>
 
-                <div class="modal-body-login forgot-password-form d-none">
+<div class="modal-body-login forgot-password-form d-none">
 
-                    <div class="form-loading d-none">
-                        <div class="spinner-border text-primary" role="status"></div>
-                    </div>
+<div class="form-loading d-none">
+    <div class="spinner-border text-primary" role="status"></div>
+</div>
 
-                    <h3 class="title">Password Recovery</h3>
-                    <p class="description"></p>
-                    <div class="form-group">
-                        <span class="input-icon"><i class="fa fa-user"></i></span>
-                        <input type="text" class="form-control" id="username" placeholder="Enter Email Or Username">
-                    </div>
-                    <div class="notif-info d-none"></div>
+<h3 class="title">Password Recovery</h3>
+<p class="description"></p>
+<div class="form-group">
+    <span class="input-icon"><i class="fa fa-user"></i></span>
+    <input type="text" class="form-control" id="username" placeholder="Enter Email Or Username">
+</div>
+<div class="notif-info d-none"></div>
 
-                    <?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
+<?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
 
-                    <div class="sign-in-btn">
-                        <button class="btn" id="submit-forgot-password">Submit</button>
-                        <button class="btn btn-login btn-orange-form">Login</button>
-                    </div>
-                </div>
+<div class="sign-in-btn">
+    <button class="btn" id="submit-forgot-password">Submit</button>
+    <button class="btn btn-login btn-orange-form">Login</button>
+</div>
+</div>
 
-                <div class="modal-body-login register-form d-none">
+<div class="modal-body-login register-form d-none">
 
-                    <div class="form-loading d-none">
-                        <div class="spinner-border text-primary" role="status"></div>
-                    </div>
+<div class="form-loading d-none">
+    <div class="spinner-border text-primary" role="status"></div>
+</div>
 
-                    <h3 class="title">Register Form</h3>
-                    <p class="description"></p>
-                    <div class="form-group">
-                        <span class="input-icon"><i class="fa fa-user"></i></span>
-                        <input type="text" class="form-control" id="username" placeholder="Enter Username">
-                    </div>
-                    <div class="form-group">
-                        <span class="input-icon"><i class="fa fa-envelope"></i></span>
-                        <input type="email" class="form-control" id="email" placeholder="Enter Email">
-                    </div>
-                    <div class="form-group">
-                        <span class="input-icon"><i class="fas fa-key"></i></span>
-                        <input type="password" class="form-control" id="password" placeholder="Enter Password">
-                    </div>
-                    <div class="notif-info d-none"></div>
+<h3 class="title">Register Form</h3>
+<p class="description"></p>
+<div class="form-group">
+    <span class="input-icon"><i class="fa fa-user"></i></span>
+    <input type="text" class="form-control" id="username" placeholder="Enter Username">
+</div>
+<div class="form-group">
+    <span class="input-icon"><i class="fa fa-envelope"></i></span>
+    <input type="email" class="form-control" id="email" placeholder="Enter Email">
+</div>
+<div class="form-group">
+    <span class="input-icon"><i class="fas fa-key"></i></span>
+    <input type="password" class="form-control" id="password" placeholder="Enter Password">
+</div>
+<div class="notif-info d-none"></div>
 
-                    <?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
-                    <div class="sign-in-btn">
-                        <button class="btn" id="submit-register">Register</button>
-                        <button class="btn btn-login btn-orange-form">Login</button>
-                    </div>
+<?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
+<div class="sign-in-btn">
+    <button class="btn" id="submit-register">Register</button>
+    <button class="btn btn-login btn-orange-form">Login</button>
+</div>
+</div>
                 </div>
 
             </div>
