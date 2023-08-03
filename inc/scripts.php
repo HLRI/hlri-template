884d2cb82de3283cff85630ea34490b8d94dd7be
Shahab Avin <shahab1992@hotmail.com>
2023-08-03 17:26:54 -0400

    log for location

--- inc/scripts.php
+++ inc/scripts.php
@@ -291,7 +291,7 @@ function theme_footer()
             });
 
             try {
-                console.log(<?php echo json_encode($locations) ?>);
+                console.log(<?php echo json_encode($locations['latitude']) ?>);
                 var map = L.map('map', {
                     zoomControl: false
                 }).setView(['<?= $locations['latitude'] ?>', '<?= $locations['longitude'] ?>'], 15);
