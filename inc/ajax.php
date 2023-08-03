7ba224d5d4e7a254d2513acf45fa5e9759f72610
Shahab Avin <shahab1992@hotmail.com>
2023-08-03 15:48:02 -0400

    test cache

--- inc/ajax.php
+++ inc/ajax.php
@@ -572,9 +572,6 @@ function getProperties(WP_REST_Request $request)
         }
     }
 
-    wp_send_json([
-        "data6" => $items
-    ]);
     return new WP_REST_Response([
         'list' => $items
     ], 200);
