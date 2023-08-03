856f79bd796ec766a454ad3c3a9943b309c0a59a
ehsanerfani98 <you@example.com>
2023-08-03 12:13:24 -0700

    taxonomy developer

--- inc/ajax.php
+++ inc/ajax.php
@@ -475,6 +475,9 @@ function create_routes()
 }
 function getProperties(WP_REST_Request $request)
 {
+    wp_send_json( [
+        'data' => 'dsd'
+    ] );
 
     $auth_user = checkToken();
     $is_login = false;
