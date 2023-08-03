0eba3afcef6840cf8762131eae1c92af95466334
Shahab Avin <shahab1992@hotmail.com>
2023-08-03 11:59:46 -0400

    head commit

--- inc/ajax.php
+++ inc/ajax.php
@@ -666,9 +666,9 @@ function my_awesome_func_two($request)
                             $mapMetaType = $mapMeta['opt-type'];
                         }
 
-                        $mapMetaType = array_map(function ($item) {
-                            return ($item == "Home") ? "Detached" : $item;
-                        }, $mapMetaType);
+//                        $mapMetaType = array_map(function ($item) {
+//                            return ($item == "Home") ? "Detached" : $item;
+//                        }, $mapMetaType);
 
 
                         $is_floorplan = get_floorplans_from_property(get_the_ID(), $mapMeta['opt-occupancy']);
