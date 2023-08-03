df33b8c099d150a3943475a00bd8e259f9d411ba
ehsanerfani98 <you@example.com>
2023-08-03 09:47:35 -0700

    neighborhood

--- template-parts/components/neighborhood.php
+++ template-parts/components/neighborhood.php
@@ -6,7 +6,8 @@ $terms = get_terms([
 ]);
 $terms = array_slice($terms, 0, 8);
 foreach ($terms as $term) {
-    var_dump($term);
+    // $meta = get_term_meta($term->term_id, 'neighborhood_options', true);
+    var_dump($term->term_id);
 }
 
 ?>
