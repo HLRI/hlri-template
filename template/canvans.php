c2fbd8c5212d18ad15980801cd82586f791ad647
ehsanerfani98 <you@example.com>
2023-08-29 10:50:28 -0700

    test api

--- template/canvans.php
+++ template/canvans.php
@@ -1,6 +1,5 @@
 <?php /* Template Name: Canvans */ ?>
 
-<?php /* Template Name: Api */ ?>
 <?php
 
 $args = array(
@@ -30,20 +29,3 @@ if ($portfolio_query->have_posts()) {
 wp_reset_postdata();
 
 echo json_encode($portfolio_data);
-
-
-<html <?php language_attributes(); ?>>
-
-<head>
-<meta charset="<?php bloginfo('charset') ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head() ?>
-</head>
-
-<body <?php body_class(); ?>>
-<?php the_content(); ?>
-<?php wp_footer() ?>
-</body>
-
-</html>
