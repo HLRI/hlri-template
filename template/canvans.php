cff08c5639548463771c495599520cf0178a0b62
ehsanerfani98 <you@example.com>
2023-08-29 10:52:00 -0700

    test api

--- template/canvans.php
+++ template/canvans.php
@@ -1,31 +1,17 @@
 <?php /* Template Name: Canvans */ ?>
 
-<?php
+<html <?php language_attributes(); ?>>
 
-$args = array(
-    'post_type'      => 'properties',
-    'posts_per_page' => -1,
-);
+<head>
+<meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
+</head>
 
-$portfolio_query = new WP_Query($args);
+<body <?php body_class(); ?>>
+<?php the_content(); ?>
+<?php wp_footer() ?>
+</body>
 
-
-if ($portfolio_query->have_posts()) {
-    while ($portfolio_query->have_posts()) {
-        $portfolio_query->the_post();
-
-        $post_title = get_the_title();
-        $thumbnail_url = get_the_post_thumbnail_url();
-
-        $portfolio_item = array(
-            'title'     => $post_title,
-            'thumbnail' => $thumbnail_url,
-        );
-
-        $portfolio_data[] = $portfolio_item;
-    }
-}
-
-wp_reset_postdata();
-
-echo json_encode($portfolio_data);
+</html>
