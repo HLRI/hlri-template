fe0c51cfeb0962b08bfb7e415ef86879d78487f3
ehsanerfani98 <you@example.com>
2023-09-21 07:52:59 -0700

    archive properties

--- archive-properties.php
+++ archive-properties.php
@@ -1,4 +1,7 @@
-<?php $theme_options = get_option('hlr_framework'); ?>
+<?php 
+wp_die('gg');
+
+$theme_options = get_option('hlr_framework'); ?>
 
 <?php
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
