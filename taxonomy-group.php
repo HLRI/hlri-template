23b291e11bf01ccc95224d66ec8189a6d2846a9f
ehsanerfani98 <you@example.com>
2023-10-25 07:32:51 -0700

    test

--- taxonomy-group.php
+++ taxonomy-group.php
@@ -43,9 +43,13 @@ $peroperties = new WP_Query($arg);
 
                         <div class="card-body-listing card-body-listing-v2">
                             <div class="card-listing-content card-listing-content-v2">
-                                <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
+                                <a href="<?php the_permalink() ?>" target="_blank">
+                                    <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
+                                </a>
                                 <div class="card-listing-description card-listing-description-v2">
-                                    <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_excerpt()) ?>
+                                    <a href="<?php the_permalink() ?>" target="_blank">
+                                        <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_excerpt()) ?>
+                                    </a>
                                 </div>
                             </div>
                             <div class="lable-listing lable-listing-v2">
