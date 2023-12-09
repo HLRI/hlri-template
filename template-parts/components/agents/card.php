2e793ed2344f7ba7d2b251c65f445fd72356acbf
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-12-10 01:33:44 +0330

    add agent card

---
+++ template-parts/components/agents/card.php
@@ -0,0 +1,11 @@
+<a href="<?= get_the_permalink() ?>" title="<?php the_title() ?>">
+ <div class="card-teams">
+    <?php the_post_thumbnail('normal', ['loading' => 'lazy']) ?>
+
+    <div class="card-teams-content">
+        <a class="card-teams-title  text-capitalize" href="<?= get_the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a>
+        <div class=" card-teams-staff  text-capitalize"><?php the_terms(get_the_ID(), 'staff', '', ',', ' ') ?></div>
+    </div>
+
+   </div>
+</a>
