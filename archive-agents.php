fb5d8f865b952bd45c1aee335cfa559eb849b38a
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-12-11 23:25:03 +0330

    [UPDATE]: archive-agents page redesign 1. update agents card 2. update agents list component

--- archive-agents.php
+++ archive-agents.php
@@ -1,10 +1,15 @@
 <?php get_header(); ?>
 
-<div class="container" >
-<div>
-Our Agents
-</div>
-<?php include(HLR_THEME_COMPONENT . 'agents/list.php'); ?>
+<div class="agents" >
+    <div class="agents-header">
+          <div class="agents-header-title text-center">
+              <h3>Our expert team </h3>
+              <h2 class="font-weight-bold"> We have world expert team</h2>
+          </div>
+    </div>
+    <div class="agents-content fadeInUp container" >
+      <?php include(HLR_THEME_COMPONENT . 'agents/list.php'); ?>
+    </div>
 </div>
 
 <?php get_footer(); ?>
