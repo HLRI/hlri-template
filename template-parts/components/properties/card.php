8a682e7a92b9732a786fc9171f81a41b43574779
mojtaba-1180 <mohammadsgrdev@gmail.com>
2023-12-22 22:50:28 +0330

    [UPDATE] : archive Properties and create card for properties

---
+++ template-parts/components/properties/card.php
@@ -0,0 +1,67 @@
+<div class="col-lg-3 px-2 mb-5">
+    <div class="card-listing card-listing-v2">
+
+        <div class="card-listing-image">
+            <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
+                <?php the_post_thumbnail('normal', ['loading' => 'lazy']) ?>
+            </a>
+        </div>
+
+
+        <div class="card-body-listing card-body-listing-v2">
+            <div class="card-listing-content card-listing-content-v2">
+                <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
+                    <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
+                </a>
+                <div class="card-listing-description card-listing-description-v2">
+                    <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
+                        <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_excerpt()) ?>
+                    </a>
+                </div>
+            </div>
+
+            <div class="lable-listing lable-listing-v2">
+                <?php if (!empty($mdata['opt-min-price-sqft'])) : ?>
+                    <div><?= "$" . $mdata['opt-min-price-sqft'] . " to " . "$" . $mdata['opt-max-price-sqft'] ?></div>
+                <?php endif; ?>
+                <?php if (!empty($mdata['opt-size-min'])) : ?>
+                    <div><?= $mdata['opt-size-min'] . " - " . $mdata['opt-size-max'] . " Sq Ft | " . $mdata['opt-occupancy'] ?></div>
+                <?php endif; ?>
+                <?php if (!empty($mdata['opt-address'])) : ?>
+                    <div><?= $mdata['opt-address'] ?></div>
+                <?php endif; ?>
+            </div>
+        </div>
+
+
+
+        <div class="more more-v2">
+            <div class="card-listing-options">
+                <div>
+                    <i onclick="setLikeProperties(this, <?= get_the_ID() ?>)" role="button" class="fa fa-heart" <?= isset($_COOKIE[get_the_ID()]) ? ' style="color:red" ' : '' ?>></i>
+                    <span class="text-muted" id="like-total">
+                        <?php if (!empty(get_post_meta(get_the_ID(), 'total_like', true))) : ?>
+                            <?= get_post_meta(get_the_ID(), 'total_like', true)   ?>
+                        <?php endif; ?>
+                    </span>
+                </div>
+                
+                <i role="button" class="fa fa-share-alt"></i>
+                <i <?= is_user_logged_in() && in_array(get_the_ID(), (array) get_user_meta(get_current_user_id(), 'properties_favorites', false)) ? 'style="color:#9de450"' : '' ?> role="button" onclick="bookmark(this,<?= get_the_ID() ?>)" class="fa fa-bookmark"></i>
+            </div>
+            <a href="<?= get_the_permalink() ?>" title="<?php the_title() ?>" class="">more</a>
+            
+        </div>
+
+        <!-- <div class="card-share">
+            <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-facebook-square"></i></a>
+            <a target="_blank" href="https://reddit.com/submit?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&title=<?php the_title() ?>"><i class="fa fa-reddit"></i></a>
+            <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=linkedin&title=<?php the_title() ?>&summary=<?php the_excerpt() ?>"><i class="fa fa-linkedin-square"></i></a>
+            <a target="_blank" href="https://wa.me/?text=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-whatsapp"></i></a>
+            <a target="_blank" href="https://telegram.me/share/url?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=telegram"><i class="fa fa-telegram"></i></a>
+            <a target="_blank" href="https://www.pinterest.com/pin/create/button?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&media=<?= get_the_post_thumbnail_url() ?>&description=<?php the_title() ?>"><i class="fa fa-pinterest"></i></a>
+            <a target="_blank" href="https://twitter.com/intent/tweet?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-twitter-square"></i></a>
+            <span class="share-close"><i role="button" class="fa fa-arrow-up"></i></span>
+        </div> -->
+    </div>
+</div>
