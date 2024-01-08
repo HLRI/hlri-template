dc5837cee8b64bdae5c2a8cfdde02fdd89d88366
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-01-08 19:52:16 +0330

    [UPDATE] : archive post and post card updated

--- archive-properties.php
+++ archive-properties.php
@@ -1,63 +1,148 @@
-<?php $theme_options = get_option('hlr_framework'); ?>
-
 <?php
-$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+/* Template Name: Archive Properties */
+$theme_options = get_option('hlr_framework');
+
+get_header();
+
+// Override the global define for a specific page
+define('CUSTOM_PAGE_HEADER', [
+    'subtitle' => "Explore Your Dream Space",
+    'title' => 'Properties List – All Properties',
+]);
+
+// Include the custom-page-header.php file
+include(HLR_THEME_COMPONENT . 'custom-page-header.php');
+
+// Determine the page number for pagination
+$paged = get_query_var('paged') ? absint(get_query_var('paged')) : 1;
+
+// Start building the query argument
 $arg = [
     'post_type' => 'properties',
     'post_status' => 'publish',
-    'posts_per_page'   => get_option('posts_per_page'),
+    'posts_per_page' => get_option('posts_per_page'),
     'paged' => $paged,
 ];
 
-$peroperties = new WP_Query($arg);
+// Check if a neighborhood filter has been applied
+$neighborhood = isset($_GET['neighborhood_filter']) ? $_GET['neighborhood_filter'] : '';
+if (!empty($neighborhood)) {
+    $arg['tax_query'] = [
+        [
+            'taxonomy' => 'neighborhood',
+            'field' => 'slug',
+            'terms' => $neighborhood,
+        ],
+    ];
+}
 
+// Check if a cities filter has been applied
+$cities = isset($_GET['cities_filter']) ? $_GET['cities_filter'] : '';
+if (!empty($cities)) {
+    $arg['tax_query'] = [
+        [
+            'taxonomy' => 'city',
+            'field' => 'slug',
+            'terms' => $cities,
+        ],
+    ];
+}
 
+$properties = new WP_Query($arg);
 ?>
 
-<?php get_header() ?>
-
-
-<?php
-    // Override the global define for a specific page
-    define('CUSTOM_PAGE_HEADER', [
-        'subtitle' => "Explore Your Dream Space",
-        'title' => 'Properties List – All Properties',
-    ]);
-
-    // Include the custom-page-header.php file
-    include(HLR_THEME_COMPONENT . 'custom-page-header.php');
-
-    if ($peroperties->have_posts()) : ?>
-    <div class="container-lg my-5">
-        
-        <div class="row">
-            <div class="col-md-12 px-md-5 properties-wrapper" >
-                <?php while ($peroperties->have_posts()) : $peroperties->the_post(); ?>
-                     <?php include(HLR_THEME_COMPONENT . 'properties/card.php'); ?>
-                <?php endwhile; ?>
+<div class="container-lg my-5">
+    <div class="d-flex w-100 bg-foreground mt-4 position-sticky sticky-top shadow mb-4 rounded pt-2 px-4 align-items-center" style="width:100%; z-index:77;gap:10px;">
+        <form class="mx-auto mx-md-0 row w-100 w-md-75" action="<?php echo esc_url(get_post_type_archive_link('properties')); ?>" method="get">
+            <!-- Cities -->
+            <div class="form-group col-6 col-md-3">
+                  <label for="cities_filter">City</label>
+                  <select name="cities_filter" class="form-control" id="cities_filter" onchange="this.form.submit()">
+                    <option value="">Select City</option>
+                        <?php
+                            $terms = get_terms(['taxonomy' => 'city', 'hide_empty' => false]);
+                            $selected_city = sanitize_text_field($_GET['cities_filter'] ?? '');
+                        foreach ($terms as $term) :
+                        ?>
+                            <option value="<?php echo esc_attr($term->slug); ?>" <?php selected( $selected_city, $term->slug ); ?>>
+                                <?php echo esc_html($term->name); ?>
+                            </option>
+                        <?php 
+                        endforeach;
+                        ?>
+                </select>
+            </div>   
+            <!-- group -->
+            <div class="form-group col-6 col-md-3">
+                  <label for="group_filter">Group</label>
+                  <select name="group_filter"  class="form-control" id="group_filter" onchange="this.form.submit()">
+                    <option value="">Select Group</option>
+                        <?php
+                            $terms = get_terms(['taxonomy' => 'group', 'hide_empty' => false]);
+                            $selected_group = sanitize_text_field($_GET['group_filter'] ?? '');
+                        foreach ($terms as $term) :
+                        ?> 
+                            <option value="<?php echo esc_attr($term->slug); ?>" <?php selected( $selected_group, $term->slug ); ?>>
+                                <?php echo esc_html($term->name); ?>
+                            </option>
+                        <?php 
+                        endforeach;
+                        ?>
+                </select>
             </div>
-            <?php wp_reset_postdata(); ?>
-        </div>
-
-        <div class="mt-5 row d-flex align-items-center justify-content-center">
-            <?php
-            echo paginate_links(array(
-                'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                'total'        => $peroperties->max_num_pages,
-                'current'      => max(1, get_query_var('paged')),
-                'format'       => '?paged=%#%',
-                'show_all'     => false,
-                'type'         => 'plain',
-                'end_size'     => 2,
-                'mid_size'     => 1,
-                'prev_next'    => true,
-                'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-                'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-                'add_args'     => false,
-                'add_fragment' => '',
-            ));
-            ?>
+          <!-- Neighborhoods -->
+            <div class="form-group col-6 col-md-3">
+                  <label for="neighborhood_filter">Neighborhood</label>
+                  <select name="neighborhood_filter"  class="form-control" id="neighborhood_filter" onchange="this.form.submit()">
+                    <option value="">Select Neighborhood</option>
+                    <?php
+                        $terms = get_terms(['taxonomy' => 'neighborhood', 'hide_empty' => false]);
+                        $selected_neighborhood = sanitize_text_field($_GET['neighborhood_filter'] ?? '');
+                    foreach ($terms as $term) :
+                    ?>
+                        <option value="<?php echo esc_attr($term->slug); ?>" <?php selected( $selected_neighborhood, $term->slug ); ?>>
+                            <?php echo esc_html($term->name); ?>
+                        </option>
+                    <?php 
+                    endforeach;
+                    ?>
+                </select>
+                <input type="hidden" name="post_type" value="properties">
+                <!-- Keep track of pagination -->
+                <?php if ($paged) : ?>
+                    <input type="hidden" name="paged" value="<?php echo esc_attr($paged); ?>">
+                <?php endif; ?>
+            </div>
+          
+            
+        </form>
+    </div>
+    <div class="row">
+        <div class="col-md-12 px-md-5 properties-wrapper">
+            <?php if ($properties->have_posts()) : ?>
+                <?php while ($properties->have_posts()) : $properties->the_post();
+                    $mdata = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
+                    // Output your metadata here, e.g., print_r($mdata)
+                    include(HLR_THEME_COMPONENT . 'properties/card.php');
+                endwhile;
+                wp_reset_postdata(); ?>
+            <?php else : ?>
+                <p><?php esc_html_e('Sorry, no properties matched your criteria.', 'text-domain'); ?></p>
+            <?php endif; ?>
         </div>
     </div>
-<?php endif; ?>
+    <div class="mt-5 row d-flex align-items-center justify-content-center">
+        <?php
+            echo paginate_links(array(
+                'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+                'total' => $properties->max_num_pages,
+                'current' => max(1, get_query_var('paged')),
+                'format' => '?paged=%#%',
+                'prev_text' => sprintf('<i></i> %1$s', __('Newer Properties', 'text-domain')),
+                'next_text' => sprintf('%1$s <i></i>', __('Older Properties', 'text-domain')),
+            ));
+        ?>
+    </div>
+</div>
+
 <?php get_footer(); ?>
--- template-parts/components/properties/card.php
+++ template-parts/components/properties/card.php
@@ -5,41 +5,57 @@
             <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
                 <?php the_post_thumbnail('thumbnail', ['loading' => 'lazy']) ?>
             </a>
-            <div class="properties-card_actions">
-                <div class="properties-card_actions_btn">
-                    <i onclick="setLikeProperties(this, <?= get_the_ID() ?>)" role="button" class="fa fa-heart" <?= isset($_COOKIE[get_the_ID()]) ? ' style="color:red" ' : '' ?>></i>
-                    <span class="text-muted" id="like-total">
-                        <?php if (!empty(get_post_meta(get_the_ID(), 'total_like', true))) : ?>
-                            <?= get_post_meta(get_the_ID(), 'total_like', true)   ?>
-                        <?php endif; ?>
-                    </span>
-                </div>
-                <div class="properties-card_actions_btn">
-                    <i <?= is_user_logged_in() && in_array(get_the_ID(), (array) get_user_meta(get_current_user_id(), 'properties_favorites', false)) ? 'style="color:#9de450"' : '' ?> role="button" onclick="bookmark(this,<?= get_the_ID() ?>)" class="fa fa-bookmark"></i>
-                </div>
-            </div>
         </div>
         <div class="properties-card_detail" >
             <div class="properties-card_title" >
                  <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
                     <h6 class="text-black"><?= strlen(get_the_title())  > 18 ? substr(get_the_title(), 0, 18) . '...' : get_the_title() ?></h6>
                 </a>
-            </div>
+            </div>   
             <div class="properties-card_desc" >
                  <a href="<?= get_the_permalink() ?>" title="<?= get_the_title() ?>">
-                    <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_excerpt()) ?>
+                    <?= strlen(strip_tags(get_the_excerpt()))  > 44 ? substr(strip_tags(get_the_excerpt()), 0, 44) . '...' : strip_tags(get_the_excerpt()) ?>
                 </a>
-            </div>
+            </div>   
             <div class="properties-card_opt" >
                 <?php if (!empty($mdata['opt-min-price-sqft'])) : ?>
-                    <div class="properties-card_price">price: <?= "$" . $mdata['opt-min-price-sqft'] . " to " . "$" . $mdata['opt-max-price-sqft'] ?></div>
+                    <div class="properties-card_price">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tag"><path d="M12 2H2v10l9.29 9.29c.94.94 2.48.94 3.42 0l6.58-6.58c.94-.94.94-2.48 0-3.42L12 2Z"/><path d="M7 7h.01"/></svg>    
+                        <?= "$" . $mdata['opt-min-price-sqft'] . " to " . "$" . $mdata['opt-max-price-sqft'] ?>
+                    </div>
                 <?php endif; ?>
                 <?php if (!empty($mdata['opt-size-min'])) : ?>
-                    <div class="properties-card_size">size: <?= $mdata['opt-size-min'] . " - " . $mdata['opt-size-max'] . " Sq Ft | " . $mdata['opt-occupancy'] ?></div>
+                    <div class="properties-card_size">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ruler"><path d="M21.3 15.3a2.4 2.4 0 0 1 0 3.4l-2.6 2.6a2.4 2.4 0 0 1-3.4 0L2.7 8.7a2.41 2.41 0 0 1 0-3.4l2.6-2.6a2.41 2.41 0 0 1 3.4 0Z"/><path d="m14.5 12.5 2-2"/><path d="m11.5 9.5 2-2"/><path d="m8.5 6.5 2-2"/><path d="m17.5 15.5 2-2"/></svg> 
+                    <?= $mdata['opt-size-min'] . " - " . $mdata['opt-size-max'] . " Sq Ft"?></div>
                 <?php endif; ?>
                 <?php if (!empty($mdata['opt-address'])) : ?>
-                    <div class="properties-card_addr">address: <?= $mdata['opt-address'] ?></div>
+                    <div class="properties-card_addr"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
+                    <?= $mdata['opt-address'] ?></div>
                 <?php endif; ?>
+                <?php if (!empty($mdata['opt-occupancy'])) : ?>
+                    <div class="properties-card_addr"> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar"><rect width="18" height="18" x="3" y="4" rx="2" ry="2"/><line x1="16" x2="16" y1="2" y2="6"/><line x1="8" x2="8" y1="2" y2="6"/><line x1="3" x2="21" y1="10" y2="10"/></svg>
+                        Built in <?= $mdata['opt-occupancy']  ?>
+                    </div>
+                <?php endif;?>
+            </div>
+            <div class="properties-card_actions">
+                <div  onclick="setLikeProperties(this, <?= get_the_ID() ?>)" role="button" class="properties-card_actions_btn">
+                    <i class="fa fa-heart" <?= isset($_COOKIE[get_the_ID()]) ? ' style="color:red" ' : '' ?>></i>
+                    <span id="like-total">
+                        <?php if (!empty(get_post_meta(get_the_ID(), 'total_like', true)) && get_post_meta(get_the_ID(), 'total_like', true) ) : ?>
+                            <?= get_post_meta(get_the_ID(), 'total_like', true)   ?>
+                        <?php else: ?>
+                            Like
+                        <?php endif; ?>
+                    </span>
+                </div>
+
+                <div class="properties-card_actions_btn" role="button" onclick="bookmark(this,<?= get_the_ID() ?>)">
+                    <i <?= is_user_logged_in() && in_array(get_the_ID(), (array) get_user_meta(get_current_user_id(), 'properties_favorites', false)) ? 'style="color:#9de450"' : '' ?>  class="fa fa-bookmark"></i>
+                </div>  
             </div>
         </div>
     </div>
