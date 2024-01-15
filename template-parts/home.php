ea121784cd2b58aeae27a26b945fc4e849ec01e7
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-01-15 22:42:13 +0330

    [UPDATE]: home page slider handle mobile mode

--- template-parts/home.php
+++ template-parts/home.php
@@ -1,5 +1,12 @@
 <?php $theme_options = get_option('hlr_framework'); ?>
-<?php include(HLR_THEME_COMPONENT . 'home/hero.php'); ?>
+
+<?php
+if(wp_is_mobile()):
+    include(HLR_THEME_COMPONENT . 'home/hero.php');
+else:
+    include(HLR_THEME_COMPONENT . 'slider.php');
+endif;
+?>
 <?php include(HLR_THEME_COMPONENT . 'top-section.php'); ?>
 
 <!--  homepage content -->
@@ -15,7 +22,7 @@
 <?php include(HLR_THEME_COMPONENT . 'counter.php'); ?>
 
 
-
+ 
 <?php include(HLR_THEME_COMPONENT . 'ads-banner.php'); ?>
       
     <?php do_shortcode('[testimonials count="4" display="1"]'); ?>
