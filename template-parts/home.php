cbbd8d7b3a1a1a74f837b32624d54ae5fe45bf5b
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-01-16 00:16:56 +0330

    [UPDATE]: top section home  page update

--- template-parts/home.php
+++ template-parts/home.php
@@ -12,22 +12,18 @@ endif;
 <!--  homepage content -->
 <div class="container-lg">
 
-<?php include(HLR_THEME_COMPONENT . 'neighborhood.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'properties-category/just-launched.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'properties-category/buy-with-5-percent-down.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'properties-category/buy-with-10-percent-down.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'properties-category/coming-soon.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'properties-category/condo-assignment.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'personal-list.php'); ?>
-<?php include(HLR_THEME_COMPONENT . 'counter.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'neighborhood.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'properties-category/just-launched.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'properties-category/buy-with-5-percent-down.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'properties-category/buy-with-10-percent-down.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'properties-category/coming-soon.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'properties-category/condo-assignment.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'personal-list.php'); ?>
+    <?php include(HLR_THEME_COMPONENT . 'counter.php'); ?>
 
 
- 
-<?php include(HLR_THEME_COMPONENT . 'ads-banner.php'); ?>
-      
+    <?php include(HLR_THEME_COMPONENT . 'ads-banner.php'); ?>
     <?php do_shortcode('[testimonials count="4" display="1"]'); ?>
-       
-
-
     <?php include(HLR_THEME_COMPONENT . 'blog-list.php'); ?>
+
 </div>
