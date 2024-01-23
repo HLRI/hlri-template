b9be9e12f052626788e5d40b3ee5e4c140aa918b
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-01-23 19:10:51 +0330

    [UPDATE]: card properties fix bug

--- template-parts/components/properties/card.php
+++ template-parts/components/properties/card.php
@@ -32,7 +32,8 @@
                 <?php if (!empty($mdata['opt-address'])) : ?>
                     <div class="properties-card_addr"> 
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
-                    <?= $mdata['opt-address'] ?></div>
+                    <?= strlen($mdata['opt-address'])  > 38 ? substr($mdata['opt-address'], 0, 38) . '...' : $mdata['opt-address'] ?>
+                    </div>
                 <?php endif; ?>
                 <?php if (!empty($mdata['opt-occupancy'])) : ?>
                     <div class="properties-card_addr"> 
