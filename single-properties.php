727f4665682cf326615cc04dca53c4c17e1eacf2
Shahab Avin <shahab1992@hotmail.com>
2024-02-10 16:36:49 -0500

    Auto-commit on Sat 02-10-2024 16_36_49.60

--- single-properties.php
+++ single-properties.php
@@ -33,7 +33,7 @@ $data = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
 
 
     <?php if (!empty($data['opt_price_min'])) : ?>
-                                    $<?= number_format($data['opt_price_min']) ?>
+                                    <?php echo number_format($data['opt_price_min']) ?>
                 <?php endif; ?>
 
                 <ul>
