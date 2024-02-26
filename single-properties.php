3c0cf21e718273ae8351fdaf31ddf016cae237c3
Shahab Avin <shahab1992@hotmail.com>
2024-02-26 13:57:04 -0500

    Auto-commit on Mon 02-26-2024 13_57_04.58

--- single-properties.php
+++ single-properties.php
@@ -559,12 +559,8 @@ $data = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
                                         $floor = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
                                         ?>
                                         <?php $data5 = get_post_meta(get_the_ID(), 'hlr_framework_floorplan_images_gallery', true); print_r($data5);
-                                        if (!empty($data5)) {
                                             $gallery_ids = explode(',', $data5);
                                             print_r($gallery_ids);
-                                        } else {
-                                            $gallery_ids = [];
-                                        }
                                         foreach ($gallery_ids as $gallery_item_id) {
                                             $floor_galleries_data2[] = [
                                                 'gallery_url' => wp_get_attachment_url($gallery_item_id),
