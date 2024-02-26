1d5188f0d650af0dc7d5fabafc53cbeca2a92d5e
Shahab Avin <shahab1992@hotmail.com>
2024-02-26 14:29:50 -0500

    Auto-commit on Mon 02-26-2024 14_29_50.11

--- single-properties.php
+++ single-properties.php
@@ -559,9 +559,9 @@ $data = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
                                         $floor = get_post_meta(get_the_ID(), 'hlr_framework_floorplans', true);
                                         ?>
                                         <?php $data5 = get_post_meta(get_the_ID(), 'hlr_framework_floorplan_images_gallery', true); print_r($data5) . '+';
-                                            $gallery_ids = explode(',', $data5);
-                                            print_r($gallery_ids) . '+1';
-                                        foreach ($gallery_ids as $gallery_item_id) {
+                                            $gallery_ids2 = explode(',', $data5);
+                                            print_r($gallery_ids2) . '+1';
+                                        foreach ($gallery_ids2 as $gallery_item_id) {
                                             $floor_galleries_data2[] = [
                                                 'gallery_url' => wp_get_attachment_url($gallery_item_id),
                                                 'caption' => wp_get_attachment_caption($gallery_item_id)
