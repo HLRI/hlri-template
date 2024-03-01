3df9ceb6b2aa25fbe10f1a37bb10bd858d5f17da
Shahab Avin <shahab1992@hotmail.com>
2024-02-29 20:15:19 -0500

    Auto-commit on Thu 02-29-2024 20_15_19.51

--- single-properties-assignment.php
+++ single-properties-assignment.php
@@ -25,9 +25,9 @@ $data = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
                     <div class="container">
                         <h2><?= $psd['title'] ?></h2>
                         <p class="project-title toptitle"><?= $data['opt-project-name'] ?></p>
-                            <p class="text-muted top-excerpt">
-                                <?= the_excerpt(); ?>
-                            </p>
+                        <p class="text-muted top-excerpt" ><?php if (!empty(the_excerpt())) : ?>
+                                kkk  <?php echo the_excerpt() ?>
+                            <?php endif; ?></p>
                             <?php if (($data['opt-sales-type'] == "Assignment") || ($data['opt-sales-type'] == "Resale")) : ?>
                             <style>
                                 .characteristics-cnt ul li{
