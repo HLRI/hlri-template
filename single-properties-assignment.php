7affccf498f0b53aace03607c4626655dede5d4d
Shahab Avin <shahab1992@hotmail.com>
2024-03-09 13:49:38 -0500

    Auto-commit on Sat 03-09-2024 13_49_38.67

--- single-properties-assignment.php
+++ single-properties-assignment.php
@@ -99,7 +99,7 @@ $data = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true);
                                         <?php endif; ?>
                                         <?php if (!empty($data['opt-locker'])) : ?>
                                             <li class="property-type ic-proptype">
-                                                <i class="fas fa-lock"></i><br><?php echo $data['opt-locker'] . ' Locker' ?>
+                                                <i class="fas fa-lock"></i><br><b><?php echo $data['opt-locker'] ?></b> Locker
                                             </li>
                                         <?php endif; ?>
                                         <?php if (!empty($data['opt-occupancy'])) : ?>
