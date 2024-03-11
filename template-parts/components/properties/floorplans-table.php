6ca1c17576f0b5ca6ba0bb68b602fd9c556e6de6
Shahab Avin <shahab1992@hotmail.com>
2024-03-11 15:40:09 -0400

    Auto-commit on Mon 03-11-2024 15_40_09.20

--- template-parts/components/properties/floorplans-table.php
+++ template-parts/components/properties/floorplans-table.php
@@ -2,7 +2,7 @@
     <div class="content">
         <div class="row mb-lg-4 mb-2">
             <div class="col-12 mb-4">
-                <h4 class="font-weight-bold h3">Browse more Floor Plans</h4>
+                <h4 class="font-weight-bold h3">Browse more Imperia Condos by Truman Floor Plans</h4>
             </div>
             <div class="col-12">
                 <div class="btn-group submitter-group float-left">
