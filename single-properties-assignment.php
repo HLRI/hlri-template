8013a46282dd49ef908eb91a5b062c6e4fad83a4
Shahab Avin <shahab1992@hotmail.com>
2024-03-30 22:50:22 -0400

    Auto-commit on Sat 03-30-2024 22_50_21.97

--- single-properties-assignment.php
+++ single-properties-assignment.php
@@ -679,6 +679,7 @@ function addOrdinalSuffix($number)
                 });
             </script>
         </div>
+        <br>
         <?= do_shortcode($psd['theme_options']['opt-properties-shortcode']) ?>
 
     </div>
