60162f4e6d46a42af4a5028fac3dc0fd2dbf5052
Shahab Avin <shahab1992@hotmail.com>
2024-03-30 22:24:29 -0400

    Auto-commit on Sat 03-30-2024 22_24_29.46

--- functions.php
+++ functions.php
@@ -112,3 +112,19 @@ function custom_property_template($template)
     // For other cases, return the original template
     return $template;
 }
+
+add_filter('wpcf7_form_elements', function( $content ) {
+    $dom = new DOMDocument();
+    $dom->preserveWhiteSpace = false;
+    $dom->loadHTML(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+    $xpath = new DomXPath($dom);
+    $spans = $xpath->query("//span[contains(@class, 'wpcf7-form-control-wrap')]" );
+
+    foreach ( $spans as $span ) :
+        $children = $span->firstChild;
+        $span->parentNode->replaceChild( $children, $span );
+    endforeach;
+
+    return $dom->saveHTML();
+});
