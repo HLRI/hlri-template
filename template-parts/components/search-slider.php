d48124fdace46f67515cf1c65a588fb96e18a21f
Shahab Avin <shahab1992@hotmail.com>
2024-04-09 16:10:11 -0400

    Auto-commit on Tue 04-09-2024 16_10_11.63

--- template-parts/components/search-slider.php
+++ template-parts/components/search-slider.php
@@ -1,6 +1,6 @@
 <div class="wrap-search-slider">
     <div class="search-slider">
-        <input autocomplete="off" autocorrect="off"  type="search" id="s" name="s" value="" placeholder="Search condos" autocomplete="off">
+        <input autocomplete="off" autocorrect="off"  type="search" id="s" name="s" value="" placeholder="Search condos" autocomplete="off" class="form-control input-search-slider keyword" placeholder="Search by location...">
         <input name="topsearch" autocomplete="off" autocorrect="off"  type="search" onkeyup="hlr_search()" class="form-control input-search-slider keyword" placeholder="Search by location...">
         <i class="fa fa-search"></i>
     </div>
