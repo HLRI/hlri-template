d5d47ac8f45102215c281c57c49b0763f1db6f9f
Shahab Avin <shahab1992@hotmail.com>
2024-04-09 16:26:34 -0400

    Auto-commit on Tue 04-09-2024 16_26_34.47

--- template-parts/components/search-slider.php
+++ template-parts/components/search-slider.php
@@ -1,7 +1,7 @@
 <div class="wrap-search-slider">
     <div class="search-slider">
-        <input  autocomplete="off" autocorrect="off"  type="search" id="s" name="s" value="" placeholder="Search condos" autocomplete="off" class="form-control input-search-slider keyword" placeholder="Search by location...">
-<!--        <input name="topsearch" autocomplete="off" autocorrect="off"  type="search" onkeyup="hlr_search()" class="form-control input-search-slider keyword" placeholder="Search by location...">-->
+        <input type="text" name="sd">
+        <input name="topsearch" autocomplete="off" autocorrect="off"  type="search" onkeyup="hlr_search()" class="form-control input-search-slider keyword" placeholder="Search by location...">
         <i class="fa fa-search"></i>
     </div>
     <div class="search-result">
