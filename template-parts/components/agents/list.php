018244a1e0ca980d9181e68252fce85c9257fb91
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 14:18:21 -0400

    Auto-commit on Tue 05-07-2024 14_18_21.48

--- template-parts/components/agents/list.php
+++ template-parts/components/agents/list.php
@@ -44,25 +44,25 @@ if ($profiles->have_posts()) : ?>
             wp_reset_postdata();
             ?>
         </div>
-        <div class="mt-5 row d-flex align-items-center justify-content-center">
-            <?php
-            echo paginate_links(array(
-                'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                'total'        => $sorted_profiles->max_num_pages,
-                'current'      => max(1, get_query_var('paged')),
-                'format'       => '?paged=%#%',
-                'show_all'     => false,
-                'type'         => 'plain',
-                'end_size'     => 2,
-                'mid_size'     => 1,
-                'prev_next'    => true,
-                'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-                'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-                'add_args'     => false,
-                'add_fragment' => '',
-            ));
-            ?>
-        </div>
+<!--        <div class="mt-5 row d-flex align-items-center justify-content-center">-->
+<!--            --><?php
+//            echo paginate_links(array(
+//                'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+//                'total'        => $sorted_profiles->max_num_pages,
+//                'current'      => max(1, get_query_var('paged')),
+//                'format'       => '?paged=%#%',
+//                'show_all'     => false,
+//                'type'         => 'plain',
+//                'end_size'     => 2,
+//                'mid_size'     => 1,
+//                'prev_next'    => true,
+//                'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
+//                'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
+//                'add_args'     => false,
+//                'add_fragment' => '',
+//            ));
+//            ?>
+<!--        </div>-->
 
     </div>
 <?php endif; ?>
