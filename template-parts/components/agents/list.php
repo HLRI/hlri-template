f1bded133731fcff8f79e99808d7dc2c0bc5bdd1
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 14:18:58 -0400

    Auto-commit on Tue 05-07-2024 14_18_57.99

--- template-parts/components/agents/list.php
+++ template-parts/components/agents/list.php
@@ -1,68 +1,60 @@
+<?php $theme_options = get_option('hlr_framework'); ?>
+
 <?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-
-$term = get_queried_object();
-
 $arg = [
     'post_type' => 'agents',
     'post_status' => 'publish',
-    'posts_per_page'   => get_option('posts_per_page'),
-    'paged' => $paged,
+    'posts_per_page' => -1
 ];
 
 $profiles = new WP_Query($arg);
 
-if ($profiles->have_posts()) : ?>
-
-    <div class="container-fluid px-5 mt-10">
+?>
+<?php if ($profiles->have_posts()) : ?>
+    <?php     $sorted_profiles = array();
+    ?>
+    <div class="container-fluid my-5">
         <div class="row">
-            <?php
-            // Initialize an array to store agents sorted by opt-agents-order
-            $sorted_profiles = array();
+            <div class="col-lg-12 ">
+                <div class="titr-list">
+                    <h3 class="font-weight-bold">DEDICATED AGENTS</h3>
+                    <?php if (!empty($theme_options['opt-homeleaderrealtylinkviews-agents-link']['url'])) : ?>
+                        <a href="<?= $theme_options['opt-homeleaderrealtylinkviews-agents-link']['url'] ?>" title="<?= $theme_options['opt-homeleaderrealtylinkviews-agents-link']['alt'] ?>" class="view-more">View more</a>
+                    <?php endif; ?>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="owl-carousel owl-theme teams wrap-list">
+                        <?php
+                        // Initialize an array to store agents sorted by opt-agents-order
+                        $sorted_profiles = array();
 
-            // Start the loop to populate the sorted_profiles array
-            while ($profiles->have_posts()) : $profiles->the_post();
-                // Retrieve the opt-agents-order value for the current post
-                $agent = get_post_meta(get_the_ID(), 'hlr_framework_agents', true);
-                $order = $agent['opt-agents-order'];
+                        // Start the loop to populate the sorted_profiles array
+                        while ($profiles->have_posts()) : $profiles->the_post();
+                            // Retrieve the opt-agents-order value for the current post
+                            $agent = get_post_meta(get_the_ID(), 'hlr_framework_agents', true);
+                            $order = $agent['opt-agents-order'];
 
-                // Add the current agents to the sorted_profiles array with opt-agents-order as the key
-                $sorted_profiles[$order] = get_post();
-            endwhile;
+                            // Add the current agents to the sorted_profiles array with opt-agents-order as the key
+                            $sorted_profiles[$order] = get_post();
+                        endwhile;
 
-            // Sort the sorted_profiles array based on opt-agents-order
-            ksort($sorted_profiles);
+                        // Sort the sorted_profiles array based on opt-agents-order
+                        ksort($sorted_profiles);
 
-            // Loop through sorted_profiles to display the agents
-            foreach ($sorted_profiles as $post) :
-                setup_postdata($post);
-                // Include your template part here
-                include(HLR_THEME_COMPONENT . 'agents/card.php');
-            endforeach;
+                        // Loop through sorted_profiles to display the agents
+                        foreach ($sorted_profiles as $post) :
+                            setup_postdata($post);
+                            // Include your template part here
+                            include(HLR_THEME_COMPONENT . 'agents/card-mini.php');
+                        endforeach;
 
-            // Restore original post data
-            wp_reset_postdata();
-            ?>
+                        // Restore original post data
+                        wp_reset_postdata();
+                        ?>
+                    </div>
+
+                </div>
+            </div>
         </div>
-<!--        <div class="mt-5 row d-flex align-items-center justify-content-center">-->
-<!--            --><?php
-//            echo paginate_links(array(
-//                'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-//                'total'        => $sorted_profiles->max_num_pages,
-//                'current'      => max(1, get_query_var('paged')),
-//                'format'       => '?paged=%#%',
-//                'show_all'     => false,
-//                'type'         => 'plain',
-//                'end_size'     => 2,
-//                'mid_size'     => 1,
-//                'prev_next'    => true,
-//                'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-//                'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-//                'add_args'     => false,
-//                'add_fragment' => '',
-//            ));
-//            ?>
-<!--        </div>-->
-
     </div>
 <?php endif; ?>
