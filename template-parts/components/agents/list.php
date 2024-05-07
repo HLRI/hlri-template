9bdfd9f76079d24a911626b352bbd855afe18946
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 14:12:26 -0400

    Auto-commit on Tue 05-07-2024 14_12_26.83

--- template-parts/components/agents/list.php
+++ template-parts/components/agents/list.php
@@ -34,7 +34,7 @@ if ($profiles->have_posts()) : ?>
             <?php
             echo paginate_links(array(
                 'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                'total'        => $profiles->max_num_pages,
+                'total'        => $sorted_profiles->max_num_pages,
                 'current'      => max(1, get_query_var('paged')),
                 'format'       => '?paged=%#%',
                 'show_all'     => false,
