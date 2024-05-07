90216cce460b79c3c28aba7f0f299f46ee6909aa
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 14:11:40 -0400

    Auto-commit on Tue 05-07-2024 14_11_40.23

--- template-parts/components/agents/list.php
+++ template-parts/components/agents/list.php
@@ -13,11 +13,21 @@ $arg = [
 $profiles = new WP_Query($arg);
 
 if ($profiles->have_posts()) : ?>
+
     <div class="container-fluid px-5 mt-10">
         <div class="row">
-            <?php while ($profiles->have_posts()) : $profiles->the_post(); ?>
+            <?php
+            $sorted_profiles = array();
+            while ($profiles->have_posts()) : $profiles->the_post();
+                $agent = get_post_meta(get_the_ID(), 'hlr_framework_agents', true);
+                $order = $agent['opt-agents-order'];
+                // Add the current agents to the sorted_profiles array with opt-agents-order as the key
+                $sorted_profiles[$order] = get_post();
+                ?>
                   <?php include(HLR_THEME_COMPONENT . 'agents/card.php'); ?>
-            <?php endwhile; ?>
+            <?php endwhile;
+            ksort($sorted_profiles);
+            ?>
             <?php wp_reset_postdata(); ?>
         </div>
         <div class="mt-5 row d-flex align-items-center justify-content-center">
