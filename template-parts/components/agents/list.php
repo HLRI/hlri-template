c4d8acc47964116f2dde86526884756a9bab39ce
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 14:31:59 -0400

    Auto-commit on Tue 05-07-2024 14_31_59.40

--- template-parts/components/agents/list.php
+++ template-parts/components/agents/list.php
@@ -46,23 +46,4 @@ $profiles = new WP_Query($arg);
     ?>
     </div>
     </div>
-    <div class="mt-5 row d-flex align-items-center justify-content-center">
-        <?php
-        echo paginate_links(array(
-            'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-            'total'        => $sorted_profiles->max_num_pages,
-            'current'      => max(1, get_query_var('paged')),
-            'format'       => '?paged=%#%',
-            'show_all'     => false,
-            'type'         => 'plain',
-            'end_size'     => 2,
-            'mid_size'     => 1,
-            'prev_next'    => true,
-            'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-            'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-            'add_args'     => false,
-            'add_fragment' => '',
-        ));
-        ?>
-    </div>
 <?php endif; ?>
