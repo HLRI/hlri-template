2c219b2cd3f2fe70df99b3810c06e2802c4b662b
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 18:41:57 -0400

    Auto-commit on Tue 05-07-2024 18_41_57.10

--- template-parts/components/main-menu.php
+++ template-parts/components/main-menu.php
@@ -72,15 +72,7 @@
 
 
             <div class="wrap-right-menu">
-                <div style="display: flex;
-    flex-direction: row-reverse;
-    justify-content: start;
-    align-items: center;">
 
-                    <img src="https://condoy.com/wp-content/uploads/2024/05/hlri.png" style="
-    height: 22px;
-">
-                </div>
                 <div class="switch-mode">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun d-block sw-mode" viewBox="0 0 16 16">
                         <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
@@ -125,7 +117,15 @@
                     <a href="#" class="icon-btn"><i class="fa fa-heart-o"></i></a> -->
                 </div>
 
+                <div style="display: flex;
+    flex-direction: row-reverse;
+    justify-content: start;
+    align-items: center;">
 
+                    <img src="https://condoy.com/wp-content/uploads/2024/05/hlri.png" style="
+    height: 22px;
+">
+                </div>
             </div>
 
 
