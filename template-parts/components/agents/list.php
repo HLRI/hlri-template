02ee58a17f0f89e7480a4f0fc4150a7963dbd375
Shahab Avin <shahab1992@hotmail.com>
2024-05-07 13:44:51 -0400

    Auto-commit on Tue 05-07-2024 13_44_51.62

--- template-parts/components/agents/list.php
+++ template-parts/components/agents/list.php
@@ -12,13 +12,35 @@ $arg = [
 
 $profiles = new WP_Query($arg);
 
-if ($profiles->have_posts()) : ?>
+if ($profiles->have_posts()) :
+    // Initialize an array to store agents sorted by opt-agents-order
+    $sorted_profiles = array();
+    while ($profiles->have_posts()) :
+        $profiles->the_post();
+        // Retrieve the opt-agents-order value for the current post
+        $agent = get_post_meta(get_the_ID(), 'hlr_framework_agents', true);
+        $order = $agent['opt-agents-order'];
+
+        // Add the current agent to the sorted_profiles array with opt-agents-order as the key
+        $sorted_profiles[$order] = get_post();
+    endwhile;
+
+    // Sort the sorted_profiles array based on opt-agents-order
+    ksort($sorted_profiles);
+
+    ?>
     <div class="container-fluid px-5 mt-10">
         <div class="row">
-            <?php while ($profiles->have_posts()) : $profiles->the_post(); ?>
-                  <?php include(HLR_THEME_COMPONENT . 'agents/card.php'); ?>
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
+            <?php
+            // Loop through sorted_profiles to display the agents
+            foreach ($sorted_profiles as $post) :
+                setup_postdata($post);
+                // Include your template part here
+                include(HLR_THEME_COMPONENT . 'agents/card.php');
+            endforeach;
+            // Restore original post data
+            wp_reset_postdata();
+            ?>
         </div>
         <div class="mt-5 row d-flex align-items-center justify-content-center">
             <?php
