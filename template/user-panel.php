788d4f3af8930baff308dd9d70ffa950ff45c148
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 02:25:42 -0400

    Auto-commit on Wed 05-15-2024  2_25_42.09

--- template/user-panel.php
+++ template/user-panel.php
@@ -74,25 +74,25 @@ $user_info = wp_get_current_user();
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
                             <h6 class="p-3 mb-0">Messages</h6>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img loading="lazy" src="<?= get_avatar_url(get_current_user_id()) ?>" alt="image" class="profile-pic">
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a message</h6>
-                                    <p class="text-gray mb-0"> 1 Minutes ago </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img loading="lazy" src="<?= get_avatar_url(get_current_user_id()) ?>" alt="image" class="profile-pic">
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a message</h6>
-                                    <p class="text-gray mb-0"> 15 Minutes ago </p>
-                                </div>
-                            </a>
+<!--                            <a class="dropdown-item preview-item">-->
+<!--                                <div class="preview-thumbnail">-->
+<!--                                    <img loading="lazy" src="--><?php //= get_avatar_url(get_current_user_id()) ?><!--" alt="image" class="profile-pic">-->
+<!--                                </div>-->
+<!--                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">-->
+<!--                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a message</h6>-->
+<!--                                    <p class="text-gray mb-0"> 1 Minutes ago </p>-->
+<!--                                </div>-->
+<!--                            </a>-->
+<!--                            <div class="dropdown-divider"></div>-->
+<!--                            <a class="dropdown-item preview-item">-->
+<!--                                <div class="preview-thumbnail">-->
+<!--                                    <img loading="lazy" src="--><?php //= get_avatar_url(get_current_user_id()) ?><!--" alt="image" class="profile-pic">-->
+<!--                                </div>-->
+<!--                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">-->
+<!--                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a message</h6>-->
+<!--                                    <p class="text-gray mb-0"> 15 Minutes ago </p>-->
+<!--                                </div>-->
+<!--                            </a>-->
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item preview-item">
                                 <div class="preview-thumbnail">
