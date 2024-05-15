9475e8131917962508ee01024a6f8f38553f5071
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 12:29:36 -0400

    Auto-commit on Wed 05-15-2024 12_29_36.17

--- archive-neighborhood.php
+++ archive-neighborhood.php
@@ -12,16 +12,30 @@ get_header();
     <div class="row">
         <?php
         // Query all terms from the 'neighborhood' taxonomy
-        $neighborhoods = get_terms( array(
-            'taxonomy' => 'neighborhood',
+        $neighborhoods = get_terms(array(
+            'taxonomy'   => 'neighborhood',
             'hide_empty' => false, // Set to true if you want to hide empty neighborhoods
-        ) );
+        ));
 
         // Loop through each neighborhood
-        foreach ( $neighborhoods as $neighborhood ) :
-            $meta = get_term_meta($neighborhood->term_id, 'neighborhood_options', true);
-
+        foreach ($neighborhoods as $neighborhood) :
+            $meta        = get_term_meta($neighborhood->term_id, 'neighborhood_options', true);
+            $neighborhood_id = $neighborhood->term_id;
+            // Query the number of properties for each neighborhood
+            $property_count_query = new WP_Query(array(
+                'post_type'      => 'property',
+                'posts_per_page' => -1,
+                'tax_query'      => array(
+                    array(
+                        'taxonomy' => 'neighborhood',
+                        'field'    => 'term_id',
+                        'terms'    => $neighborhood_id,
+                    ),
+                ),
+            ));
+            $property_count = $property_count_query->found_posts;
             ?>
+
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="card mb-4">
                     <a href="<?= get_term_link($neighborhood); ?>">
@@ -33,6 +47,7 @@ get_header();
                     </a>
                     <div class="card-body">
                         <h5 class="card-title"><?= $neighborhood->name ?></h5>
+                        <p>Available Properties: <?= $property_count ?></p>
                         <a href="<?= get_term_link($neighborhood); ?>" class="btn btn-primary">Explore</a>
                     </div>
                 </div>
