15a81289748ab7430eb4d9e5bc0c5c2561efcba9
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 01:27:38 -0400

    Auto-commit on Wed 05-15-2024  1_27_38.15

--- taxonomy-developer.php
+++ taxonomy-developer.php
@@ -7,7 +7,27 @@ if (is_user_logged_in() && (current_user_can('administrator') || current_user_ca
         <div class="row">
             <div class="col">
                 <h1>Welcome to the Developer Archive Template</h1>
-                <!-- Your content here -->
+                <h2>Property Listings</h2>
+                <ul>
+                    <?php
+                    $args = array(
+                        'post_type' => 'properties',
+                        'posts_per_page' => 10, // Adjust as needed
+                    );
+                    $properties_query = new WP_Query($args);
+                    if ($properties_query->have_posts()) :
+                        while ($properties_query->have_posts()) :
+                            $properties_query->the_post();
+                            ?>
+                            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                        <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    else :
+                        echo '<p>No properties found.</p>';
+                    endif;
+                    ?>
+                </ul>
             </div>
         </div>
     </div>
