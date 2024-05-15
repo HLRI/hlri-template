10a923dcb8685ad4960ec3e88a896102ef902a20
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 15:36:12 -0400

    Auto-commit on Wed 05-15-2024 15_36_12.03

--- template-parts/components/mobile-menu.php
+++ template-parts/components/mobile-menu.php
@@ -113,53 +113,36 @@ if (!empty($menuitems)) : ?>
                         <div class="side-content">
                             <nav class="animated bounceInDown side-nav-dropdown">
                                 <ul>
-                                    <?php $i = 0; ?>
                                     <?php foreach ($menuitems as $item) : ?>
-                                        <?php if (empty($item->children)) : ?>
-                                            <?php if ($item->menu_item_parent == 0) : ?>
-                                                <?php if ($item->title == 'hr') : ?>
-                                                    <hr class="my-3">
-                                                <?php else : ?>
-                                                    <?php
-                                                    $meta = get_post_meta($item->ID, '_prefix_menu_options', true);
-                                                    ?>
-                                                    <a href="<?= $item->url ?>" class="d-flex align-items-center nav-link nav-item"><?php if (!empty($meta['icon'])) : ?><i class="<?= str_replace('fas', 'fa', $meta['icon']) ?> fontsize-icon-account icon-color-sidebar"></i><?php endif; ?><span class="ml-2 pr-2 color-text-sidebar" $attributes=""><?= $item->title ?></span></a>
-                                                <?php endif; ?>
-                                            <?php endif; ?>
-                                        <?php else : ?>
-                                            <?php if ($item->menu_item_parent == 0) : ?>
-                                                <li class="sub-menu">
-                                                    <a href="<?= !empty($item->children) ? 'javascript:void(0);' : $item->url ?>">
-                                                        <?= $item->title ?>
-                                                        <?= !empty($item->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?>
-                                                    </a>
-                                                    <?php if (!empty($item->children)) : ?>
-                                                        <ul>
-                                                            <?php foreach ($item->children as $sub) : ?>
-                                                                <li class="sub-menu">
-                                                                    <a href="<?= $sub->url ?>">
-                                                                        <?= $sub->title ?>
-                                                                        <?= !empty($sub->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?>
-                                                                    </a>
-                                                                    <?php if (!empty($sub->children)) : ?>
-                                                                        <ul>
-                                                                            <?php foreach ($sub->children as $sub2) : ?>
-                                                                                <li><a href="<?= $sub2->url ?>"><?= $sub2->title ?></a></li>
-                                                                            <?php endforeach; ?>
-                                                                        </ul>
-                                                                    <?php endif; ?>
-                                                                </li>
-                                                            <?php endforeach; ?>
-                                                        </ul>
-                                                    <?php endif; ?>
-                                                </li>
-                                            <?php endif; ?>
+                                        <?php if ($item->menu_item_parent == 0) : ?>
+                                            <?php renderMenuItem($item); ?>
                                         <?php endif; ?>
-                                        <?php $i++; ?>
                                     <?php endforeach; ?>
                                 </ul>
                             </nav>
                         </div>
+
+                        <?php
+                        function renderMenuItem($item) {
+                            $meta = get_post_meta($item->ID, '_prefix_menu_options', true);
+                            $hasChildren = !empty($item->children);
+                            ?>
+                            <li class="<?= $hasChildren ? 'sub-menu' : '' ?>">
+                                <a href="<?= $hasChildren ? 'javascript:void(0);' : $item->url ?>">
+                                    <?= $item->title ?>
+                                    <?php if ($hasChildren) : ?>
+                                        <div class="fa fa-caret-down right"></div>
+                                    <?php endif; ?>
+                                </a>
+                                <?php if ($hasChildren) : ?>
+                                    <ul>
+                                        <?php foreach ($item->children as $child) : ?>
+                                            <?php renderMenuItem($child); ?>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                <?php endif; ?>
+                            </li>
+                        <?php } ?>
 
                     </div>
                 </div>
