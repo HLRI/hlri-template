1b98007b6bd342e29fafaeab1e40dffb669c21cf
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 16:14:49 -0400

    Auto-commit on Wed 05-15-2024 16_14_48.93

--- template-parts/components/mobile-menu.php
+++ template-parts/components/mobile-menu.php
@@ -111,51 +111,13 @@ if (!empty($menuitems)) : ?>
                             <?php endif; ?>
                         </div>
                         <div class="side-content">
-                            <?php foreach ($menuitems as $item) : ?>
-                                <?php if (empty($item->children)) : ?>
-                                    <?php if ($item->menu_item_parent == 0) : ?>
-                                        <?php if ($item->title == 'hr') : ?>
-                                            <hr class="my-3">
-                                        <?php else : ?>
-                                            <?php
-                                            $meta = get_post_meta($item->ID, '_prefix_menu_options', true);
-                                            ?>
-                                            <a href="<?= $item->url ?>" class="d-flex align-items-center nav-link nav-item"><?php if (!empty($meta['icon'])) : ?><i class="<?= str_replace('fas', 'fa', $meta['icon']) ?> fontsize-icon-account icon-color-sidebar"></i><?php endif; ?><span class="ml-2 pr-2 color-text-sidebar" $attributes=""><?= $item->title ?></span></a>
-                                        <?php endif; ?>
-                                    <?php endif; ?>
-                                <?php else : ?>
-                                    <?php $i = 0 ?>
-                                    <?php if ($i <= 0) : ?>
-                                        <nav class='animated bounceInDown side-nav-dropdown'>
-                                            <ul>
-                                            <?php endif; ?>
-                                            <?php if ($item->menu_item_parent == 0) : ?>
-                                                <li class='sub-menu'><a href='<?= !empty($item->children) ? 'javascript:void(0);' : $item->url ?>'><?= $item->title ?><?= !empty($item->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?></a>
-                                                    <?php if (!empty($item->children)) : ?>
-                                                        <ul>
-                                                            <?php foreach ($item->children as $sub) : ?>
-                                                                <li class='sub-menu'><a href='<?= $sub->url ?>'><?= $sub->title ?><?= !empty($sub->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?></a>
-                                                                    <?php if (!empty($sub->children)) : ?>
-                                                                        <ul>
-                                                                            <?php foreach ($sub->children as $sub2) : ?>
-                                                                                <li><a href='<?= $sub2->url ?>'><?= $sub2->title ?></a></li>
-                                                                            <?php endforeach; ?>
-                                                                        </ul>
-                                                                    <?php endif; ?>
-                                                                </li>
-                                                            <?php endforeach; ?>
-                                                        </ul>
-                                                    <?php endif; ?>
-                                                </li>
-                                            <?php endif; ?>
-                                            <?php if ($i <= 0) : ?>
-                                            </ul>
-                                        </nav>
-                                    <?php endif; ?>
-                                    <?php $i++; ?>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        </div>
+                            if (!empty($menuitems)) {
+                            echo '<ul class="vertical-menu">';
+                                foreach ($menuitems as $item) {
+                                echo '<li><a href="' . esc_url($item['url']) . '">' . esc_html($item['title']) . '</a></li>';
+                                }
+                                echo '</ul>';
+                            }                        </div>
                     </div>
                 </div>
                 <?php if ($theme_options['footer-mobile-menu']['opt-display-footer-mobile-menu']) : ?>
