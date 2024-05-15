403f41bf7fc63b8f17acfaf6583a4b4e631a777b
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 13:53:52 -0400

    Auto-commit on Wed 05-15-2024 13_53_52.34

--- archive-neighborhood.php
+++ archive-neighborhood.php
@@ -41,14 +41,13 @@ get_header();
     </div>
 </div>
 <div class="container my-5">
-    <div style="
-    z-index: 9;
-    display: inline-block;
-    width: 300px;
-    margin: 0px auto;
-    position: relative;
-">
-        <input type="text" id="searchInput" placeholder="Search...">
+<div style="
+z-index: 9;
+display: inline-block;
+width: 300px;
+margin: 0px auto;
+position: relative;
+"><input type="text" id="searchInput" placeholder="Search...">
 </div>
     <div class="row" id="searchResults">
         <!-- Your existing card elements will be dynamically added here -->
