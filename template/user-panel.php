1137d62de2fa63323fd8b5b85ce73ca827911945
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 02:28:50 -0400

    Auto-commit on Wed 05-15-2024  2_28_49.89

--- template/user-panel.php
+++ template/user-panel.php
@@ -115,39 +115,46 @@ $user_info = wp_get_current_user();
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
                             <h6 class="p-3 mb-0">Notifications</h6>
                             <div class="dropdown-divider"></div>
+<!--                            <a class="dropdown-item preview-item">-->
+<!--                                <div class="preview-thumbnail">-->
+<!--                                    <div class="preview-icon bg-success">-->
+<!--                                        <i class="mdi mdi-calendar"></i>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">-->
+<!--                                    <h6 class="preview-subject font-weight-normal mb-1">Event today</h6>-->
+<!--                                    <p class="text-gray ellipsis mb-0"> Just a reminder that you have an event today </p>-->
+<!--                                </div>-->
+<!--                            </a>-->
+<!--                            <div class="dropdown-divider"></div>-->
+<!--                            <a class="dropdown-item preview-item">-->
+<!--                                <div class="preview-thumbnail">-->
+<!--                                    <div class="preview-icon bg-warning">-->
+<!--                                        <i class="mdi mdi-settings"></i>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">-->
+<!--                                    <h6 class="preview-subject font-weight-normal mb-1">Settings</h6>-->
+<!--                                    <p class="text-gray ellipsis mb-0"> Update dashboard </p>-->
+<!--                                </div>-->
+<!--                            </a>-->
+<!--                            <div class="dropdown-divider"></div>-->
+<!--                            <a class="dropdown-item preview-item">-->
+<!--                                <div class="preview-thumbnail">-->
+<!--                                    <div class="preview-icon bg-info">-->
+<!--                                        <i class="mdi mdi-link-variant"></i>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">-->
+<!--                                    <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>-->
+<!--                                    <p class="text-gray ellipsis mb-0"> New admin wow! </p>-->
+<!--                                </div>-->
+<!--                            </a>-->
+<!--                            <div class="dropdown-divider"></div>-->
                             <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <div class="preview-icon bg-success">
-                                        <i class="mdi mdi-calendar"></i>
-                                    </div>
-                                </div>
                                 <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject font-weight-normal mb-1">Event today</h6>
-                                    <p class="text-gray ellipsis mb-0"> Just a reminder that you have an event today </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <div class="preview-icon bg-warning">
-                                        <i class="mdi mdi-settings"></i>
-                                    </div>
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject font-weight-normal mb-1">Settings</h6>
-                                    <p class="text-gray ellipsis mb-0"> Update dashboard </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <div class="preview-icon bg-info">
-                                        <i class="mdi mdi-link-variant"></i>
-                                    </div>
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
-                                    <p class="text-gray ellipsis mb-0"> New admin wow! </p>
+                                    <h6 class="preview-subject font-weight-normal mb-1">No Notification</h6>
+                                    <p class="text-gray ellipsis mb-0"> stay tuned! </p>
                                 </div>
                             </a>
                             <div class="dropdown-divider"></div>
