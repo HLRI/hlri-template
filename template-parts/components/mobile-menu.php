342a63158d51e46e41d84f81129ff8ab8de71cc3
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 15:26:00 -0400

    Auto-commit on Wed 05-15-2024 15_26_00.33

--- template-parts/components/mobile-menu.php
+++ template-parts/components/mobile-menu.php
@@ -129,33 +129,31 @@ if (!empty($menuitems)) : ?>
                                         <nav class='animated bounceInDown side-nav-dropdown'>
                                             <ul>
                                                 <?php foreach ($menu_items as $item) : ?>
-                                                    <?php if ($item->menu_item_parent == 0) : ?>
-                                                        <li class='sub-menu'>
-                                                            <a href='<?= !empty($item->children) ? 'javascript:void(0);' : $item->url ?>'>
-                                                                <?= $item->title ?>
-                                                                <?= !empty($item->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?>
-                                                            </a>
-                                                            <?php if (!empty($item->children)) : ?>
-                                                                <ul>
-                                                                    <?php foreach ($item->children as $sub) : ?>
-                                                                        <li>
-                                                                            <a href='<?= $sub->url ?>'>
-                                                                                <?= $sub->title ?>
-                                                                                <?= !empty($sub->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?>
-                                                                            </a>
-                                                                            <?php if (!empty($sub->children)) : ?>
-                                                                                <ul>
-                                                                                    <?php foreach ($sub->children as $sub2) : ?>
-                                                                                        <li><a href='<?= $sub2->url ?>'><?= $sub2->title ?></a></li>
-                                                                                    <?php endforeach; ?>
-                                                                                </ul>
-                                                                            <?php endif; ?>
-                                                                        </li>
-                                                                    <?php endforeach; ?>
-                                                                </ul>
-                                                            <?php endif; ?>
-                                                        </li>
-                                                    <?php endif; ?>
+                                                    <li class='sub-menu'>
+                                                        <a href='<?= !empty($item->children) ? 'javascript:void(0);' : $item->url ?>'>
+                                                            <?= $item->title ?>
+                                                            <?= !empty($item->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?>
+                                                        </a>
+                                                        <?php if (!empty($item->children)) : ?>
+                                                            <ul>
+                                                                <?php foreach ($item->children as $sub) : ?>
+                                                                    <li>
+                                                                        <a href='<?= $sub->url ?>'>
+                                                                            <?= $sub->title ?>
+                                                                            <?= !empty($sub->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?>
+                                                                        </a>
+                                                                        <?php if (!empty($sub->children)) : ?>
+                                                                            <ul>
+                                                                                <?php foreach ($sub->children as $sub2) : ?>
+                                                                                    <li><a href='<?= $sub2->url ?>'><?= $sub2->title ?></a></li>
+                                                                                <?php endforeach; ?>
+                                                                            </ul>
+                                                                        <?php endif; ?>
+                                                                    </li>
+                                                                <?php endforeach; ?>
+                                                            </ul>
+                                                        <?php endif; ?>
+                                                    </li>
                                                 <?php endforeach; ?>
                                             </ul>
                                         </nav>
