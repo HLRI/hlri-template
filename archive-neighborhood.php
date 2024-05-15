83262654a7d214f97927453dd5c64e1ceaa0f11a
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 11:53:17 -0400

    Auto-commit on Wed 05-15-2024 11_53_17.54

--- archive-neighborhood.php
+++ archive-neighborhood.php
@@ -1,7 +1,7 @@
 <?php
 /* Template Name: Archive Neighborhoods */
 get_header();
-echo '<div class="containerjj">';
+echo '<div class="container">';
 // Query all terms from the 'neighborhood' taxonomy
 $neighborhoods = get_terms( array(
     'taxonomy' => 'neighborhood',
@@ -10,8 +10,6 @@ $neighborhoods = get_terms( array(
 
 // Loop through each neighborhood
 foreach ( $neighborhoods as $neighborhood ) {
-//    $thumbnail_id = get_term_meta( $neighborhood->term_id, 'thumbnail', true ); // Assuming 'thumbnail' is the meta key for the thumbnail image
-    // Output the neighborhood name and its thumbnail image if available
     echo '<a href="https://condoy.com/neighborhood/' . $neighborhood->slug . '">' . $neighborhood->name . '</a>';
 }
 echo '</div>';
