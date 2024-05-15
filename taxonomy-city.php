be1f0aa476f8219f9c16f3d9f230e88f29b39765
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 01:10:37 -0400

    Auto-commit on Wed 05-15-2024  1_10_37.24

--- taxonomy-city.php
+++ taxonomy-city.php
@@ -59,7 +59,7 @@
                 echo paginate_links(array(
                     'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
                     'total'        => $peroperties->max_num_pages,
-                    'current'      => max(1, get_query_var('paged')),
+                    'current'      => max(1, get_query_var('page')),
                     'format'       => '?paged=%#%',
                     'show_all'     => false,
                     'type'         => 'plain',
