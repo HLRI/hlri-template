bf21da517c2b5d3cc4f1ff75c9bc79ec7d691636
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 12:08:29 -0400

    Auto-commit on Wed 05-15-2024 12_08_29.21

--- archive-neighborhood.php
+++ archive-neighborhood.php
@@ -38,8 +38,6 @@ get_header();
                 endif;
             endwhile;
             wp_reset_postdata();
-        else :
-            echo '<div class="col-12"><p>No neighborhoods found.</p></div>';
         endif;
         ?>
     </div>
