cff6d75c84173903b5c7cb6e2203b4a4950bed61
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 02:16:42 -0400

    Auto-commit on Wed 05-15-2024  2_16_42.88

--- taxonomy-developer.php
+++ taxonomy-developer.php
@@ -86,7 +86,7 @@ if (is_user_logged_in() && (current_user_can('administrator') || current_user_ca
 } else {
     // If user is not logged in or does not have the appropriate role, display login form
     get_header();
-    include HLR_THEME_PATH . 'template-parts/components/login-role.php';
-    get_footer();
+ echo 'You don`t have the permission to access this page, please login or update you role by contacting the website administrator at info@CondoY.com';
+ get_footer();
 }
 ?>
