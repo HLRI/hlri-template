d03dc11e8941e6fe8dbad0dd3ada7d60a1ad4278
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 15:46:30 -0400

    Auto-commit on Wed 05-15-2024 15_46_30.03

--- template-parts/components/mobile-menu.php
+++ template-parts/components/mobile-menu.php
@@ -121,7 +121,7 @@ if (!empty($menuitems)) : ?>
                                             <?php
                                             $meta = get_post_meta($item->ID, '_prefix_menu_options', true);
                                             ?>
-                                            <a href="<?= $item->url ?>" class="d-flex align-items-center nav-link nav-item"><?php if (!empty($meta['icon'])) : ?><i class="<?= str_replace('fas', 'fa', $meta['icon']) ?> fontsize-icon-account icon-color-sidebar"></i><?php endif; ?><span class="ml-2 pr-2 color-text-sidebar" $attributes=""><?= $item->title ?></span></a>
+                                            <li><a href="<?= $item->url ?>" class="d-flex align-items-center nav-link nav-item"><?php if (!empty($meta['icon'])) : ?><i class="<?= str_replace('fas', 'fa', $meta['icon']) ?> fontsize-icon-account icon-color-sidebar"></i><?php endif; ?><span class="ml-2 pr-2 color-text-sidebar" $attributes=""><?= $item->title ?></span></a></li>
                                         <?php endif; ?>
                                     <?php endif; ?>
                                 <?php else : ?>
