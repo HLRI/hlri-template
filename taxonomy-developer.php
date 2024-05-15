71464e0014dfbc93517c62c0e4514f0de0d00f67
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 01:21:42 -0400

    Auto-commit on Wed 05-15-2024  1_21_42.23

--- taxonomy-developer.php
+++ taxonomy-developer.php
@@ -1,83 +1,122 @@
-<?php
-// Check if user is logged in and has the appropriate role
-if (is_user_logged_in() && (current_user_can('administrator') || current_user_can('editor') || current_user_can('agent'))) :
-    get_header();
+<?php get_header() ?>
+
+<div class="container">
+
+    <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+    $term = get_queried_object();
+
+    $arg = [
+        'post_type' => 'properties',
+        'post_status' => 'publish',
+        'posts_per_page' => get_option('posts_per_page'),
+        'tax_query' => array(
+            array(
+                'taxonomy' => $term->taxonomy,
+                'field' => 'slug',
+                'terms' => $term->slug,
+            )
+        ),
+        'paged' => $paged,
+    ];
+
+    $peroperties = new WP_Query($arg);
+
     ?>
-    <div class="container">
-        <div class="row py-5">
-            <div class="col-12">
-                <div class="modal-content modal-content-login clearfix">
-                    <div class="modal-body body-login">
-                        <div class="card" style="width: 400px; margin: 0 auto;">
-                            <div class="modal-body-login login-form">
-                                <!-- Your login form HTML goes here -->
+
+
+    <?php if ($peroperties->have_posts()) : ?>
+
+        <div class="row">
+            <?php while ($peroperties->have_posts()) : $peroperties->the_post(); ?>
+                <?php $mdata = get_post_meta(get_the_ID(), 'hlr_framework_mapdata', true); ?>
+                <div class="col-lg-4 px-2 mb-4">
+                    <div class="card-listing card-listing-v2">
+
+                        <div class="card-listing-image card-listing-image-v2">
+                            <a href="<?php the_permalink() ?>" target="_blank">
+                                <?php the_post_thumbnail('normal', ['loading' => 'lazy']) ?>
+                            </a>
+                        </div>
+
+                        <div class="card-body-listing card-body-listing-v2">
+                            <div class="card-listing-content card-listing-content-v2">
+                                <a href="<?php the_permalink() ?>" target="_blank">
+                                    <h6 class="text-black"><?= strlen(get_the_title())  > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?></h6>
+                                </a>
+                                <div class="card-listing-description card-listing-description-v2">
+                                    <a href="<?php the_permalink() ?>" target="_blank">
+                                        <?= strlen(strip_tags(get_the_excerpt()))  > 65 ? substr(strip_tags(get_the_excerpt()), 0, 65) . '...' : strip_tags(get_the_excerpt()) ?>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="lable-listing lable-listing-v2">
+                                <?php if (!empty($mdata['opt-min-price-sqft'])) : ?>
+                                    <div><?= "$" . $mdata['opt-min-price-sqft'] . " to " . "$" . $mdata['opt-max-price-sqft'] ?></div>
+                                <?php endif; ?>
+                                <?php if (!empty($mdata['opt-size-min'])) : ?>
+                                    <div><?= $mdata['opt-size-min'] . " - " . $mdata['opt-size-max'] . " Sq Ft | " . $mdata['opt-occupancy'] ?></div>
+                                <?php endif; ?>
+                                <?php if (!empty($mdata['opt-address'])) : ?>
+                                    <div><?= $mdata['opt-address'] ?></div>
+                                <?php endif; ?>
                             </div>
                         </div>
+
+                        <div class="more more-v2">
+                            <div class="card-listing-options">
+                                <div>
+                                    <i onclick="setLikeProperties(this, <?= get_the_ID() ?>)" role="button" class="fa fa-heart" <?= isset($_COOKIE[get_the_ID()]) ? ' style="color:red" ' : '' ?>></i>
+                                    <span class="text-muted" id="like-total">
+                                        <?php if (!empty(get_post_meta(get_the_ID(), 'total_like', true))) : ?>
+                                            <?= get_post_meta(get_the_ID(), 'total_like', true)   ?>
+                                        <?php endif; ?>
+                                    </span>
+                                </div>
+                                <i role="button" class="fa fa-share-alt"></i>
+                                <i <?= is_user_logged_in() ? in_array(get_the_ID(), get_user_meta(get_current_user_id(), 'properties_favorites', true)) ? ' style="color:#9de450" ' : '' : '' ?> role="button" onclick="bookmark(this,<?= get_the_ID() ?>)" class="fa fa-bookmark"></i>
+                            </div>
+                            <a href="<?= get_the_permalink() ?>" title="<?php the_title() ?>" class="">more</a>
+                        </div>
+
+                        <div class="card-share">
+                            <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-facebook-square"></i></a>
+                            <a target="_blank" href="https://reddit.com/submit?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&title=<?php the_title() ?>"><i class="fa fa-reddit"></i></a>
+                            <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=linkedin&title=<?php the_title() ?>&summary=<?php the_excerpt() ?>"><i class="fa fa-linkedin-square"></i></a>
+                            <a target="_blank" href="https://wa.me/?text=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-whatsapp"></i></a>
+                            <a target="_blank" href="https://telegram.me/share/url?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>?ref=telegram"><i class="fa fa-telegram"></i></a>
+                            <a target="_blank" href="https://www.pinterest.com/pin/create/button?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>&media=<?= get_the_post_thumbnail_url() ?>&description=<?php the_title() ?>"><i class="fa fa-pinterest"></i></a>
+                            <a target="_blank" href="https://twitter.com/intent/tweet?url=<?= wp_get_shortlink(get_the_ID(), 'post', true) ?>"><i class="fa fa-twitter-square"></i></a>
+                            <span class="share-close"><i role="button" class="fa fa-arrow-up"></i></span>
+                        </div>
+
                     </div>
                 </div>
-            </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
         </div>
-        <?php
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        $term = get_queried_object();
 
-        $args = [
-            'post_type' => 'properties',
-            'post_status' => 'publish',
-            'posts_per_page' => get_option('posts_per_page'),
-            'tax_query' => [
-                [
-                    'taxonomy' => $term->taxonomy,
-                    'field' => 'slug',
-                    'terms' => $term->slug,
-                ]
-            ],
-            'paged' => $paged,
-        ];
-
-        $properties_query = new WP_Query($args);
-
-        if ($properties_query->have_posts()) :
-            ?>
-            <div class="row">
+        <?php if (get_query_var('paged') > 1) : ?>
+            <div class="mt-5 row d-flex align-items-center justify-content-center">
                 <?php
-                while ($properties_query->have_posts()) :
-                    $properties_query->the_post();
-                    // Your property listing HTML goes here
-                endwhile;
-                wp_reset_postdata();
+                echo paginate_links(array(
+                    'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+                    'total'        => $query->max_num_pages,
+                    'current'      => max(1, get_query_var('paged')),
+                    'format'       => '?paged=%#%',
+                    'show_all'     => false,
+                    'type'         => 'plain',
+                    'end_size'     => 2,
+                    'mid_size'     => 1,
+                    'prev_next'    => true,
+                    'prev_text'    => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
+                    'next_text'    => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
+                    'add_args'     => false,
+                    'add_fragment' => '',
+                ));
                 ?>
             </div>
-            <?php
-            if ($properties_query->max_num_pages > 1) :
-                ?>
-                <div class="mt-5 row d-flex align-items-center justify-content-center">
-                    <?php
-                    echo paginate_links([
-                        'base' => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
-                        'total' => $properties_query->max_num_pages,
-                        'current' => max(1, get_query_var('paged')),
-                        'format' => '?paged=%#%',
-                        'show_all' => false,
-                        'type' => 'plain',
-                        'end_size' => 2,
-                        'mid_size' => 1,
-                        'prev_next' => true,
-                        'prev_text' => sprintf('<i></i> %1$s', __('Newer Posts', 'text-domain')),
-                        'next_text' => sprintf('%1$s <i></i>', __('Older Posts', 'text-domain')),
-                        'add_args' => false,
-                        'add_fragment' => '',
-                    ]);
-                    ?>
-                </div>
-            <?php endif;
-        endif; ?>
-    </div>
-    <?php
-    get_footer();
-// If user is not logged in or does not have the appropriate role, redirect to login page
-else :
-    wp_redirect(wp_login_url());
-    exit;
-endif;
-?>
+        <?php endif; ?>
+    <?php endif; ?>
+    <?php get_footer() ?>
