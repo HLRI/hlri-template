d44384f613dddae1f85f4780362464de95e3d4f9
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 18:45:21 -0400

    Auto-commit on Wed 05-15-2024 18_45_21.73

--- assets/js/script.js
+++ assets/js/script.js
@@ -610,7 +610,9 @@ function getPropertiesRestApi(className, totalProperty, termID, token) {
           totalLike +
           "</span>" +
           "                </div>" +
-          '  <div style="" class="properties-card_actions_btn" role="button" onclick="bookmark(this, post.id )"> ' +
+          '  <div style="" class="properties-card_actions_btn" role="button" onclick="bookmark(this, ' +
+            post.id +
+            ')"> ' +
           `<i class="fa fa-bookmark"></i>` +
           "</div>" +
           "            </div>" +
