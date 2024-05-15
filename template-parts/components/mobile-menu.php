92ad27efb69177e3967fba08f0532afbd31860d3
Shahab Avin <shahab1992@hotmail.com>
2024-05-15 17:30:09 -0400

    Auto-commit on Wed 05-15-2024 17_30_09.12

--- template-parts/components/mobile-menu.php
+++ template-parts/components/mobile-menu.php
@@ -133,10 +133,25 @@ if (!empty($menuitems)) : ?>
                                                 <li class='sub-menu first'><a href='<?= !empty($item->children) ? 'javascript:void(0);' : $item->url ?>'><?= $item->title ?><?= !empty($item->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?></a>
                                                     <?php if (!empty($item->children)) : ?>
                                                         <ul>
-                                                            <?php foreach ($item->children as $sub) : ?>
+                                                            <?php $titles0 = []; foreach ($item->children as $sub) : ?>
+                                                            <?php if (!in_array($sub->title, $titles)): // Check if title has not been inserted before
+                                                                                    $titles[] = $sub->title;?>
                                                                 <li class='sub-menu second'><a href='<?= $sub->url ?>'><?= $sub->title ?><?= !empty($sub->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?></a>
                                                                     <?php if (!empty($sub->children)) : ?>
+                                                                        <ul>
+                                                                            <?php
+                                                                            $titles = []; // Array to store already inserted titles
+                                                                            foreach ($item->children as $sub) :
+                                                                                if (!in_array($sub->title, $titles)): // Check if title has not been inserted before
+                                                                                    $titles[] = $sub->title; // Add title to the list
+                                                                                    ?>
+                                                                                    <li class='sub-menu second'><a href='<?= $sub->url ?>'><?= $sub->title ?><?= !empty($sub->children) ? '<div class="fa fa-caret-down right"></div>' : '' ?></a></li>
+                                                                                <?php
+                                                                                endif;
+                                                                            endforeach;
+                                                                            ?>
 
+                                                                        </ul>
                                                                     <?php endif; ?>
                                                                 </li>
                                                             <?php endforeach; ?>
