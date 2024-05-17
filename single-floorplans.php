13cd299e9d18af64f8b4537aaf0e6cac84544b49
Shahab Avin <shahab1992@hotmail.com>
2024-05-16 22:17:47 -0400

    Auto-commit on Thu 05-16-2024 22_17_47.69

--- single-floorplans.php
+++ single-floorplans.php
@@ -395,7 +395,7 @@ $property = new WP_Query($args);
         background: #5dae44;
     }
 </style>
-            <div style="background: orange;border-radius: 10px;"><br><h3 class="font-weight-bold h5 mb-0 mt-1 text-center text-white">Register Now to get full package , book your unit</h3>
+            <div style="background: orange;border-radius: 10px;"><br><h3 class="font-weight-bold h5 mb-0 mt-2 text-center text-white">Register Now to get full package , book your unit</h3>
                 <?php echo do_shortcode('[contact-form-7 id="b14d9c3" title="floorplan contact form"]') ?></div>
             <?php if ($theme_options['opt-properties-status']) : ?>
                 <?php if (!empty($theme_options['opt-properties-shortcode'])) : ?>
