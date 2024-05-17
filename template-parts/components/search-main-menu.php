7551d6a2e4f0cf481c80572dc3123c0e0f8f5bc4
Shahab Avin <shahab1992@hotmail.com>
2024-05-17 00:04:51 -0400

    Auto-commit on Fri 05-17-2024  0_04_51.34

--- template-parts/components/search-main-menu.php
+++ template-parts/components/search-main-menu.php
@@ -1,7 +1,7 @@
 <?php if (!is_home()) : ?>
     <div class="wrap-search position-relative">
         <div class="input-group">
-            <input autocomplete="no-autocomplete" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by project name, city, neighborhood..."  aria-describedby="basic-addon2">
+            <input autocomplete="no-autocomplete" type="search" onkeyup="hlr_search()" class="form-control input-search keyword" placeholder="Search by project name, city..."  aria-describedby="basic-addon2">
             <div class="input-group-append">
                 <button class="btn btn-search"><i class="fa fa-search"></i></button>
             </div>  
