c50e27bae69faee27dbb97428de2174e16001102
Shahab Avin <shahab1992@hotmail.com>
2024-05-25 10:41:16 -0400

    Auto-commit on Sat 05-25-2024 10_41_16.39

--- single-properties.php
+++ single-properties.php
@@ -64,7 +64,7 @@ function addOrdinalSuffix($number)
                                                     class="gray short-lbl"></span>
                                         </li>
                                         <li data-label="Sqft" class="ic-sqft" title="Property Size: <?php echo $data['opt-size-min']; ?> to <?php echo $data['opt-size-max']; ?> Sqft.">
-                                            <i class="fas fa-vector-square"></i><br><strong><?php echo $data['opt-size-min'] ?> to <?php echo $data['opt-size-min'] ?></strong>
+                                            <i class="fas fa-vector-square"></i><br><strong><?php echo $data['opt-size-min'] ?> to <?php echo $data['opt-size-max'] ?></strong>
                                             <span
                                                     class="gray normal-lbl">Sqft</span><span
                                                     class="gray short-lbl"></span>
