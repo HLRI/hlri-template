1c70b0fcb5781d27de2ce32e7b2090c6b6a581fb
Shahab Avin <shahab1992@hotmail.com>
2024-08-09 15:06:37 -0400

    Auto-commit on Fri 08-09-2024 15_06_37.38

--- single-floorplans.php
+++ single-floorplans.php
@@ -90,6 +90,71 @@ $property = new WP_Query($args);
 
             <div class="row">
                 <div class="col-12 px-lg-0">
+
+
+<?php $gallery_images = get_post_meta(get_the_ID(), 'floorplans_gallery', true);
+?>
+
+                    <?php if (!empty($gallery_images)) : ?>
+                        <div class="your-gallery-class">
+                            <?php foreach ($gallery_images as $image_id) : ?>
+                                <?php $image_url = wp_get_attachment_url($image_id); ?>
+                                <div class="your-gallery-item-class">
+                                    <img src="<?php echo esc_url($image_url); ?>" alt="Gallery Image">
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+
+<!--                    <div class="col-12 col-md-12 justify-content-center align-items-center p-0" id="Gallery">-->
+<!--                        --><?php //if (isset($psd['galleries'][0]['gallery_url'])) : ?>
+<!--                            --><?php //if ($psd['galleries'][0]['gallery_url']) : ?>
+<!--                                <div class="vrmedia-gallery">-->
+<!--                                    <ul class="ecommerce-gallery">-->
+<!--                                        --><?php //foreach ($psd['galleries'] as $gallery_item) : ?>
+<!--                                            <li class="rounded" data-fancybox="gallery"-->
+<!--                                                data-caption="--><?php //= $gallery_item['caption'] ?><!--"-->
+<!--                                                data-src="--><?php //= $gallery_item['gallery_url'] ?><!--"-->
+<!--                                                data-thumb="--><?php //= $gallery_item['gallery_url'] ?><!--"-->
+<!--                                                data-src="--><?php //= $gallery_item['gallery_url'] ?><!--">-->
+<!--                                                <img class="rounded" loading="lazy" src="--><?php //= $gallery_item['gallery_url'] ?><!--"-->
+<!--                                                     alt="--><?php //= $gallery_item['caption'] ?><!--">-->
+<!---->
+<!--                                            </li>-->
+<!--                                        --><?php //endforeach; ?>
+<!--                                    </ul>-->
+<!--                                </div>-->
+<!--                            --><?php //else : ?>
+<!--                                <div class="d-flex flex-wrap justify-content-between" style="gap:10px;">-->
+<!--                                    <img loading="lazy" src="--><?php //= HLR_THEME_ASSETS . 'images/noimage.jpg' ?><!--" alt="">-->
+<!--                                    <img loading="lazy" src="--><?php //= HLR_THEME_ASSETS . 'images/noimage.jpg' ?><!--" alt="">-->
+<!--                                    <img loading="lazy" src="--><?php //= HLR_THEME_ASSETS . 'images/noimage.jpg' ?><!--" alt="">-->
+<!--                                    <img loading="lazy" src="--><?php //= HLR_THEME_ASSETS . 'images/noimage.jpg' ?><!--" alt="">-->
+<!--                                </div>-->
+<!--                            --><?php //endif; ?>
+<!--                        --><?php //else: ?>
+<!--                            <div class="h-100 d-flex justify-content-center align-items-center flex-column bg-foreground">-->
+<!--                                <h2 class="text-2xl font-bold">Gallery</h2>-->
+<!--                                <p class="text-gray-500">No images available at this time.</p>-->
+<!--                            </div>-->
+<!--                        --><?php //endif; ?>
+<!--                    </div>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     <div class="image-floorplan">
                         <?php
                         while ($property->have_posts()) : $property->the_post();
