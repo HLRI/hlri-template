c993684aee9289ca601ba6ce139a0867b5f0f3e9
Shahab Avin <shahab1992@hotmail.com>
2024-08-15 17:53:28 -0400

    Auto-commit on Thu 08-15-2024 17_53_28.18

--- single-properties.php
+++ single-properties.php
@@ -84,7 +84,8 @@ function addOrdinalSuffix($number)
                                                 <?php echo implode(', ', $data['opt-ownership']); ?>
                                             </li>
                                         <?php endif; ?>
-                                        <?php $res = ($key = array_search('Commercial', $data['opt-type'] ?? [])); echo '56456' . $res; die(); if (($key = array_search('Commercial', $data['opt-type'] ?? [])) !== false) : ?>
+                                        11111111111111111111111111111
+                                        <?php  if (($key = array_search('Commercial', $data['opt-type'] ?? [])) !== false) : ?>
         <li data-label="Beds" class="ic-beds" title="Number of Bedrooms: <?php echo ($data['opt-min-bed'] != "" ? $data['opt-min-bed'] . " - " : 'TBA '); ?> <?php echo $data['opt-max-bed'] ?>">
                                             <i class="fas fa-bed"></i><br>
 <!--            <strong>--><?php //echo $data['opt-min-bed'] ?><!-- - --><?php //echo $data['opt-max-bed'] ?><!--</strong>-->
