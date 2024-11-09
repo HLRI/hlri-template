3a2e9d55ded11ed7ff48750545363ddf0e11e916
Shahab Avin <shahab1992@hotmail.com>
2024-11-09 14:49:21 -0500

    Auto-commit on Sat 11-09-2024 14_49_21.27

--- single-properties.php
+++ single-properties.php
@@ -359,6 +359,11 @@ function addOrdinalSuffix($number)
             </nav>
         </div>
 
+        <style>
+            .wp-caption{
+                width:unset;
+            }
+        </style>
         <!-- content  -->
         <div class="col-12 col-sm-9 col-md-9  border-right border-left mb-4">
             <div class="container-fluid px-lg-5">
