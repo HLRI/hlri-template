a88d8c71e81459d36f3be7a8d68483222951868b
Shahab Avin <shahab1992@hotmail.com>
2024-11-22 12:02:26 -0500

    Auto-commit on Fri 11-22-2024 12_02_26.89

--- archive-tag.php
+++ archive-tag.php
@@ -1,5 +1,5 @@
 <?php
-/* Template Name: Archive Cities */
+/* Template Name: Archive Tags */
 get_header();
 ?>
 <style>
@@ -16,12 +16,12 @@ get_header();
         font-size: 0.9rem;
         padding: .375rem .6rem;
     }
-    .btn-grayed-out{
+    .btn-grayed-out {
         background-color: #c9c9c9;
         color: grey;
         border-color: #c9c9c9;
     }
-    .btn-grayed-out:hover{
+    .btn-grayed-out:hover {
         background-color: #c9c9c9;
         color: grey;
         border-color: #c9c9c9;
@@ -34,7 +34,7 @@ get_header();
         overflow: hidden;
         text-overflow: ellipsis;
     }
-    .flex-btn{
+    .flex-btn {
         margin: 0px 10px 0px 0px;
     }
     .flex-input {
@@ -53,7 +53,7 @@ get_header();
         border-color: #f7cb00;
         color: #252525;
     }
-    .top-search-bar{
+    .top-search-bar {
         z-index: 9;
         display: flex;
         width: 100%;
@@ -67,90 +67,86 @@ get_header();
 </style>
 <div class="page-header" style="background-image: url('https://condoy.com/wp-content/uploads/2023/09/250-Lawrence-Avenue-West-condos-8.jpg');">
     <div class="page-header-title text-center">
-        <h3>Toronto Cities</h3>
-        <h2 class="font-weight-bold">click the city to view the listings</h2>
+        <h3>Property Tags</h3>
+        <h2 class="font-weight-bold">Click a tag to explore properties</h2>
     </div>
 </div>
 <div class="container my-5">
-<div class="top-search-bar"><input type="search" id="searchInput" class="flex-input" placeholder="Search...">
-    <button id="searchButton" class="btn btn-submit-flex flex-btn">Search</button>
-    <button id="clearButton" class="btn btn-submit-flex flex-btn">Clear</button>
-</div>
+    <div class="top-search-bar">
+        <input type="search" id="searchInput" class="flex-input" placeholder="Search...">
+        <button id="searchButton" class="btn btn-submit-flex flex-btn">Search</button>
+        <button id="clearButton" class="btn btn-submit-flex flex-btn">Clear</button>
+    </div>
     <div class="row" id="searchResults">
-        <!-- Your existing card elements will be dynamically added here -->
+        <!-- Search results will dynamically appear here -->
     </div>
     <div class="row">
         <?php
-        // Query all terms from the 'city' taxonomy
-        $Cities = get_terms(array(
-            'taxonomy'   => 'city',
-            'hide_empty' => false, // Set to true if you want to hide empty Cities
-        ));
+        // Query all terms from the 'tag' taxonomy
+        $tags = get_terms([
+            'taxonomy'   => 'tag',
+            'hide_empty' => false, // Set to true to hide tags with no properties
+        ]);
 
-        // Loop through each city
-        foreach ($Cities as $city) :
-            $meta        = get_term_meta($city->term_id, 'city_options', true);
-            $city_id = $city->term_id;
-            // Query the number of properties for each city
-            $property_count_query = new WP_Query(array(
+        // Loop through each tag
+        foreach ($tags as $tag) :
+            $tag_id = $tag->term_id;
+            $meta = get_term_meta($tag_id, 'tag_options', true);
+
+            // Query the number of properties for each tag
+            $property_count_query = new WP_Query([
                 'post_type'      => 'property',
                 'posts_per_page' => -1,
-                'tax_query'      => array(
-                    array(
-                        'taxonomy' => 'city',
+                'tax_query'      => [
+                    [
+                        'taxonomy' => 'tag',
                         'field'    => 'term_id',
-                        'terms'    => $city_id,
-                    ),
-                ),
-            ));
+                        'terms'    => $tag_id,
+                    ],
+                ],
+            ]);
             $property_count = $property_count_query->found_posts;
             ?>
-
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="card mb-4">
-                    <a href="<?= get_term_link($city); ?>">
-                        <?php if (isset($meta['opt-city-image']) && !empty($meta['opt-city-image']['url'])) : ?>
-                            <img loading="lazy" src="<?= $meta['opt-city-image']['url'] ?>" class="card-img-top" alt="<?= isset($meta['opt-city-image']['alt']) ? $meta['opt-city-image']['alt'] : $city->name ?>">
+                    <a href="<?= get_term_link($tag); ?>">
+                        <?php if (isset($meta['opt-tag-image']) && !empty($meta['opt-tag-image']['url'])) : ?>
+                            <img loading="lazy" src="<?= $meta['opt-tag-image']['url'] ?>" class="card-img-top" alt="<?= isset($meta['opt-tag-image']['alt']) ? $meta['opt-tag-image']['alt'] : $tag->name ?>">
                         <?php else : ?>
                             <img loading="lazy" src="<?= HLR_THEME_ASSETS . 'images/No-Image-Placeholder.jpg' ?>" class="card-img-top" alt="Placeholder Image">
                         <?php endif; ?>
                     </a>
                     <div class="card-body">
-                        <h6 class="card-title"><?= $city->name ?></h6>
-                        <a href="<?= get_term_link($city); ?>" title="All listings in <?= $city->name ?>"  class="btn btn-primary"><?= get_post_count_taxonomy($city_id, 'city', 'properties') ?> Listing</a>
+                        <h6 class="card-title"><?= $tag->name ?></h6>
+                        <a href="<?= get_term_link($tag); ?>" title="All listings in <?= $tag->name ?>" class="btn btn-primary"><?= $property_count ?> Listings</a>
                         <a title="This option will be available soon" class="btn btn-primary btn-grayed-out">Explore on Map</a>
                     </div>
                 </div>
             </div>
-        <?php
-        endforeach;
-        ?>
+        <?php endforeach; ?>
     </div>
 </div>
 
 <script>
-    // Function to filter cards based on search input
     function filterCards() {
         var input, filter, cards, card, parentCol, title, i, txtValue;
         input = document.getElementById("searchInput");
         filter = input.value.toUpperCase();
         cards = document.getElementsByClassName("card");
 
-        // Loop through all cards, and hide those who don't match the search query
         for (i = 0; i < cards.length; i++) {
             card = cards[i];
-            parentCol = card.closest('.col-lg-3'); // Find the parent col-lg-3
+            parentCol = card.closest('.col-lg-3');
             title = card.querySelector(".card-title");
             txtValue = title.textContent || title.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                parentCol.style.display = ""; // Show parent col-lg-3
+                parentCol.style.display = "";
             } else {
-                parentCol.style.display = "none"; // Hide parent col-lg-3
+                parentCol.style.display = "none";
             }
         }
     }
 
-    // Function to clear search input and display all results
     function clearSearch() {
         document.getElementById("searchInput").value = "";
         var cols = document.querySelectorAll(".col-lg-3");
@@ -159,10 +155,7 @@ get_header();
         });
     }
 
-    // Add event listener for button click
     document.getElementById("searchButton").addEventListener("click", filterCards);
-
-    // Add event listener for clear button click
     document.getElementById("clearButton").addEventListener("click", clearSearch);
 </script>
 
