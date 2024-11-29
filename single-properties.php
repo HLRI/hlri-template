e901293f892e40d42831bd48488457718ab922b2
Shahab Avin <shahab1992@hotmail.com>
2024-11-29 15:06:55 -0500

    Auto-commit on Fri 11-29-2024 15_06_55.00

--- single-properties.php
+++ single-properties.php
@@ -356,9 +356,7 @@ function addOrdinalSuffix($number)
                     <?php foreach ($incentives as $incentive): ?>
                         <li class="d-flex align-items-center mb-4">
                             <i style="font-size: x-large;margin-right: 20px;min-width:32px;text-align:center;" class="<?php echo htmlspecialchars($incentive['opt-icon-incentives']); ?> me-2"></i>
-                            <a href="<?php echo htmlspecialchars($incentive['opt-link-incentives']); ?>" class="text-decoration-none">
-                                <?php echo htmlspecialchars($incentive['opt-link-incentives']); ?>
-                            </a>
+                            <?php echo htmlspecialchars($incentive['opt-link-incentives']); ?>
                         </li>
                     <?php endforeach; ?>
                     <div>
