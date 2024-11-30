35e7eb8be0568a92c2efce4aa3e5ff7497becf17
Shahab Avin <shahab1992@hotmail.com>
2024-11-30 11:35:06 -0500

    Auto-commit on Sat 11-30-2024 11_35_06.24

---
+++ page-just-launched-check.php
@@ -0,0 +1,16 @@
+<?php
+/* Template Name: Remove Properties */
+
+$action = $_GET['action'];
+if ($action = '6_months'){
+do_action('remove_just_launched_properties_event');
+    echo "Action executed successfully.";
+    exit;
+} else{
+    global $wp_query;
+    $wp_query->set_404();
+    status_header( 404 );
+    get_template_part( 404 ); // Load the 404 template
+    exit;
+}
+
--- page-removexxx.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Template Name: Remove Properties */
-
-do_action('remove_just_launched_properties_event');
-
-// Output a simple message
-echo "Action executed successfully.";
-exit;
