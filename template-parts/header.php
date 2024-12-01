34846769728cb4b1d36752a77cfb2b566764ab41
Shahab Avin <shahab1992@hotmail.com>
2024-11-30 21:38:23 -0500

    Auto-commit on Sat 11-30-2024 21_38_23.00

--- template-parts/header.php
+++ template-parts/header.php
@@ -30,6 +30,7 @@ if (is_front_page()) {
     <meta itemprop="addressCountry" content="CA" />
 </div>
         <meta itemprop="telephone" content="416-599-9599" />
+        <meta itemprop="priceRange" content="$299,000 to $2,000,000" />
         <meta itemprop="description" content="Condoy.com: Toronto & GTA\'s top pre-construction condos and townhomes. Exclusive access to insider pricing, platinum sales, and expert investment insights." />
         <div itemprop="openingHoursSpecification" itemscope itemtype="http://schema.org/OpeningHoursSpecification">
             <meta itemprop="dayOfWeek" content="Monday, Tuesday, Wednesday, Thursday, Friday" />
