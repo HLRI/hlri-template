60e708cdf18e98aea8cc58413a8095ba6e093b25
Shahab Avin <shahab1992@hotmail.com>
2024-11-30 21:16:21 -0500

    Auto-commit on Sat 11-30-2024 21_16_21.83

--- template-parts/header.php
+++ template-parts/header.php
@@ -19,7 +19,20 @@
     if (is_post_type_archive('cities') || is_tax('cities')) {
         echo '<meta name="robots" content="index, follow">';
     }
-    ?>
+if (is_front_page()) {
+    echo '<div itemscope itemtype="http://schema.org/LocalBusiness">
+        <meta itemprop="name" content="Condoy.com" />
+        <meta itemprop="address" content="300 Richmond St W #300, Toronto, ON M5V 1X2, Canada" />
+        <meta itemprop="description" content="Condoy.com: Toronto & GTA\'s top pre-construction condos and townhomes. Exclusive access to insider pricing, platinum sales, and expert investment insights." />
+        <div itemprop="openingHoursSpecification" itemscope itemtype="http://schema.org/OpeningHoursSpecification">
+            <meta itemprop="dayOfWeek" content="Monday, Tuesday, Wednesday, Thursday, Friday" />
+            <meta itemprop="opens" content="09:00" />
+            <meta itemprop="closes" content="18:00" />
+        </div>
+    </div>';
+}
+?>
+
 
 </head>
 
