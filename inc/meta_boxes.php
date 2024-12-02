5108e0908e118c6e15ee15358c37734c959caf63
Shahab Avin <shahab1992@hotmail.com>
2024-12-02 11:47:13 -0500

    Auto-commit on Mon 12-02-2024 11_47_13.29

--- inc/meta_boxes.php
+++ inc/meta_boxes.php
@@ -53,7 +53,7 @@ function custom_render_floorplans_meta_box( $post ) {
         echo '</ul>';
         echo '</div>';
 
-        echo array_sum($totalFloors) / count($totalFloors);
+        echo round(array_sum($totalFloors) / count($totalFloors));
     } else {
         echo '<p>No associated floorplans found for this property.</p>';
     }
