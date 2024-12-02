216f0c0e8fb08453c44630fb00c5582ba5430e2c
Shahab Avin <shahab1992@hotmail.com>
2024-12-02 18:16:10 -0500

    Auto-commit on Mon 12-02-2024 18_16_10.34

--- single-properties-assignment.php
+++ single-properties-assignment.php
@@ -229,7 +229,7 @@ function addOrdinalSuffix($number)
                         }
                     </style>
 
-                    <div class="container-fluid">
+                    <div class="container">
                         <div>
                             <?php
                             $project_incentives = get_post_meta(get_the_ID(), 'hlr_framework_properties-incentives', true);
