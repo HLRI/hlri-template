01aa583e610e3f6bc9986865d0a3bc4d8ddbf2fe
Shahab Avin <shahab1992@hotmail.com>
2024-12-03 11:18:36 -0500

    Auto-commit on Tue 12-03-2024 11_18_36.11

--- single-properties.php
+++ single-properties.php
@@ -566,7 +566,7 @@ if (is_user_logged_in()) {
                                 Est. Building Size
                             </div>
                             <div class="card-developments-content">
-                                <?php echo($data['opt-size-min'] != "" ? $data['opt-size-min'] . " - " : 'TBA '); ?><?php echo $data['opt-size-max'] ?>
+                                <?php echo !empty($data['opt-size-min']) ? $data['opt-size-min'] . (!empty($data['opt-size-max']) ? " - " . $data['opt-size-max'] : "") . " /sqft" : "TBA"; ?>
                             </div>
                         </div>
                     </div>
