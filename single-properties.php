9034a16013662c124d44caa98c149f33afa3636b
Shahab Avin <shahab1992@hotmail.com>
2024-12-03 11:34:42 -0500

    Auto-commit on Tue 12-03-2024 11_34_41.98

--- single-properties.php
+++ single-properties.php
@@ -740,7 +740,7 @@ if (is_user_logged_in()) {
                         }
                         ?>
                         <div class="row">
-                            <div class="col-12">
+                            <div>
                                 <script>
                                     jQuery(document).ready(function ($) {
                                         <?php foreach ($galleries as $index => $gallery): ?>
@@ -761,7 +761,7 @@ if (is_user_logged_in()) {
                                     });
                                 </script>
                                 <?php foreach ($galleries as $index => $gallery_items): ?>
-                                    <div class="col-12 col-md-12 justify-content-center align-items-center p-0 px-2"
+                                    <div class="col-12 col-md-12 justify-content-center align-items-center"
                                          id="Gallery-<?php echo $index; ?>">
                                         <div class="titr-list ml-0">
                                             <h3 class="font-weight-bold"><?php echo htmlspecialchars($titles[$index] ?? 'Gallery ' . ($index + 1)); ?></h3>
