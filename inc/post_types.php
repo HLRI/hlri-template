c047987f4da3d7a191924a8f910880f5677145fe
Shahab Avin <shahab1992@hotmail.com>
2024-12-05 18:39:26 -0500

    Auto-commit on Thu 12-05-2024 18_39_26.00

--- inc/post_types.php
+++ inc/post_types.php
@@ -115,47 +115,42 @@ function properties() {
 add_action('init', 'properties', 0);
 
 
-function floorplans() {
+function floorplans()
+{
     $labels = array(
         'name'                  => _x('Floorplans', 'floorplans General Name', 'text_domain'),
         'singular_name'         => _x('floorplan', 'floorplan Singular Name', 'text_domain'),
         'menu_name'             => __('Floorplans', 'text_domain'),
         'name_admin_bar'        => __('floorplans', 'text_domain'),
-        'hierarchical' => true, // This enables parent-child relationships
-        'rewrite' => array('slug' => 'properties', 'with_front' => false),
         'archives'              => __('Item Archives', 'text_domain'),
         'attributes'            => __('Item Attributes', 'text_domain'),
-        'parent_item_colon'     => __('Parent Item:', 'text_domain'),
-        'all_items'             => __('All Items', 'text_domain'),
-        'add_new_item'          => __('Add New Item', 'text_domain'),
-        'add_new'               => __('Add New', 'text_domain'),
-        'new_item'              => __('New Item', 'text_domain'),
-        'edit_item'             => __('Edit Item', 'text_domain'),
-        'update_item'           => __('Update Item', 'text_domain'),
-        'view_item'             => __('View Item', 'text_domain'),
-        'view_items'            => __('View Items', 'text_domain'),
-        'search_items'          => __('Search Item', 'text_domain'),
+        'parent_item_colon'     => __('Parent Property:', 'text_domain'),
+        'all_items'             => __('All Floorplans', 'text_domain'),
+        'add_new_item'          => __('Add New Floorplan', 'text_domain'),
+        'add_new'               => __('Add Floorplan', 'text_domain'),
+        'new_item'              => __('New Floorplan', 'text_domain'),
+        'edit_item'             => __('Edit Floorplan', 'text_domain'),
+        'update_item'           => __('Update Floorplan', 'text_domain'),
+        'view_item'             => __('View Floorplan', 'text_domain'),
+        'search_items'          => __('Search Floorplans', 'text_domain'),
         'not_found'             => __('Not found', 'text_domain'),
         'not_found_in_trash'    => __('Not found in Trash', 'text_domain'),
         'featured_image'        => __('Featured Image', 'text_domain'),
         'set_featured_image'    => __('Set featured image', 'text_domain'),
         'remove_featured_image' => __('Remove featured image', 'text_domain'),
         'use_featured_image'    => __('Use as featured image', 'text_domain'),
-        'insert_into_item'      => __('Insert into item', 'text_domain'),
-        'uploaded_to_this_item' => __('Uploaded to this item', 'text_domain'),
-        'items_list'            => __('Items list', 'text_domain'),
-        'items_list_navigation' => __('Items list navigation', 'text_domain'),
-        'filter_items_list'     => __('Filter items list', 'text_domain'),
-
+        'insert_into_item'      => __('Insert into floorplan', 'text_domain'),
+        'uploaded_to_this_item' => __('Uploaded to this floorplan', 'text_domain'),
+        'items_list'            => __('Floorplans list', 'text_domain'),
+        'filter_items_list'     => __('Filter floorplans list', 'text_domain'),
     );
 
     $args = array(
-        'label'                 => __('floorplans', 'text_domain'),
-        'description'           => __('floorplans Description', 'text_domain'),
+        'label'                 => __('Floorplans', 'text_domain'),
+        'description'           => __('Floorplans Description', 'text_domain'),
         'labels'                => $labels,
-        'supports'              => ['title', 'thumbnail'],
-        // 'taxonomies'            => array( ''),
-        'hierarchical'          => false,
+        'supports'              => ['title', 'thumbnail', 'page-attributes','editor','slug'],
+        'hierarchical'          => true,
         'public'                => true,
         'show_ui'               => true,
         'show_in_menu'          => true,
@@ -163,17 +158,21 @@ function floorplans() {
         'show_in_admin_bar'     => true,
         'show_in_nav_menus'     => true,
         'can_export'            => true,
-        'has_archive'           => true,
+        'has_archive'           => false,
         'exclude_from_search'   => false,
         'publicly_queryable'    => true,
         'capability_type'       => 'post',
-        'menu_icon'   => 'dashicons-analytics',
-        'rewrite'         => array('slug' => 'floorplans', 'with_front' => false),
+        'menu_icon'             => 'dashicons-analytics',
+        'rewrite'               => array(
+            'slug' => 'properties/%property%/floorplans',
+            'with_front' => false,
+        ),
     );
 
     register_post_type('floorplans', $args);
 }
 add_action('init', 'floorplans', 0);
+
 
 add_action( 'save_post_properties', 'save_last_update' );
 add_action( 'save_post_floorplans', 'save_last_update' );
