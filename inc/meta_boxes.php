d687d0e3e666efa58ee4894154e1a2dcbfda1300
Shahab Avin <shahab1992@hotmail.com>
2024-12-05 19:19:02 -0500

    Auto-commit on Thu 12-05-2024 19_19_02.75

--- inc/meta_boxes.php
+++ inc/meta_boxes.php
@@ -432,20 +432,20 @@ function custom_render_associated_floorplans()
 
 
 
-
-
-function add_floorplans_rewrite_rules()
-{
-    add_rewrite_rule(
-        '^properties/([^/]+)/floorplans/([^/]+)/?$',
-        'index.php?floorplans=$matches[2]&property=$matches[1]',
-        'top'
-    );
-}
-add_action('init', 'add_floorplans_rewrite_rules');
-
-
-
+//
+//
+//function add_floorplans_rewrite_rules()
+//{
+//    add_rewrite_rule(
+//        '^properties/([^/]+)/floorplans/([^/]+)/?$',
+//        'index.php?floorplans=$matches[2]&property=$matches[1]',
+//        'top'
+//    );
+//}
+//add_action('init', 'add_floorplans_rewrite_rules');
+//
+//
+//
 
 function floorplans_permalink($permalink, $post)
 {
