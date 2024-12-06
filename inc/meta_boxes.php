6d0db0c902dc0212bc2b92ad5943b043fecd7567
Shahab Avin <shahab1992@hotmail.com>
2024-12-05 20:09:51 -0500

    Auto-commit on Thu 12-05-2024 20_09_50.98

--- inc/meta_boxes.php
+++ inc/meta_boxes.php
@@ -238,73 +238,73 @@ function custom_render_associated_floorplans()
 
 
 
-//
-//
-//
-//
-//// start modify floorplan link
-//
-///*==================================================================================*/
-//
-//// Modify the floorplans query to include the associated property
-//function custom_modify_floorplans_query($query)
-//{
-//    if (!is_admin() || !$query->is_main_query()) {
-//        return;
-//    }
-//
-//    if ($query->get('post_type') === 'floorplans') {
-//        $query->set('rewrite', array('slug' => 'properties', 'with_front' => false));
-//    }
-//}
-//
-//add_action('pre_get_posts', 'custom_modify_floorplans_query');
-//
-///*==================================================================================*/
-//
-//// Modify the floorplans permalink structure
-//function custom_modify_floorplans_permalink($permalink, $post)
-//{
-//    if ($post->post_type === 'floorplans') {
-//        $associated_property = get_post_meta($post->ID, 'associated_property', true);
-//        $property_name = get_post_field('post_name', $associated_property); // Get the slug of the associated property
-//        $floorplan_slug = $post->post_name;
-//
-//        $permalink = home_url("/properties/$property_name/floorplans/$floorplan_slug/");
-//    }
-//
-//    return $permalink;
-//}
-//
-//add_filter('post_type_link', 'custom_modify_floorplans_permalink', 10, 2);
-////hgj
-//
-///*==================================================================================*/
-//
-//// Register additional rewrite rules for the modified floorplans permalink structure
-//function custom_add_rewrite_rules()
-//{
-//    add_rewrite_rule('^properties/([^/]+)/floorplans/([^/]+)/?$', 'index.php?properties=$matches[1]&floorplans=$matches[2]', 'top');
-//}
-//
-//add_action('init', 'custom_add_rewrite_rules');
-//
-///*==================================================================================*/
-//
-//// Flush rewrite rules when the associated property is saved or updated
-//function custom_flush_rewrite_rules()
-//{
-//    flush_rewrite_rules();
-//}
-//
-//add_action('save_post_associated_property', 'custom_flush_rewrite_rules');
-//add_action('delete_post_associated_property', 'custom_flush_rewrite_rules');
-//
-///*==================================================================================*/
-//
-//// end modify floorplan link
-//
-//
+
+
+
+
+// start modify floorplan link
+
+/*==================================================================================*/
+
+// Modify the floorplans query to include the associated property
+function custom_modify_floorplans_query($query)
+{
+    if (!is_admin() || !$query->is_main_query()) {
+        return;
+    }
+
+    if ($query->get('post_type') === 'floorplans') {
+        $query->set('rewrite', array('slug' => 'properties', 'with_front' => false));
+    }
+}
+
+add_action('pre_get_posts', 'custom_modify_floorplans_query');
+
+/*==================================================================================*/
+
+// Modify the floorplans permalink structure
+function custom_modify_floorplans_permalink($permalink, $post)
+{
+    if ($post->post_type === 'floorplans') {
+        $associated_property = get_post_meta($post->ID, 'associated_property', true);
+        $property_name = get_post_field('post_name', $associated_property); // Get the slug of the associated property
+        $floorplan_slug = $post->post_name;
+
+        $permalink = home_url("/properties/$property_name/floorplans/$floorplan_slug/");
+    }
+
+    return $permalink;
+}
+
+add_filter('post_type_link', 'custom_modify_floorplans_permalink', 10, 2);
+//hgj
+
+/*==================================================================================*/
+
+// Register additional rewrite rules for the modified floorplans permalink structure
+function custom_add_rewrite_rules()
+{
+    add_rewrite_rule('^properties/([^/]+)/floorplans/([^/]+)/?$', 'index.php?properties=$matches[1]&floorplans=$matches[2]', 'top');
+}
+
+add_action('init', 'custom_add_rewrite_rules');
+
+/*==================================================================================*/
+
+// Flush rewrite rules when the associated property is saved or updated
+function custom_flush_rewrite_rules()
+{
+    flush_rewrite_rules();
+}
+
+add_action('save_post_associated_property', 'custom_flush_rewrite_rules');
+add_action('delete_post_associated_property', 'custom_flush_rewrite_rules');
+
+/*==================================================================================*/
+
+// end modify floorplan link
+
+
 
 
 
