8f5251fcce09c16df83d050a5852d42f4dd4be87
Shahab Avin <shahab1992@hotmail.com>
2024-12-24 12:02:33 -0500

    Auto-commit on Tue 12-24-2024 12_02_33.73

--- page-lottery-code.php
+++ page-lottery-code.php
@@ -7,10 +7,11 @@ get_header();
 
     <div class="container mt-5 mb-5 pt-5 pb-5">
         <div class="mt-5 mb-5 pt-5 pb-5">
-            <h1>
+            <h1>Thank you!</h1>
+            <h2>
                 Your Lottery Code Is:
                 <?= isset($_SESSION['lottery-code']) ? $_SESSION['lottery-code'] : 'No code generated'; ?>
-            </h1>
+            </h2>
         </div>
     </div>
 
