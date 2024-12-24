78336b66b22f195d6a278cde1ff50d4c4a4bf09a
Shahab Avin <shahab1992@hotmail.com>
2024-12-24 11:39:29 -0500

    Auto-commit on Tue 12-24-2024 11_39_29.24

---
+++ page-lottery-code.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: lottery code
+*/
+get_header();
+?>
+
+    <div class="container mt-5 mb-5 pt-5 pb-5">
+    <div class="mt-5 mb-5 pt-5 pb-5">
+
+    <h1>Your Lottery Code Is <?= $_SESSION['cf7_submission']['lottery-code'];
+        ?></h1>
+        <p></p>
+    </div>
+    </div>
+
+<?php
+get_footer();
