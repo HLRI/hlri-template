e6dfe7ef97e83fd270684385fc96c773cb15c4ad
Shahab Avin <shahab1992@hotmail.com>
2024-12-28 13:47:12 -0500

    Auto-commit on Sat 12-28-2024 13_47_12.47

--- archive-agents.php
+++ archive-agents.php
@@ -6,6 +6,7 @@
         define('CUSTOM_PAGE_HEADER', [
             'title' => 'We have world expert team',
             'subtitle' => 'Our expert team',
+            'paragraph' => 'Home Leader Realty is one of the top leading brokerages in the sales of pre-construction and resale condos in the GTA. Our team specializes in pre-construction sales. Through our cultivated developer relationships, we have access to PLATINUM SALES and TRUE UNIT ALLOCATION in advance of the general REALTOR® and the general public. With cutting edge technology and a skilled team of experts, Home Leader Realty will lead you to the best return on your investments. Buying a condo in Toronto is one of the best investments you will ever make and putting your trust in this brokerage will outperform, whether it is your first home or added investment. Our combined 100 years plus of experience serves only the best locations meeting your needs.',
         ]);
 
         include(HLR_THEME_COMPONENT . 'custom-page-header.php');
