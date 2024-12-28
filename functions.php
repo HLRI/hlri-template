21c7abc3b00a01f51d56eff1b08cbf09170d6d67
Shahab Avin <shahab1992@hotmail.com>
2024-12-28 14:28:33 -0500

    Auto-commit on Sat 12-28-2024 14_28_33.12

--- functions.php
+++ functions.php
@@ -25,6 +25,7 @@ include HLR_THEME_PATH . 'inc/post_types.php';
 include HLR_THEME_PATH . 'inc/meta_boxes.php';
 include HLR_THEME_PATH . 'inc/taxonomy.php';
 include HLR_THEME_PATH . 'inc/visit_history.php';
+include HLR_THEME_PATH . 'inc/admin_functions.php';
 include HLR_THEME_PATH . 'inc/admin_pages.php';
 include HLR_THEME_PATH . 'inc/caching.php';
 
@@ -441,62 +442,3 @@ function update_property_price_per_sqft_on_floorplan_edit($post_id, $post, $upda
 
 
 
-// Add a custom rewrite rule to catch the 'admin_function' query variable under wp-admin
-add_action('init', function () {
-    add_rewrite_rule(
-        '^wp-admin/admin_function/?$', // Custom URL: example.com/wp-admin/admin_function
-        'index.php?action=1', // Internal query parameter
-        'top' // Priority at the top of the rules
-    );
-});
-
-// Register the custom query variable 'admin_function'
-add_filter('query_vars', function ($query_vars) {
-    $query_vars[] = 'action'; // Add 'admin_function' to the list of query vars
-    return $query_vars;
-});
-
-// Hook into template_redirect to run the functions based on the 'admin_function' query variable
-add_action('template_redirect', function () {
-    // Ensure that only administrators can access this URL
-    if (!is_admin() && current_user_can('administrator')) {
-        $function_name = get_query_var('action');
-
-        // Check if the query variable is set and matches a known function
-        if ($function_name) {
-            // Call different functions based on the 'admin_function' parameter
-            switch ($function_name) {
-                case 'function_name_1':
-                    function_name_1();
-                    break;
-                case 'function_name_2':
-                    function_name_2();
-                    break;
-                case 'function_name_3':
-                    function_name_3();
-                    break;
-                // Add more cases for other functions as needed
-                default:
-                    // If the function is not recognized, show an error or default message
-                    echo 'Function not found.';
-                    break;
-            }
-            exit; // Prevent WordPress from rendering a full page
-        }
-    } else {
-        wp_die('You do not have sufficient permissions to access this page.');
-    }
-});
-
-// Example functions
-function function_name_1() {
-    echo 'This is Function 1';
-}
-
-function function_name_2() {
-    echo 'This is Function 2';
-}
-
-function function_name_3() {
-    echo 'This is Function 3';
-}
