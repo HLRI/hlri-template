c1ccf1a9f1bb7059acb42ebfa7a88f02a0c4b64f
Shahab Avin <shahab1992@hotmail.com>
2024-12-28 20:54:31 -0500

    Auto-commit on Sat 12-28-2024 20_54_31.25

--- template-parts/components/properties-category/hot-deals.php
+++ template-parts/components/properties-category/hot-deals.php
@@ -6,7 +6,7 @@
                 <a href="<?= home_url('group/this-month-hot-new-projects-in-toronto/') ?>" title="" class="view-more">View more</a>
             </div>
             <div class="d-flex justify-content-center">
-                <div class="owl-carousel owl-theme listing-wrap wrap-list hotdeals">
+                <div class="owl-carousel owl-theme listing-wrap wrap-list hot-deals">
                 </div>
             </div>
         </div>
