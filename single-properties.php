b0bcaf526b48b6853092fa393e03fff16ba985c1
Shahab Avin <shahab1992@hotmail.com>
2024-12-28 21:23:23 -0500

    Auto-commit on Sat 12-28-2024 21_23_22.98

--- single-properties.php
+++ single-properties.php
@@ -1211,7 +1211,25 @@ if ($peroperties_single) :
                                                 <a href="<?= get_the_permalink() ?>"
                                                    title="<?= strlen(get_the_title()) > 12 ? substr(get_the_title(), 0, 12) . '...' : get_the_title() ?>">
                                                     <!--                                                    --><?php //= strlen(strip_tags($psd['excerpt'])) > 65 ? substr(strip_tags($psd['excerpt']), 0, 65) . '...' : substr(strip_tags($psd['excerpt']), 0, 65) ?>
-                                                </a>
+                                                </a><br>
+                                                <div>
+                                                    <?php
+                                                    $project_incentives = get_post_meta(get_the_ID(), 'hlr_framework_properties-incentives', true);
+                                                    if (!empty($project_incentives['opt_properties_incentives_items'])) {
+                                                        $incentives = $project_incentives['opt_properties_incentives_items'];
+                                                    } else {
+                                                        $incentives = [];
+                                                    }
+                                                    ?>
+                                                    <ul class="custom-incentive-list mb-4">
+                                                        <?php foreach ($incentives as $incentive): ?>
+                                                            <li class="custom-incentive-item d-flex align-items-center mb-2 text-center">
+                                                                <i class="text-dark <?php echo htmlspecialchars($incentive['opt-icon-incentives']); ?> me-2 custom-icon"></i>
+                                                                <span class="custom-text"><?php echo $incentive['opt-link-incentives']; ?></span>
+                                                            </li>
+                                                        <?php endforeach; ?>
+                                                    </ul>
+                                                </div>
                                             </div>
                                         </div>
 
