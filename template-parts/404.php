397b940cf59802e719a3d736eade0555b5967ec6
Shahab Avin <shahab1992@hotmail.com>
2024-12-28 20:30:13 -0500

    Auto-commit on Sat 12-28-2024 20_30_13.06

--- template-parts/404.php
+++ template-parts/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="d-flex align-items-center justify-content-center flex-column" style="width: 100%;position: relative;">
+    <div class="d-flex align-items-center justify-content-center flex-column mt-5" style="width: 100%;position: relative;">
         <div style="height: auto;">
             <img loading="lazy" style="width: 100%;" src="<?= HLR_THEME_ASSETS . 'images/404.png' ?>" alt="">
         </div>
