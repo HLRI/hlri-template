0dee5c6796241627c9896fe0d8711399639ba530
Shahab Avin <shahab1992@hotmail.com>
2024-12-28 19:31:34 -0500

    Auto-commit on Sat 12-28-2024 19_31_34.66

--- assets/js/script.js
+++ assets/js/script.js
@@ -518,8 +518,8 @@ function getPropertiesRestApi(className, totalProperty, termID, token) {
           : "";
 
         var incentives = post.metadata['incentives'];
-        var incentivesI = post.metadata['incentives'][0];
-        console.log(incentives,incentivesI);
+        var incentivesI = incentives['opt_properties_incentives_items'];
+        console.log(incentivesI);
         var minSize = post.metadata["opt-size-min"]
           ? '<div class="properties-card_size">' +
             '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21.3 15.3a2.4 2.4 0 0 1 0 3.4l-2.6 2.6a2.4 2.4 0 0 1-3.4 0L2.7 8.7a2.41 2.41 0 0 1 0-3.4l2.6-2.6a2.41 2.41 0 0 1 3.4 0Z"/><path d="m14.5 12.5 2-2"/><path d="m11.5 9.5 2-2"/><path d="m8.5 6.5 2-2"/><path d="m17.5 15.5 2-2"/></svg>' +
