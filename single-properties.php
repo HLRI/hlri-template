cb11e12467ece93d019e01202a4476b8af91adef
Shahab Avin <shahab1992@hotmail.com>
2024-12-30 14:58:19 -0500

    Auto-commit on Mon 12-30-2024 14_58_19.26

--- single-properties.php
+++ single-properties.php
@@ -122,7 +122,7 @@ if (is_user_logged_in()) {
                                                 <i class="fas fa-bed"></i><br>
                                                 <strong><?php echo($data['opt-min-bed'] != "" ? $data['opt-min-bed'] . " - " : 'TBA '); ?><?php echo $data['opt-max-bed'] ?>
 
-                                                    <?php echo($data['opt-max-half-bath'] != "" ? '<span style="color: #FF5722;font-size: 12px;">+ ' . $data['opt-max-half-bath'] . 'Den</span>' : ''); ?>
+                                                    <?php echo($data['opt-den-max'] != "" ? '<span style="color: #7c7c7c;font-size: 12px;>+ ' . $data['opt-den-max'] . 'Den</span>' : ''); ?>
 
                                                 </strong>
                                                 <span class="gray normal-lbl">Bedroom<?php echo(intval($data['opt-max-bed']) >= 2 ? 's' : ''); ?></span><span
@@ -131,7 +131,9 @@ if (is_user_logged_in()) {
 
                                             <li data-label="Baths" class="ic-baths"
                                                 title="Number of Bathrooms: <?php echo($data['opt-min-bath'] != "" ? $data['opt-min-bath'] . " - " : 'TBA '); ?> <?php echo $data['opt-max-bath'] ?>">
-                                                <i class="fas fa-bath"></i><br><strong><?php echo($data['opt-min-bath'] != "" ? $data['opt-min-bath'] . " - " : 'TBA '); ?><?php echo $data['opt-max-bath'] ?></strong>
+                                                <i class="fas fa-bath"></i><br><strong><?php echo($data['opt-min-bath'] != "" ? $data['opt-min-bath'] . " - " : 'TBA '); ?><?php echo $data['opt-max-bath'] ?>
+                                                    <?php echo($data['opt-max-half-bath'] != "" ? '<span style="color: #7c7c7c;font-size: 12px;">+ ' . $data['opt-max-half-bath'] . 'Den</span>' : ''); ?>
+                                                </strong>
                                                 <span class="gray normal-lbl">Bathroom<?php echo(intval($data['opt-max-bath']) >= 2 ? 's' : ''); ?></span><span
                                                         class="gray short-lbl"></span>
                                             </li>
