d02f086b89264b96991393ebb36af99307ec028f
Shahab Avin <shahab1992@hotmail.com>
2024-12-31 16:23:54 -0500

    Auto-commit on Tue 12-31-2024 16_23_54.18

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,34 +35,6 @@ jQuery(document).ready(function ($) {
     margin: 5,
   });
 
-  jQuery(".listing-wrap").owlCarousel({
-    responsive: {
-      200: { items: 1 },
-      300: { items: 1 },
-      600: { items: 2 },
-      768: { items: 2 },
-      992: { items: 2 },
-      1100: { items: 2 },
-      1300: { items: 2 },
-      1400: { items: 2 },
-      1600: { items: 2 },
-    },
-    loop: true,
-    nav: true,
-    navText: [
-      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-    ],
-    autoplay: false,
-    autoplaySpeed: 1000,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    rtl: false,
-    center: false,
-    dots: false,
-    // autoWidth: true,
-    margin: 5,
-  });
 
   $(".neighborhood").owlCarousel({
     responsive: {
