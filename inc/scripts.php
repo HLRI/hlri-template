1d951ef2550692495293913388655330f817e81f
Shahab Avin <shahab1992@hotmail.com>
2025-01-07 17:52:57 -0500

    Auto-commit on Tue 01-07-2025 17_52_57.18

--- inc/scripts.php
+++ inc/scripts.php
@@ -522,7 +522,7 @@ function add_search_input_to_meta_box($meta_box_id)
         <style>.categorydiv div.tabs-panel{height:250px;}</style><script>
 
             // avoid an error:
-            if(document.getElementById('in-group-1064-2').checked === false){
+            if(document.getElementById('in-group-10-2').checked === false){
                 let commercialCheck = document.getElementById('in-group-1064-2').checked = false;
             }
             document.getElementById('in-group-10-2').addEventListener('change', function(event) {
